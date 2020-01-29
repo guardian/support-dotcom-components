7d74ac156aad5bfd6371281ab9cce9ce48e1d410
Andre Silva <andre.silva.freelancer@guardian.co.uk>
2020-01-29 18:42:35 +0000

    Adds some logic to Epic wrapper

--- src/components/ContributionsEpicWrapper.tsx
+++ src/components/ContributionsEpicWrapper.tsx
@@ -6,8 +6,38 @@ type Props = {
     targeting: EpicTargeting;
 };
 
+type ContentType = {
+    contentType: string;
+};
+
+type SuitableType = {
+    shouldHideReaderRevenue: boolean;
+    isMinuteArticle: boolean;
+    isPaidContent: boolean;
+};
+
+// Determine if it's the right type of content to be considered for an epic
+const isCorrectContent = ({ contentType }: ContentType): boolean => {
+    const acceptedTypes = ['Article'];
+    return acceptedTypes.includes(contentType);
+};
+
+// Determine if the content is suitable for an epic
+const isEpicSuitable = ({
+    shouldHideReaderRevenue,
+    isMinuteArticle,
+    isPaidContent,
+}: SuitableType): boolean => {
+    const isNotSuitable = [shouldHideReaderRevenue, isMinuteArticle, isPaidContent].some(
+        condition => condition,
+    );
+    return !isNotSuitable;
+};
+
 export const ContributionsEpicWrapper: React.FC<Props> = ({ children, targeting }: Props) => {
-    console.log('Targeting: ');
-    console.log(targeting);
-    return <>{children}</>;
+    if (isCorrectContent(targeting) && isEpicSuitable(targeting)) {
+        return children;
+    }
+
+    return null;
 };
