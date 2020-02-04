c3bc8e064bac4af5d40e98b2ee431d341f2f116e
Andre Silva <andre.silva.freelancer@guardian.co.uk>
2020-02-04 14:23:27 +0000

    Refactor typography.ts to use types exported from the Design System

--- src/utils/typography.ts
+++ src/utils/typography.ts
@@ -1,21 +1,29 @@
-// This file provides access to the 4 typography helpers from the Guardian Design System (@src-foundations/typography)
+// This file provides access to the 4 typography helpers from the Guardian Design System (@guardian/src-foundations/typography)
 // with the output of each helper extended to include the font faces expected by both pre- and post-design system platforms (i.e. Frontend vs DCR)
-// This ensures we can develop our components unaware of platform differences whilst taking advantage of convenience of the design system helper functions.
-// NOTE - meant to be used as temporary tool to abstract those differences until we find a solution on the platform side.
+// This ensures we can develop our components unaware of platform differences whilst leveraging the convenience of the helpers in the design system.
+// NOTE - this is meant to be used as temporary tool to abstract those differences until we find a solution on the platform side.
 import {
+    // The original Design System typography objects
     titlepiece as dsTitlepiece,
     headline as dsHeadline,
     body as dsBody,
     textSans as dsTextSans,
+    // Types used to compose the FontScaleArgs type
     LineHeight,
     FontWeight,
+    // The types we'll use to cast the size names that we'll turn into functions
     TitlepieceSizes,
     HeadlineSizes,
     BodySizes,
     TextSansSizes,
+    // Objects where each key is a size name (e.g. 'small' or 'medium') so we know which sizes are allowed into any typography object
+    titlepieceSizes,
+    headlineSizes,
+    bodySizes,
+    textSansSizes,
 } from '@guardian/src-foundations/typography';
 
-// Copied over from @src/foundations
+// Copied over from @guardian/src-foundations/typography
 // Defines the FontScaleArgs argument allowed into any typography helper
 type FontScaleArgs = {
     lineHeight?: LineHeight;
@@ -23,15 +31,20 @@ type FontScaleArgs = {
     italic?: boolean;
 };
 
-// Describes any of the helper functions we're mapping from @src/foundations
+// Describes any of the helper functions we're mapping from @guardian/src-foundations/typography
 interface TypographyHelperFunction {
     [key: string]: Function;
 }
 
-// Assigning TITLEPIECE sizing helper functions
+// Declaring the objects to which the helper functions will be dynamically assigned
 const titlepiece: TypographyHelperFunction = {};
-const titlepieceSizes: TitlepieceSizes[] = ['small', 'medium', 'large'];
-titlepieceSizes.forEach((size): void => {
+const headline: TypographyHelperFunction = {};
+const body: TypographyHelperFunction = {};
+const textSans: TypographyHelperFunction = {};
+
+// Assigning TITLEPIECE sizing helper functions
+Object.keys(titlepieceSizes).forEach((sizeName): void => {
+    const size = sizeName as TitlepieceSizes;
     titlepiece[size] = (fontScaleArgs: FontScaleArgs): string => `${dsTitlepiece[size](
         fontScaleArgs,
     )};
@@ -39,35 +52,24 @@ titlepieceSizes.forEach((size): void => {
 });
 
 // Assigning HEADLINE sizing helper functions
-const headline: TypographyHelperFunction = {};
-const headlineSizes: HeadlineSizes[] = [
-    'xxxsmall',
-    'xxsmall',
-    'xsmall',
-    'small',
-    'medium',
-    'large',
-    'xlarge',
-];
-headlineSizes.forEach((size): void => {
+Object.keys(headlineSizes).forEach((sizeName): void => {
+    const size = sizeName as HeadlineSizes;
     headline[size] = (fontScaleArgs: FontScaleArgs): string =>
         `${dsHeadline[size](fontScaleArgs)};
         font-family: GH Guardian Headline, Guardian Egyptian Web, Georgia, serif;`;
 });
 
 // Assigning BODY sizing helper functions
-const body: TypographyHelperFunction = {};
-const bodySizes: BodySizes[] = ['small', 'medium'];
-bodySizes.forEach((size): void => {
+Object.keys(bodySizes).forEach((sizeName): void => {
+    const size = sizeName as BodySizes;
     body[size] = (fontScaleArgs: FontScaleArgs): string =>
         `${dsBody[size](fontScaleArgs)};
         font-family: GuardianTextEgyptian, Guardian Text Egyptian Web, Georgia, serif;`;
 });
 
 // Assigning TEXT SANS sizing helper functions
-const textSans: TypographyHelperFunction = {};
-const textSansSizes: TextSansSizes[] = ['xsmall', 'small', 'medium', 'large', 'xlarge'];
-textSansSizes.forEach((size): void => {
+Object.keys(textSansSizes).forEach((sizeName): void => {
+    const size = sizeName as TextSansSizes;
     textSans[size] = (fontScaleArgs: FontScaleArgs): string =>
         `${dsTextSans[size](fontScaleArgs)};
         font-family: GuardianTextSans, Guardian Text Sans Web, Helvetica Neue, Helvetica, Arial, Lucida Grande, sans-serif;`;
