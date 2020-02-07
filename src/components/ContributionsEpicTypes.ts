061924aa67243628aed2719a036fbdb4d40ec856
Tom Wey <tom.wey@theguardian.com>
2020-02-07 11:51:54 +0000

    Validate POST /epic payload with JSON schema
    Also introduce a mechanism to re-generate the JSON schema based on
    TypeScript types.

---
+++ src/components/ContributionsEpicTypes.ts
@@ -0,0 +1,33 @@
+export type EpicLocalisation = {
+    countryCode: string;
+};
+
+export type EpicTracking = {
+    ophanPageId: string;
+    ophanComponentId: string;
+    platformId: string;
+    campaignCode: string;
+    abTestName: string;
+    abTestVariant: string;
+    referrerUrl: string;
+};
+
+export type Tag = {
+    id: string;
+    type: string;
+};
+
+export type EpicTargeting = {
+    contentType: string;
+    sectionName: string;
+    shouldHideReaderRevenue: boolean;
+    isMinuteArticle: boolean;
+    isPaidContent: boolean;
+    tags: Tag[];
+};
+
+export type EpicPayload = {
+    tracking: EpicTracking;
+    localisation: EpicLocalisation;
+    targeting: EpicTargeting;
+};
---
+++ src/scripts/generate-json-schema.ts
@@ -0,0 +1,29 @@
+import * as TJS from 'typescript-json-schema';
+import * as fs from 'fs';
+import * as path from 'path';
+
+const basePath = './src';
+
+const settings = {
+    required: true,
+};
+
+const compilerOptions = {
+    strictNullChecks: true,
+};
+
+const program = TJS.getProgramFromFiles(
+    [path.resolve('./src/components/ContributionsEpicTypes.ts')],
+    compilerOptions,
+    basePath,
+);
+
+const schema = TJS.generateSchema(program, 'EpicPayload', settings);
+
+const schemaPath = path.join(__dirname, '../schemas/epicPayload.schema.json');
+
+fs.writeFile(schemaPath, JSON.stringify(schema, null, 4), 'utf8', err => {
+    if (err) {
+        console.log(err);
+    }
+});
