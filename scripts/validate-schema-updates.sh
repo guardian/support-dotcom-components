4206e3575e3647b01a532db2a05c167ff3b77b72
Andre Silva <andre.silva.freelancer@guardian.co.uk>
2020-02-12 10:55:37 +0000

    Make echos pretty again

--- scripts/validate-schema-updates.sh
+++ scripts/validate-schema-updates.sh
@@ -2,7 +2,10 @@ differences=$(git diff --name-only src/schemas/)
 
 if [ "$differences" != "" ]
 then
+        tput setab 7;
+        tput setab 0;
         echo "  ${differences}"
+        tput setab 0;
         tput setaf 1;
         echo "One or move JSON Schemas have been updated. Please review and commit the changes."
         exit 1
