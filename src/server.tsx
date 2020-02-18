03386ddf7deac82e14f83664752434763136f06f
Nicolas Long <nicolas.long@theguardian.com>
2020-02-18 18:02:30 +0000

    Log and ignore shouldNotRenderEpic for now

--- src/server.tsx
+++ src/server.tsx
@@ -66,10 +66,11 @@ const buildEpic = async (
         highlighted,
     };
 
-    // Determine whether to render the Epic or return empty HTML and CSS
+    // Only log for now - as Frontend does this and we may have bugs
+    // We'll need to do more than log when we start our DCR test though
     if (shouldNotRenderEpic(targeting)) {
         console.log(`Did not render for targeting data: ${JSON.stringify(targeting)}`);
-        return null;
+        // return null; TODO handle more explicitly/cleanly
     }
 
     const { html, css } = extractCritical(
