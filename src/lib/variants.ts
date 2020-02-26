d3287996b1cb4abd096abdffe07b2c4a401455e1
Nicolas Long <nicolas.long@theguardian.com>
2020-02-26 17:27:34 +0000

    Add some initial filters and test

--- src/lib/variants.ts
+++ src/lib/variants.ts
@@ -12,11 +12,6 @@ interface MaxViews {
     minDaysBetweenViews: number;
 }
 
-enum UserCohort {
-    AllExistingSupporters = 'AllExistingSupporters',
-    AllNonSupporters = 'AllNonSupporters',
-}
-
 interface Cta {
     text: string;
     baseUrl: string;
@@ -44,23 +39,51 @@ interface Test {
     excludedSections: string[];
     alwaysAsk: boolean;
     maxViews?: MaxViews;
-    userCohort: UserCohort;
+    userCohort: string;
     isLiveBlog: boolean;
     hasCountryName: boolean;
     variants: Variant[];
     highPriority: boolean;
     useLocalViewLog: boolean;
     articlesViewedSettings?: ArticlesViewedSettings;
+
+    // TODO check with Contributions Team as not in current examples so perhaps
+    // they are always 0 and 1?
+    audience: number;
+    audienceOffset?: number;
 }
 
 export interface EpicTests {
     tests: Test[];
 }
 
-export const select = (metadata: EpicTargeting): Variant => {
+interface Filter {
+    id: string;
+    test: (test: Test, targeting: EpicTargeting) => boolean;
+}
+
+// https://github.com/guardian/frontend/blob/master/static/src/javascripts/projects/common/modules/experiments/ab-core.js#L39
+const selectVariant = (test: Test, mvtId: number): Variant | undefined => {
+    // Calculate range of mvtIDs for variants and return first match
+    const lowest = mvtId * (test.audienceOffset || 0);
+    const highest = lowest + mvtId * test.audience;
+
+    if (mvtId > lowest && mvtId <= highest) {
+        return test.variants[mvtId % test.variants.length];
+    }
+
+    return undefined;
+};
+
+export const findVariant = (
+    data: EpicTests,
+    targeting: EpicTargeting,
+    mvtId: number,
+): Variant | undefined => {
     // Also need to include canRun of individual variants (only relevant for
     // manually configured tests).
 
+    // TODO include max view throttling here too
     // https://github.com/guardian/frontend/blob/master/static/src/javascripts/projects/common/modules/commercial/contributions-utilities.js#L378
     /* return (
         (!initVariant.canRun || initVariant.canRun()) &&
@@ -72,5 +95,49 @@ export const select = (metadata: EpicTargeting): Variant => {
         copyIsValid()
     ); */
 
-    return undefined;
+    const hasTags: Filter = {
+        id: 'hasTags',
+        test: (test, targeting) => {
+            if (test.tagIds.length < 1) {
+                return true;
+            }
+
+            const intersection = test.tagIds.filter(tagId =>
+                targeting.tags.map(tag => tag.id).includes(tagId),
+            );
+
+            return intersection.length > 0;
+        },
+    };
+
+    const hasSection: Filter = {
+        id: 'hasSection',
+        test: (test, targeting) => {
+            if (test.sections.length < 1) {
+                return true;
+            }
+
+            return test.sections.includes(targeting.sectionName);
+        },
+    };
+
+    const userInTest: Filter = {
+        id: 'userInTest',
+        test: (test, _) => selectVariant(test, mvtId) !== undefined,
+    };
+
+    const filters: Filter[] = [hasSection, hasTags, userInTest];
+    const test = data.tests.find(test =>
+        filters.every(filter => {
+            const got = filter.test(test, targeting);
+
+            if (!got && process.env.NODE_ENV !== 'production') {
+                console.log(`filter failed: ${test.name}; ${filter.id}`);
+            }
+
+            return got;
+        }),
+    );
+
+    return test ? selectVariant(test, mvtId) : undefined;
 };
