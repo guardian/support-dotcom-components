e9c2a89e2b9ee2f8271cd2e7daa3221416ece58c
Nicolas Long <nicolas.long@theguardian.com>
2020-02-26 15:03:08 +0000

    Add skeleton tests

---
+++ src/lib/variants.test.ts
@@ -0,0 +1,7 @@
+describe('select variants', () => {
+    it.skip('should filter by max views conditions', () => {});
+    it.skip('should filter by country group match', () => {});
+    it.skip('should filter by required tags or sections', () => {});
+    it.skip('should filter by excluded tags or sections', () => {});
+    it.skip('should filter by copy is valid', () => {});
+});
