c6373f86adb87b685c2bf11051f2af1b62c876cc
Tom Wey <tom.wey@theguardian.com>
2020-02-27 09:17:33 +0000

    Don’t cache a failed epic content fetch

--- src/api/contributionsApi.test.ts
+++ src/api/contributionsApi.test.ts
@@ -1,32 +1,38 @@
-import { fetchDefaultEpicContent } from './contributionsApi';
+import { fetchDefaultEpicContent, clearCachedEpic } from './contributionsApi';
 
 jest.mock('node-fetch', () => require('fetch-mock').sandbox());
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const fetchMock = require('node-fetch');
+fetchMock.config.overwriteRoutes = true;
+
+const epicUrl =
+    'https://interactive.guim.co.uk/docsdata/1fy0JolB1bf1IEFLHGHfUYWx-niad7vR9K954OpTOvjE.json';
+
+const epicResponse = {
+    sheets: {
+        control: [
+            {
+                heading: 'Since you’re here...',
+                paragraphs: 'First paragraph',
+                highlightedText: 'Highlighted Text',
+            },
+            {
+                heading: '',
+                paragraphs: 'Second paragraph',
+                highlightedText: '',
+            },
+        ],
+    },
+};
+
+beforeEach(() => {
+    clearCachedEpic();
+    fetchMock.resetHistory();
+});
 
 describe('fetchDefaultEpic', () => {
     it('fetches and returns the data in the expected format', async () => {
-        const url =
-            'https://interactive.guim.co.uk/docsdata/1fy0JolB1bf1IEFLHGHfUYWx-niad7vR9K954OpTOvjE.json';
-
-        const response = {
-            sheets: {
-                control: [
-                    {
-                        heading: 'Since you’re here...',
-                        paragraphs: 'First paragraph',
-                        highlightedText: 'Highlighted Text',
-                    },
-                    {
-                        heading: '',
-                        paragraphs: 'Second paragraph',
-                        highlightedText: '',
-                    },
-                ],
-            },
-        };
-
-        fetchMock.get(url, response);
+        fetchMock.get(epicUrl, epicResponse);
 
         const epicData = await fetchDefaultEpicContent();
 
@@ -35,5 +41,27 @@ describe('fetchDefaultEpic', () => {
             paragraphs: ['First paragraph', 'Second paragraph'],
             highlighted: ['Highlighted Text'],
         });
+    });
+
+    it('caches successful epic fetches', async () => {
+        fetchMock.get(epicUrl, epicResponse);
+
+        await fetchDefaultEpicContent();
+        await fetchDefaultEpicContent();
+
+        expect(fetchMock.calls().length).toEqual(1);
+    });
+
+    it('does not cache unsuccessful epic fetches', async () => {
+        fetchMock.get(epicUrl, { status: 500 });
+
+        try {
+            await fetchDefaultEpicContent();
+        } catch {}
+        try {
+            await fetchDefaultEpicContent();
+        } catch {}
+
+        expect(fetchMock.calls().length).toEqual(2);
     });
 });
