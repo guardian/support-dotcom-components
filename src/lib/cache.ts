ba910d87209ab749b6b83282150ad668732ac0d4
Nicolas Long <nicolas.long@theguardian.com>
2020-03-06 16:45:20 +0000

    Document cache function

--- src/lib/cache.ts
+++ src/lib/cache.ts
@@ -1,5 +1,9 @@
 import NodeCache from 'node-cache';
 
+// Note, this is heavyweight implementation as it constructs a new cache for
+// each invocation. The assumption is that we'll only ever cache a few values;
+// if we end up wanting to cache lots of things, we should refactor to *share*
+// caches.
 export const cacheAsync = <T>(
     fn: () => Promise<T>,
     ttlSec: number,
