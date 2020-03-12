c539ff2b4daee7b2054db5ce107a9fd644fd2b1a
Andre Silva <andre.silva.freelancer@guardian.co.uk>
2020-03-12 13:03:05 +0000

    Returning null when targeting checks fail

--- src/server.tsx
+++ src/server.tsx
@@ -75,10 +75,10 @@ const buildEpic = async (
         highlighted,
     };
 
-    // Only log for now - as Frontend does this and we may have bugs
-    // We'll need to do more than log when we start our DCR test though
+    // Don't render the Epic if our targeting checks fail
     if (shouldNotRenderEpic(targeting)) {
-        console.log(`Should not render for targeting data: ${JSON.stringify(targeting)}`);
+        // console.log(`Should not render for targeting data: ${JSON.stringify(targeting)}`);
+        return null;
     }
 
     const { html, css } = extractCritical(
