66ebc71f7afbaf989c7005b1edaae87e189e065f
Andre Silva <andre.silva.freelancer@guardian.co.uk>
2020-03-19 20:28:47 +0000

    DRY Epic stories

--- src/components/ContributionsEpic.stories.tsx
+++ src/components/ContributionsEpic.stories.tsx
@@ -12,93 +12,63 @@ export default {
     decorators: [withKnobs],
 };
 
-export const defaultStory = (): ReactElement => {
-    // Epic content props
-    const variant: Variant = {
-        name: 'Test Epic',
-        heading: text('heading', testData.content.heading),
-        paragraphs: object('paragraphs', testData.content.paragraphs),
-        highlightedText: text('highlightedText', testData.content.highlightedText),
-        showTicker: false,
-    };
-
-    // Epic metadata props
-    const epicTracking = {
-        ophanPageId: text('ophanPageId', testData.tracking.ophanPageId),
-        ophanComponentId: text('ophanComponentId', testData.tracking.ophanComponentId),
-        platformId: text('platformId', testData.tracking.platformId),
-        campaignCode: text('campaignCode', testData.tracking.campaignCode),
-        abTestName: text('abTestName', testData.tracking.abTestName),
-        abTestVariant: text('abTestVariant', testData.tracking.abTestVariant),
-        referrerUrl: text('referrerUrl', testData.tracking.referrerUrl),
-    };
-
-    // Epic localisation props
-    const epicLocalisation = {
-        countryCode: text('countryCode', testData.localisation.countryCode || 'GB'),
-    };
-
-    // Number of articles viewed
-    // Used to replace the template placeholder
-    const periodInWeeks = 52;
-    const numArticles = getArticleViewCountForWeeks(
-        testData.targeting.weeklyArticleHistory,
-        periodInWeeks,
-    );
-
-    return (
-        <StorybookWrapper>
-            <ContributionsEpic
-                variant={variant}
-                tracking={epicTracking}
-                localisation={epicLocalisation}
-                numArticles={numArticles}
-            />
-        </StorybookWrapper>
-    );
+// Epic content props
+const variant: Variant = {
+    name: 'Test Epic',
+    heading: text('heading', testData.content.heading),
+    paragraphs: object('paragraphs', testData.content.paragraphs),
+    highlightedText: text('highlightedText', testData.content.highlightedText),
+    showTicker: false,
 };
+
+// Epic metadata props
+const epicTracking = {
+    ophanPageId: text('ophanPageId', testData.tracking.ophanPageId),
+    ophanComponentId: text('ophanComponentId', testData.tracking.ophanComponentId),
+    platformId: text('platformId', testData.tracking.platformId),
+    campaignCode: text('campaignCode', testData.tracking.campaignCode),
+    abTestName: text('abTestName', testData.tracking.abTestName),
+    abTestVariant: text('abTestVariant', testData.tracking.abTestVariant),
+    referrerUrl: text('referrerUrl', testData.tracking.referrerUrl),
+};
+
+// Epic localisation props
+const epicLocalisation = {
+    countryCode: text('countryCode', testData.localisation.countryCode || 'GB'),
+};
+
+// Number of articles viewed
+// Used to replace the template placeholder
+const periodInWeeks = 52;
+const numArticles = getArticleViewCountForWeeks(
+    testData.targeting.weeklyArticleHistory,
+    periodInWeeks,
+);
+
+export const defaultStory = (): ReactElement => (
+    <StorybookWrapper>
+        <ContributionsEpic
+            variant={variant}
+            tracking={epicTracking}
+            localisation={epicLocalisation}
+            numArticles={numArticles}
+        />
+    </StorybookWrapper>
+);
 
 defaultStory.story = { name: 'Default epic' };
 
 export const imageStory = (): ReactElement => {
-    // Epic content props
-    const variant: Variant = {
-        name: 'Test Epic',
-        heading: text('heading', testData.content.heading),
-        paragraphs: object('paragraphs', testData.content.paragraphs),
-        highlightedText: text('highlightedText', testData.content.highlightedText),
-        showTicker: false,
+    // Add background image to Epic variant
+    const variantWithImage: Variant = {
+        ...variant,
         backgroundImageUrl: text('backgroundImageUrl', testData.content.backgroundImageUrl),
     };
-
-    // Epic metadata props
-    const epicTracking = {
-        ophanPageId: text('ophanPageId', testData.tracking.ophanPageId),
-        ophanComponentId: text('ophanComponentId', testData.tracking.ophanComponentId),
-        platformId: text('platformId', testData.tracking.platformId),
-        campaignCode: text('campaignCode', testData.tracking.campaignCode),
-        abTestName: text('abTestName', testData.tracking.abTestName),
-        abTestVariant: text('abTestVariant', testData.tracking.abTestVariant),
-        referrerUrl: text('referrerUrl', testData.tracking.referrerUrl),
-    };
-
-    // Epic localisation props
-    const epicLocalisation = {
-        countryCode: text('countryCode', testData.localisation.countryCode || 'GB'),
-    };
-
-    // Number of articles viewed
-    // Used to replace the template placeholder
-    const periodInWeeks = 52;
-    const numArticles = getArticleViewCountForWeeks(
-        testData.targeting.weeklyArticleHistory,
-        periodInWeeks,
-    );
 
     return (
         <StorybookWrapper>
             <ContributionsEpic
-                variant={variant}
+                variant={variantWithImage}
                 tracking={epicTracking}
                 localisation={epicLocalisation}
                 numArticles={numArticles}
