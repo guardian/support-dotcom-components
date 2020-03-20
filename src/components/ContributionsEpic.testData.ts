5bbfee743a325f5c52b4c7f0e14ff2ebfcadfe6a
Andre Silva <andre.silva.freelancer@guardian.co.uk>
2020-03-20 12:07:05 +0000

    Adds support for Epic images

--- src/components/ContributionsEpic.testData.ts
+++ src/components/ContributionsEpic.testData.ts
@@ -10,6 +10,8 @@ const content = {
     ],
     highlightedText:
         'Support The Guardian from as little as %%CURRENCY_SYMBOL%%1 - and it only takes a minute. Thank you.',
+    backgroundImageUrl:
+        'https://images.unsplash.com/photo-1494256997604-768d1f608cac?ixlib=rb-1.2.1&auto=format&fit=crop&w=1701&q=80',
 };
 
 const tracking: EpicTracking = {
