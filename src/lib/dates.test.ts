fcd405cdc8c4f1b001fbaa527ed02755d4e77359
Andre Silva <andre.silva.freelancer@guardian.co.uk>
2020-03-31 13:32:39 +0100

    Tweak

--- src/lib/dates.test.ts
+++ src/lib/dates.test.ts
@@ -1,10 +1,10 @@
 import { daysSince, isRecentOneOffContributor } from '../lib/dates';
 
 describe('daysSince', () => {
+    const now = new Date('2020-02-18T10:30:00');
     it('should count the days properly', () => {
-        const then = new Date('2020-02-14T10:30:00');
-        const now = new Date('2020-02-18T10:30:00');
-        expect(daysSince(then, now)).toBe(4);
+        const then = new Date('2020-02-14T13:30:00');
+        expect(daysSince(then, now)).toBe(3);
     });
 });
 
