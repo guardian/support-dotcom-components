774e4708ef849fa7380482c59c326f30c18a44a9
Tom Wey <tom.wey@theguardian.com>
2020-04-14 09:55:21 +0100

    Refactor custom middlewares out of server

---
+++ src/middleware/errorHandling.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import { ValidationError } from '../errors/validationError';
+
+export const errorHandling = (
+    error: Error,
+    req: express.Request,
+    res: express.Response,
+    // Error handling middleware in Express needs to take 4 arguments in the handler
+    // for it to run when `next()` function is called in the route handler
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    next: express.NextFunction,
+): void => {
+    const { message } = error;
+
+    switch (error.constructor) {
+        case ValidationError:
+            res.status(400).send({ error: message });
+            break;
+        default:
+            res.status(500).send({ error: message });
+    }
+
+    console.log('Something went wrong: ', message);
+};
