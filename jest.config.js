aeb8999eb5647815c67b650aea4760b98d4740d2
Simon Adcock <simonadcock2@gmail.com>
2020-04-15 20:16:03 +0100

    point jest at cjs modules for source

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
+    moduleNameMapper: {
+        '^@guardian/src-foundations/(.*)$': '@guardian/src-foundations/$1/cjs',
+    },
 };
