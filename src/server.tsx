c22957d274b9617a625bc18507897a6b8f30d62c
nic.long <nicolaslong@gmail.com>
2020-05-06 11:59:08 +0100

    Revert "Revert "Fix lint warnings""
    This reverts commit 9a2443d7acf8f38e20b6fd6a7c22c3a2124e8ccc.

--- src/server.tsx
+++ src/server.tsx
@@ -128,75 +128,72 @@ app.post(
 );
 
 // TODO remove once migration complete
-app.post(
-    '/epic/compare-variant-decision',
-    async (req: express.Request, res: express.Response, next: express.NextFunction) => {
-        if (process.env.LOG_COMPARE_VARIANTS !== 'true') {
-            res.send('ignoring');
-            return;
-        }
+app.post('/epic/compare-variant-decision', async (req: express.Request, res: express.Response) => {
+    if (process.env.LOG_COMPARE_VARIANTS !== 'true') {
+        res.send('ignoring');
+        return;
+    }
 
-        const {
-            targeting,
-            expectedTest,
-            expectedVariant,
-            expectedCampaignCode,
-            expectedCampaignId,
-            frontendLog,
-        } = req.body;
+    const {
+        targeting,
+        expectedTest,
+        expectedVariant,
+        expectedCampaignCode,
+        expectedCampaignId,
+        frontendLog,
+    } = req.body;
 
-        // Ignore some manually defined tests in Frontend for now
-        const ignores = [
-            'FrontendDotcomRenderingEpic',
-            'RemoteRenderEpicRoundTwo',
-            'ContributionsEpicPrecontributionReminderRoundTwo',
-        ];
+    // Ignore some manually defined tests in Frontend for now
+    const ignores = [
+        'FrontendDotcomRenderingEpic',
+        'RemoteRenderEpicRoundTwo',
+        'ContributionsEpicPrecontributionReminderRoundTwo',
+    ];
 
-        if (ignores.includes(expectedTest)) {
-            res.send('ignoring');
-            return;
-        }
+    if (ignores.includes(expectedTest)) {
+        res.send('ignoring');
+        return;
+    }
 
-        // We need these to satisfy the buildDynamicEpic interface, but it
-        // doesn't matter what values we use
-        const sampleTracking = {
-            ophanPageId: 'xxxxxxxxxxxxx',
-            ophanComponentId: 'ACQUISITIONS_EPIC',
-            platformId: 'GUARDIAN_WEB',
-            clientName: 'xxx',
-            referrerUrl: 'https://theguardian.com',
-        };
+    // We need these to satisfy the buildDynamicEpic interface, but it
+    // doesn't matter what values we use
+    const sampleTracking = {
+        ophanPageId: 'xxxxxxxxxxxxx',
+        ophanComponentId: 'ACQUISITIONS_EPIC',
+        platformId: 'GUARDIAN_WEB',
+        clientName: 'xxx',
+        referrerUrl: 'https://theguardian.com',
+    };
 
-        const got = await buildEpic(sampleTracking, targeting);
+    const got = await buildEpic(sampleTracking, targeting);
 
-        const notBothFalsy = expectedTest || got;
-        const gotTestName = got?.meta?.abTestName;
-        const gotVariantName = got?.meta?.abTestVariant;
-        const gotCampaignCode = got?.meta?.campaignCode;
-        const gotCampaignId = got?.meta?.campaignId;
+    const notBothFalsy = expectedTest || got;
+    const gotTestName = got?.meta?.abTestName;
+    const gotVariantName = got?.meta?.abTestVariant;
+    const gotCampaignCode = got?.meta?.campaignCode;
+    const gotCampaignId = got?.meta?.campaignId;
 
-        const notTheSame =
-            gotTestName !== expectedTest ||
-            gotVariantName !== expectedVariant ||
-            gotCampaignCode !== expectedCampaignCode ||
-            gotCampaignId !== expectedCampaignId;
+    const notTheSame =
+        gotTestName !== expectedTest ||
+        gotVariantName !== expectedVariant ||
+        gotCampaignCode !== expectedCampaignCode ||
+        gotCampaignId !== expectedCampaignId;
 
-        if (notBothFalsy && notTheSame) {
-            console.log(
-                'comparison failed with data: ' +
-                    JSON.stringify({
-                        status: 'comparison failed',
-                        got: `${gotTestName}:${gotVariantName}:${gotCampaignCode}:${gotCampaignId}`,
-                        want: `${expectedTest}:${expectedVariant}:${expectedCampaignCode}:${expectedCampaignId}`,
-                        targeting,
-                        frontendLog,
-                    }),
-            );
-        }
+    if (notBothFalsy && notTheSame) {
+        console.log(
+            'comparison failed with data: ' +
+                JSON.stringify({
+                    status: 'comparison failed',
+                    got: `${gotTestName}:${gotVariantName}:${gotCampaignCode}:${gotCampaignId}`,
+                    want: `${expectedTest}:${expectedVariant}:${expectedCampaignCode}:${expectedCampaignId}`,
+                    targeting,
+                    frontendLog,
+                }),
+        );
+    }
 
-        res.send('thanks');
-    },
-);
+    res.send('thanks');
+});
 
 app.use(errorHandlingMiddleware);
 
