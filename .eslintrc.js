7eecde36aa9fbcfb6f0dc27f8883aeed5726f87c
nic.long <nicolaslong@gmail.com>
2020-05-06 11:12:45 +0100

    Revert "Allow inferrable types for parameters (fixes error)"
    This reverts commit 2f92a3c35769582512ea1e8705a13dd39cc06a5d.

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,12 +16,6 @@ module.exports = {
         // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
         // e.g. "@typescript-eslint/explicit-function-return-type": "off",
         curly: 2,
-        '@typescript-eslint/no-inferrable-types': [
-            'error',
-            {
-                ignoreParameters: true,
-            },
-        ],
     },
     settings: {
         react: {
