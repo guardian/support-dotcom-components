aaf138a781e6cd3e00a9f70064a9e59f01b21107
Lucy Monie Hall <lucy.monie.hall@gmail.com>
2020-06-15 16:12:37 +0100

    Move close state into banner body

--- src/components/SubscriptionBanner.stories.tsx
+++ src/components/SubscriptionBanner.stories.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useState } from 'react';
+import React, { ReactElement } from 'react';
 import { SubscriptionsBanner } from './modules/subscriptionsBanner/SubscriptionsBanner';
 import { withKnobs } from '@storybook/addon-knobs';
 import { StorybookWrapper } from '../utils/StorybookWrapper';
@@ -10,18 +10,9 @@ export default {
 };
 
 export const defaultStory = (): ReactElement => {
-    const [showBanner, toggleBanner] = useState(true);
     return (
         <StorybookWrapper>
-            <>
-                {showBanner && (
-                    <SubscriptionsBanner
-                        subscriptionUrl="/"
-                        signInUrl="/"
-                        closeBanner={(): void => toggleBanner(false)}
-                    />
-                )}
-            </>
+            <SubscriptionsBanner subscriptionUrl="/" signInUrl="/" />
         </StorybookWrapper>
     );
 };
--- src/components/modules/subscriptionsBanner/SubscriptionsBanner.tsx
+++ src/components/modules/subscriptionsBanner/SubscriptionsBanner.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { css } from 'emotion';
 import { ThemeProvider } from 'emotion-theming';
 import { body, headline, textSans } from '@guardian/src-foundations/typography/cjs';
@@ -239,89 +239,95 @@ const closeButton = css`
 type SubscriptionsBannerProps = {
     subscriptionUrl: string;
     signInUrl: string;
-    closeBanner: () => void;
 };
 
 export const SubscriptionsBanner: React.FC<SubscriptionsBannerProps> = ({
     subscriptionUrl,
     signInUrl,
-    closeBanner,
 }: SubscriptionsBannerProps) => {
+    const [showBanner, closeBanner] = useState(true);
     return (
-        <section
-            id="js-site-message--subscription-banner"
-            className={banner}
-            data-target="subscriptions-banner"
-        >
-            <div className={contentContainer}>
-                <div className={topLeftComponent}>
-                    <h3 className={heading}>
-                        The world is changing by the minute. <br className={headLineBreak} />
-                        Keep up with a digital subscription.
-                    </h3>
-                    <p className={paragraph}>
-                        Two Guardian apps, with you every day. <strong>The Daily</strong>, joining
-                        you in the morning to share politics, culture, food and opinion.{' '}
-                        <strong>Live</strong>, constantly by your side, keeping you connected with
-                        the outside world.
-                    </p>
-                    <ThemeProvider theme={buttonReaderRevenue}>
-                        <LinkButton
-                            id="js-site-message--subscription-banner__cta"
-                            data-link-name="subscription-banner : cta"
-                            priority="primary"
-                            size="default"
-                            href={subscriptionUrl}
-                        >
-                            <span className={buttonTextDesktop}>Become a digital subscriber</span>
-                            <span className={buttonTextTablet}>Become a subscriber</span>
-                            <span className={buttonTextMobile}>Subscribe now</span>
-                        </LinkButton>
-                    </ThemeProvider>
-                    <ThemeProvider theme={brand}>
-                        <Button
-                            id="js-site-message--subscription-banner__cta-dismiss"
-                            data-link-name="subscription-banner : not now"
-                            onClick={closeBanner}
-                            priority="subdued"
-                        >
-                            Not now
-                        </Button>
-                    </ThemeProvider>
-                    <div className={siteMessage}>
-                        Already a subscriber?{' '}
-                        <a
-                            id="js-site-message--subscription-banner__sign-in"
-                            href={signInUrl}
-                            data-link-name="subscription-banner : sign in"
-                        >
-                            Sign in
-                        </a>{' '}
-                        to not see this again
-                    </div>
-                </div>
-                <div className={bottomRightComponent}>
-                    <img
-                        className={packShot}
-                        src="https://i.guim.co.uk/img/media/773ead1bd414781052c0983858e6859993870dd3/34_72_1825_1084/1825.png?width=500&quality=85&s=24cb49b459c52c9d25868ca20979defb"
-                        alt=""
-                    />
-                    <div className={iconPanel}>
-                        <button
-                            onClick={closeBanner}
-                            className={closeButton}
-                            id="js-site-message--subscription-banner__close-button"
-                            data-link-name="subscription-banner : close"
-                            aria-label="Close"
-                        >
-                            <Close />
-                        </button>
-                        <div className={logoContainer}>
-                            <Logo />
+        <>
+            {showBanner ? (
+                <section
+                    id="js-site-message--subscription-banner"
+                    className={banner}
+                    data-target="subscriptions-banner"
+                >
+                    <div className={contentContainer}>
+                        <div className={topLeftComponent}>
+                            <h3 className={heading}>
+                                The world is changing by the minute.{' '}
+                                <br className={headLineBreak} />
+                                Keep up with a digital subscription.
+                            </h3>
+                            <p className={paragraph}>
+                                Two Guardian apps, with you every day. <strong>The Daily</strong>,
+                                joining you in the morning to share politics, culture, food and
+                                opinion. <strong>Live</strong>, constantly by your side, keeping you
+                                connected with the outside world.
+                            </p>
+                            <ThemeProvider theme={buttonReaderRevenue}>
+                                <LinkButton
+                                    id="js-site-message--subscription-banner__cta"
+                                    data-link-name="subscription-banner : cta"
+                                    priority="primary"
+                                    size="default"
+                                    href={subscriptionUrl}
+                                >
+                                    <span className={buttonTextDesktop}>
+                                        Become a digital subscriber
+                                    </span>
+                                    <span className={buttonTextTablet}>Become a subscriber</span>
+                                    <span className={buttonTextMobile}>Subscribe now</span>
+                                </LinkButton>
+                            </ThemeProvider>
+                            <ThemeProvider theme={brand}>
+                                <Button
+                                    id="js-site-message--subscription-banner__cta-dismiss"
+                                    data-link-name="subscription-banner : not now"
+                                    onClick={(): void => closeBanner(false)}
+                                    priority="subdued"
+                                >
+                                    Not now
+                                </Button>
+                            </ThemeProvider>
+                            <div className={siteMessage}>
+                                Already a subscriber?{' '}
+                                <a
+                                    id="js-site-message--subscription-banner__sign-in"
+                                    href={signInUrl}
+                                    data-link-name="subscription-banner : sign in"
+                                >
+                                    Sign in
+                                </a>{' '}
+                                to not see this again
+                            </div>
+                        </div>
+                        <div className={bottomRightComponent}>
+                            <img
+                                className={packShot}
+                                src="https://i.guim.co.uk/img/media/773ead1bd414781052c0983858e6859993870dd3/34_72_1825_1084/1825.png?width=500&quality=85&s=24cb49b459c52c9d25868ca20979defb"
+                                alt=""
+                            />
+                            <div className={iconPanel}>
+                                <button
+                                    onClick={(): void => closeBanner(false)}
+                                    className={closeButton}
+                                    id="js-site-message--subscription-banner__close-button"
+                                    data-link-name="subscription-banner : close"
+                                    aria-label="Close"
+                                >
+                                    <Close />
+                                </button>
+                                <div className={logoContainer}>
+                                    <Logo />
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>
-        </section>
+                </section>
+            ) : null}
+        </>
     );
 };
