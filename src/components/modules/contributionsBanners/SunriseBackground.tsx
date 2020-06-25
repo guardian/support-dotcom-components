6b7b05b8baf1e7e0e6af5046b33b8e3dec7d98f7
Tom Pretty <tompretty2@gmail.com>
2020-06-25 10:00:35 +0100

    Add pulse animation on goal reached

--- src/components/modules/contributionsBanners/SunriseBackground.tsx
+++ src/components/modules/contributionsBanners/SunriseBackground.tsx
@@ -52,9 +52,50 @@ const innerCircleGoalNotReachedAnimation = (
     `;
 };
 
-const innnerCircleMobile = (percentage: number): string => {
+const innerCircleGoalReachedAnimation = (
+    startFill: number,
+    finalFill: number,
+    nameSuffix: string,
+): string => {
     return css`
-        ${innerCircle}
+        clip-path: circle(${finalFill}%);
+        @keyframes grow-reached-${nameSuffix} {
+            0% {
+                clip-path: circle(${startFill}%);
+            }
+            100% {
+                clip-path: circle(${finalFill}%);
+            }
+        }
+        @keyframes pulse-${nameSuffix} {
+            0% {
+                clip-path: circle(${finalFill}%);
+            }
+            100% {
+                clip-path: circle(48%);
+            }
+        }
+
+        animation-name: grow-reached-${nameSuffix}, pulse-${nameSuffix};
+        animation-duration: 2s, 2s;
+        animation-delay: 0s, 2.2s;
+        animation-timing-function: ease, ease-in-out;
+        animation-iteration-count: 1, infinite;
+        animation-direction: normal, alternate;
+    `;
+};
+
+const innerCircleMobile = css`
+    ${innerCircle}
+
+    ${from.tablet} {
+        display: none;
+    }
+`;
+
+const innerCircleMobileGoalNotReached = (percentage: number): string => {
+    return css`
+        ${innerCircleMobile}
 
         ${innerCircleGoalNotReachedAnimation(
             INNER_CIRCLE_MOBILE_START_FILL,
@@ -62,16 +103,36 @@ const innnerCircleMobile = (percentage: number): string => {
             percentage,
             'mobile',
         )}
-
-        ${from.tablet} {
-            display: none;
-        }
     `;
 };
 
-const innnerCircleTablet = (percentage: number): string => {
+const innerCircleMobileGoalReached = css`
+    ${innerCircleMobile}
+
+    ${innerCircleGoalReachedAnimation(
+        INNER_CIRCLE_MOBILE_START_FILL,
+        INNER_CIRCLE_FINAL_FILL,
+        'mobile',
+    )}
+`;
+
+const innerCircleTablet = css`
+    ${innerCircle}
+
+    display: none;
+
+    ${from.tablet} {
+        display: block;
+    }
+
+    ${from.desktop} {
+        display: none;
+    }
+`;
+
+const innerCircleTabletGoalNotReached = (percentage: number): string => {
     return css`
-        ${innerCircle}
+        ${innerCircleTablet}
 
         ${innerCircleGoalNotReachedAnimation(
             INNER_CIRCLE_TABLET_START_FILL,
@@ -79,22 +140,36 @@ const innnerCircleTablet = (percentage: number): string => {
             percentage,
             'tablet',
         )}
-
-        display: none;
-
-        ${from.tablet} {
-            display: block;
-        }
-
-        ${from.desktop} {
-            display: none;
-        }
     `;
 };
 
-const innnerCircleDesktop = (percentage: number): string => {
+const innerCircleTabletGoalReached = css`
+    ${innerCircleTablet}
+
+    ${innerCircleGoalReachedAnimation(
+        INNER_CIRCLE_TABLET_START_FILL,
+        INNER_CIRCLE_FINAL_FILL,
+        'tablet',
+    )}
+`;
+
+const innnerCircleDesktop = css`
+    ${innerCircle}
+
+    display: none;
+
+    ${from.desktop} {
+        display: block;
+    }
+
+    ${from.wide} {
+        display: none;
+    }
+`;
+
+const innerCircleDesktopGoalNotReached = (percentage: number): string => {
     return css`
-        ${innerCircle}
+        ${innnerCircleDesktop}
 
         ${innerCircleGoalNotReachedAnimation(
             INNER_CIRCLE_DESKTOP_START_FILL,
@@ -102,22 +177,32 @@ const innnerCircleDesktop = (percentage: number): string => {
             percentage,
             'desktop',
         )}
-
-        display: none;
-
-        ${from.desktop} {
-            display: block;
-        }
-
-        ${from.wide} {
-            display: none;
-        }
     `;
 };
 
-const innnerCircleWide = (percentage: number): string => {
+const innerCircleDesktopGoalReached = css`
+    ${innnerCircleDesktop}
+
+    ${innerCircleGoalReachedAnimation(
+        INNER_CIRCLE_DESKTOP_START_FILL,
+        INNER_CIRCLE_FINAL_FILL,
+        'desktop',
+    )}
+`;
+
+const innerCircleWide = css`
+    ${innerCircle}
+
+    display: none;
+
+    ${from.desktop} {
+        display: block;
+    }
+`;
+
+const innerCircleWideGoalNotReached = (percentage: number): string => {
     return css`
-        ${innerCircle}
+        ${innerCircleWide}
 
         ${innerCircleGoalNotReachedAnimation(
             INNER_CIRCLE_WIDE_START_FILL,
@@ -125,14 +210,18 @@ const innnerCircleWide = (percentage: number): string => {
             percentage,
             'wide',
         )}
-
-        display: none;
-
-        ${from.desktop} {
-            display: block;
-        }
     `;
 };
+
+const innerCircleWideGoalReached = css`
+    ${innerCircleWide}
+
+    ${innerCircleGoalReachedAnimation(
+        INNER_CIRCLE_WIDE_START_FILL,
+        INNER_CIRCLE_FINAL_FILL,
+        'wide',
+    )}
+`;
 
 const outerCircleMobile = css`
     color: ${brandAlt[200]};
@@ -184,13 +273,18 @@ export interface SunriseBackgroundProps {
 const SunriseBackground: React.FC<SunriseBackgroundProps> = ({
     percentage,
 }: SunriseBackgroundProps) => {
+    const isGoalReached = percentage >= 1.0;
     return (
         <div className={svgContainer}>
             <svg className={svg} viewBox="0 0 1300 230">
                 {/* wide */}
                 <circle className={outerCircleWide} cx="50%" cy="90%" r="45%" fill="currentColor" />
                 <circle
-                    className={innnerCircleWide(percentage)}
+                    className={
+                        isGoalReached
+                            ? innerCircleWideGoalReached
+                            : innerCircleWideGoalNotReached(percentage)
+                    }
                     cx="50%"
                     cy="90%"
                     r="45%"
@@ -205,7 +299,11 @@ const SunriseBackground: React.FC<SunriseBackgroundProps> = ({
                     fill="currentColor"
                 />
                 <circle
-                    className={innnerCircleDesktop(percentage)}
+                    className={
+                        isGoalReached
+                            ? innerCircleDesktopGoalReached
+                            : innerCircleDesktopGoalNotReached(percentage)
+                    }
                     cx="50%"
                     cy="90%"
                     r="55%"
@@ -220,7 +318,11 @@ const SunriseBackground: React.FC<SunriseBackgroundProps> = ({
                     fill="currentColor"
                 />
                 <circle
-                    className={innnerCircleTablet(percentage)}
+                    className={
+                        isGoalReached
+                            ? innerCircleTabletGoalReached
+                            : innerCircleTabletGoalNotReached(percentage)
+                    }
                     cx="50%"
                     cy="90%"
                     r="55%"
@@ -235,7 +337,11 @@ const SunriseBackground: React.FC<SunriseBackgroundProps> = ({
                     fill="currentColor"
                 />
                 <circle
-                    className={innnerCircleMobile(percentage)}
+                    className={
+                        isGoalReached
+                            ? innerCircleMobileGoalReached
+                            : innerCircleMobileGoalNotReached(percentage)
+                    }
                     cx="50%"
                     cy="75%"
                     r="66%"
