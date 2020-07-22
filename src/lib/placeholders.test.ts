c6d9c56605fb4c1e0d19c1bb9faf973610506f52
Tom Pretty <tompretty2@gmail.com>
2020-07-22 08:22:40 +0100

    Add extra test for %%ARTICLE_COUNT%%

--- src/lib/placeholders.test.ts
+++ src/lib/placeholders.test.ts
@@ -1,9 +1,14 @@
 import { containsNonArticleCountPlaceholder } from './placeholders';
 
-describe('containsPlaceholder', () => {
-    it('returns true if string contains placeholder', () => {
+describe('containsNonArticleCountPlaceholder', () => {
+    it('returns true if string contains placeholder (that is not %%ARTICLE_COUNT%%)', () => {
         const got = containsNonArticleCountPlaceholder('apple %%SOME_PLACEHOLDER%%');
         expect(got).toBe(true);
+    });
+
+    it('returns false if string contains an article count placeholder', () => {
+        const got = containsNonArticleCountPlaceholder('apple %%ARTICLE_COUNT%%');
+        expect(got).toBe(false);
     });
 
     it('returns false if string does not contain placeholder', () => {
