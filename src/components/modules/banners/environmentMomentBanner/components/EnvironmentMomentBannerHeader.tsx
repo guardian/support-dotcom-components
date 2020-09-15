c81099173538c459075d2b1de6eb88987929e3c8
Tom Pretty <tompretty2@gmail.com>
2020-09-15 11:27:03 +0100

    Add tablet version of the header

--- src/components/modules/banners/environmentMomentBanner/components/EnvironmentMomentBannerHeader.tsx
+++ src/components/modules/banners/environmentMomentBanner/components/EnvironmentMomentBannerHeader.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { css } from '@emotion/core';
+import { from } from '@guardian/src-foundations/mq';
 import { headline } from '@guardian/src-foundations/typography';
 import { space } from '@guardian/src-foundations';
 import { neutral } from '@guardian/src-foundations/palette';
@@ -11,6 +12,10 @@ const container = css`
     flex-direction: row;
     border-top: 1px solid ${neutral[86]};
     border-bottom: 1px solid ${neutral[86]};
+
+    ${from.tablet} {
+        border-top: none;
+    }
 `;
 
 const iconAndTextContainer = css`
@@ -18,6 +23,10 @@ const iconAndTextContainer = css`
     position: relative;
     z-index: 100;
     margin-left: ${space[4]}px;
+
+    ${from.tablet} {
+        margin-left: ${space[9]}px;
+    }
 `;
 
 const iconContainer = css`
@@ -25,6 +34,10 @@ const iconContainer = css`
     img {
         height: 100%;
         display: block;
+    }
+
+    ${from.tablet} {
+        height: 125px;
     }
 `;
 
@@ -34,6 +47,12 @@ const textContainer = css`
     margin-left: ${space[2]}px;
     ${headline.xsmall({ fontWeight: 'bold' })}
     white-space: nowrap;
+
+    ${from.tablet} {
+        font-size: 40px;
+        margin-top: 40px;
+        line-height: 100%;
+    }
 `;
 
 const lineContainer = css`
@@ -43,11 +62,38 @@ const lineContainer = css`
     left: 0;
     right: 0;
     display: flex;
+    flex-direction: column;
     align-items: center;
+    justify-content: space-evenly;
 `;
 const line = css`
     width: 100%;
     border-top: 1px solid ${neutral[86]};
+`;
+
+const secondLine = css`
+    ${line}
+    display: none;
+
+    ${from.tablet} {
+        display: block;
+    }
+`;
+
+const hideAfterTablet = css`
+    display: block;
+
+    ${from.tablet} {
+        display: none;
+    }
+`;
+
+const hideBeforeTablet = css`
+    display: none;
+
+    ${from.tablet} {
+        display: block;
+    }
 `;
 
 const EnvironmentMomentBannerHeader: React.FC = () => (
@@ -56,13 +102,22 @@ const EnvironmentMomentBannerHeader: React.FC = () => (
             <div css={iconContainer}>
                 <IconPen />
             </div>
-            <div css={textContainer}>
-                <span>Our climate promise</span>
-                <span>to you</span>
+            <div css={hideAfterTablet}>
+                <div css={textContainer}>
+                    <span>Our climate promise</span>
+                    <span>to you</span>
+                </div>
+            </div>
+            <div css={hideBeforeTablet}>
+                <div css={textContainer}>
+                    <span>Our climate</span>
+                    <span>promise to you</span>
+                </div>
             </div>
         </div>
         <div css={lineContainer}>
             <div css={line} />
+            <div css={secondLine} />
         </div>
     </header>
 );
