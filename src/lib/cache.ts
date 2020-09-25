e73eb00ed71beba997fb3b057e6107752bc08eac
Tom Forbes <tom.forbes@theguardian.com>
2020-09-25 11:05:46 +0100

    Re-implement cache

--- src/lib/cache.ts
+++ src/lib/cache.ts
@@ -1,24 +1,45 @@
-import NodeCache from 'node-cache';
+interface Cache {
+    [key: string]: any
+}
+const cache: Cache = {};
 
-const cache = new NodeCache();
+const retryIntervalMs = 5000;
 
 export const cacheAsync = <T>(
     fn: () => Promise<T>,
     ttlSec: number,
     key: string,
 ): [() => void, () => Promise<T>] => {
-    const retFn = async (): Promise<T> => {
-        const got = cache.get(key);
-        if (got !== undefined) {
-            return got as T;
-        }
+    const getValue = async () => {
+        if (cache[key] !== undefined) {
+            return Promise.resolve(cache[key] as T);
+        } else {
+            // First read attempt on this key. Fetch an initial value and setup the refresh scheduler
+            const result: T = await fn();
+            cache[key] = result;
 
-        const res = await fn();
-        cache.set(key, res, ttlSec);
-        return res;
+            const scheduleRefresh = (ms: number) => {
+                setTimeout(
+                    async () => {
+                        try {
+                            cache[key] = await fn();
+                            scheduleRefresh(ms);
+                        } catch(err) {
+                            console.log(`Error refreshing cached value for key ${key}: ${err}`);
+                            scheduleRefresh(retryIntervalMs);
+                        }
+                    },
+                    ms,
+                );
+            };
+
+            scheduleRefresh(ttlSec * 1000);
+
+            return Promise.resolve(result);
+        }
     };
 
-    const resetFn = (): number => cache.del(key);
+    const reset = () => { cache[key] = undefined };
 
-    return [resetFn, retFn];
+    return [reset, getValue];
 };
