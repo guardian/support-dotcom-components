907b0fe46116732e1dd2c194b739e92164eaca14
Imogen Hardy <imogen.hardy@guardian.co.uk>
2020-09-29 16:02:50 +0100

    adjust default targeting so ROW sees new DS banner

--- src/tests/banners/DigitalSubscriptionsBannerTest.ts
+++ src/tests/banners/DigitalSubscriptionsBannerTest.ts
@@ -12,7 +12,7 @@ export const DigitalSubscriptionsBanner: BannerTest = {
     canRun: (targeting: BannerTargeting, pageTracking: BannerPageTracking) => {
         if (targeting.switches.remoteSubscriptionsBanner) {
             const region = readerRevenueRegionFromCountryCode(targeting.countryCode);
-            return !(region === 'australia' || region === 'rest-of-world');
+            return region !== 'australia';
         }
         return false;
     },
