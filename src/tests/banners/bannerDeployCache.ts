3770f90d7698fb1ecb952f2aaba944ccc2f4a8bf
Tom Forbes <tom.forbes@theguardian.com>
2020-10-07 07:59:39 +0100

    get banner deploy times from s3 (#255)

--- src/tests/banners/bannerDeployCache.ts
+++ src/tests/banners/bannerDeployCache.ts
@@ -13,11 +13,13 @@ const fetchBannerDeployTime = (
     region: ReaderRevenueRegion,
     bannerChannel: BannerChannel,
 ) => (): Promise<Date> => {
+    const channel = bannerChannel === 'contributions' ? 'channel1' : 'channel2';
     return fetch(
-        `https://www.theguardian.com/reader-revenue/${bannerChannel}-banner-deploy-log/${region}`,
+        `https://gu-contributions-public.s3-eu-west-1.amazonaws.com/banner-deploy/PROD/${channel}/${region}.json`,
     )
         .then(response => response.json())
         .then(data => {
+            console.log(`Got banner deploy time for ${channel}/${region}`, data.time);
             return new Date(data.time);
         });
 };
@@ -31,81 +33,32 @@ export interface BannerDeployCaches {
         [key in ReaderRevenueRegion]: () => Promise<Date>;
     };
 }
-const ContributionsDeployDate = new Date(Date.parse('2020-10-06 06:00:00'));
-const SubscriptionsDeployDate = new Date(Date.parse('2020-10-06 06:00:00'));
+
+const cachedDeployTime = (
+    region: ReaderRevenueRegion,
+    bannerChannel: BannerChannel,
+): (() => Promise<Date>) =>
+    cacheAsync(
+        fetchBannerDeployTime(region, bannerChannel),
+        fiveMinutes,
+        `fetch${bannerChannel}BannerDeployTime_${region}`,
+    )[1];
+
 export const bannerDeployCaches: BannerDeployCaches = {
     contributions: {
-        'united-kingdom': () => Promise.resolve(ContributionsDeployDate),
-        'united-states': () => Promise.resolve(ContributionsDeployDate),
-        australia: () => Promise.resolve(ContributionsDeployDate),
-        'rest-of-world': () => Promise.resolve(ContributionsDeployDate),
+        'united-kingdom': cachedDeployTime('united-kingdom', 'contributions'),
+        'united-states': cachedDeployTime('united-states', 'contributions'),
+        australia: cachedDeployTime('australia', 'contributions'),
+        'rest-of-world': cachedDeployTime('rest-of-world', 'contributions'),
         // Contributions doesn't separate europe from row
-        'european-union': () => Promise.resolve(ContributionsDeployDate),
-        // TODO - fix timestamp fetching
-        // 'united-kingdom': cacheAsync(
-        //     fetchBannerDeployTime('united-kingdom', 'contributions'),
-        //     fiveMinutes,
-        //     'fetchEngagementBannerDeployTime_united-kingdom',
-        // )[1],
-        // 'united-states': cacheAsync(
-        //     fetchBannerDeployTime('united-states', 'contributions'),
-        //     fiveMinutes,
-        //     'fetchEngagementBannerDeployTime_united-states',
-        // )[1],
-        // australia: cacheAsync(
-        //     fetchBannerDeployTime('australia', 'contributions'),
-        //     fiveMinutes,
-        //     'fetchEngagementBannerDeployTime_australia',
-        // )[1],
-        // 'rest-of-world': cacheAsync(
-        //     fetchBannerDeployTime('rest-of-world', 'contributions'),
-        //     fiveMinutes,
-        //     'fetchEngagementBannerDeployTime_rest-of-world',
-        // )[1],
-        // // Contributions doesn't separate europe from row
-        // 'european-union': cacheAsync(
-        //     fetchBannerDeployTime('rest-of-world', 'contributions'),
-        //     fiveMinutes,
-        //     'fetchEngagementBannerDeployTime_rest-of-world',
-        // )[1],
+        'european-union': cachedDeployTime('rest-of-world', 'contributions'),
     },
     subscriptions: {
-        'united-kingdom': () => Promise.resolve(SubscriptionsDeployDate),
-        'united-states': () => Promise.resolve(SubscriptionsDeployDate),
-        australia: cacheAsync(
-            fetchBannerDeployTime('australia', 'subscriptions'),
-            fiveMinutes,
-            'fetchSubscriptionsBannerDeployTime_australia',
-        )[1],
-        'rest-of-world': () => Promise.resolve(SubscriptionsDeployDate),
+        'united-kingdom': cachedDeployTime('united-kingdom', 'subscriptions'),
+        'united-states': cachedDeployTime('united-states', 'subscriptions'),
+        australia: cachedDeployTime('australia', 'subscriptions'),
+        'rest-of-world': cachedDeployTime('rest-of-world', 'subscriptions'),
         // Subscriptions separates europe from row
-        'european-union': () => Promise.resolve(SubscriptionsDeployDate),
-        // TODO - fix timestamp fetching
-        // 'united-kingdom': cacheAsync(
-        //     fetchBannerDeployTime('united-kingdom', 'subscriptions'),
-        //     fiveMinutes,
-        //     'fetchSubscriptionsBannerDeployTime_united-kingdom',
-        // )[1],
-        // 'united-states': cacheAsync(
-        //     fetchBannerDeployTime('united-states', 'subscriptions'),
-        //     fiveMinutes,
-        //     'fetchSubscriptionsBannerDeployTime_united-states',
-        // )[1],
-        // australia: cacheAsync(
-        //     fetchBannerDeployTime('australia', 'subscriptions'),
-        //     fiveMinutes,
-        //     'fetchSubscriptionsBannerDeployTime_australia',
-        // )[1],
-        // 'rest-of-world': cacheAsync(
-        //     fetchBannerDeployTime('rest-of-world', 'subscriptions'),
-        //     fiveMinutes,
-        //     'fetchSubscriptionsBannerDeployTime_rest-of-world',
-        // )[1],
-        // // Subscriptions separates europe from row
-        // 'european-union': cacheAsync(
-        //     fetchBannerDeployTime('european-union', 'subscriptions'),
-        //     fiveMinutes,
-        //     'fetchSubscriptionsBannerDeployTime_european-union',
-        // )[1],
+        'european-union': cachedDeployTime('european-union', 'subscriptions'),
     },
 };
