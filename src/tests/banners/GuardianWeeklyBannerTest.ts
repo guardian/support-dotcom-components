39ca205e63dd0428552a0ebd218e2d1ad851b0dd
Lucy Monie Hall <lucy.monie.hall@gmail.com>
2020-10-09 19:24:43 +0100

    Update region where GW banner is shown to Europe

--- src/tests/banners/GuardianWeeklyBannerTest.ts
+++ src/tests/banners/GuardianWeeklyBannerTest.ts
@@ -12,7 +12,7 @@ export const GuardianWeeklyBanner: BannerTest = {
     canRun: (targeting: BannerTargeting, pageTracking: BannerPageTracking) => {
         if (targeting.switches.remoteSubscriptionsBanner) {
             const region = readerRevenueRegionFromCountryCode(targeting.countryCode);
-            return region === 'Australia';
+            return region === 'EuropeanUnion';
         }
         return false;
     },
