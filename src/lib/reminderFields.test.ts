95c1fe5e39ed7ca2ade23ff8d147167ffbc3c7a9
Tom Forbes <tom.forbes@theguardian.com>
2020-11-24 12:06:59 +0000

    Fix reminder month

--- src/lib/reminderFields.test.ts
+++ src/lib/reminderFields.test.ts
@@ -4,27 +4,39 @@ describe('buildReminderFields', () => {
     it('should set date to the next calendar month if the current date is BEFORE the 20th', () => {
         const novemberNineteenth = new Date('2020-11-19 00:00:00');
 
-        const expected = 'December 2020';
-        const actual = buildReminderFields(novemberNineteenth).reminderDateAsString;
+        const expected = {
+            reminderCTA: `Remind me in December`,
+            reminderDate: `2020-12-19 00:00:00`,
+            reminderDateAsString: `December 2020`,
+        };
+        const actual = buildReminderFields(novemberNineteenth);
 
-        expect(actual).toBe(expected);
+        expect(actual).toEqual(expected);
     });
 
     it('should set date to the next + 1 calendar month if the current date is the 20th', () => {
         const novemberTwentieth = new Date('2020-11-20 00:00:00');
 
-        const expected = 'January 2021';
-        const actual = buildReminderFields(novemberTwentieth).reminderDateAsString;
+        const expected = {
+            reminderCTA: `Remind me in January`,
+            reminderDate: `2021-01-19 00:00:00`,
+            reminderDateAsString: `January 2021`,
+        };
+        const actual = buildReminderFields(novemberTwentieth);
 
-        expect(actual).toBe(expected);
+        expect(actual).toEqual(expected);
     });
 
     it('should set date to the next + 1 calendar month if the current date is AFTER the 20th', () => {
         const novemberTwentyFirst = new Date('2020-11-21 00:00:00');
 
-        const expected = 'January 2021';
-        const actual = buildReminderFields(novemberTwentyFirst).reminderDateAsString;
+        const expected = {
+            reminderCTA: `Remind me in January`,
+            reminderDate: `2021-01-19 00:00:00`,
+            reminderDateAsString: `January 2021`,
+        };
+        const actual = buildReminderFields(novemberTwentyFirst);
 
-        expect(actual).toBe(expected);
+        expect(actual).toEqual(expected);
     });
 });
