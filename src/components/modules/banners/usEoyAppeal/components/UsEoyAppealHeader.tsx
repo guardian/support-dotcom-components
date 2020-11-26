ec3d7484c5383c2f012b26ab739aa29695e1292b
Tom Pretty <tompretty2@gmail.com>
2020-11-26 13:24:50 +0000

    Add styles to temp header copy

--- src/components/modules/banners/usEoyAppeal/components/UsEoyAppealHeader.tsx
+++ src/components/modules/banners/usEoyAppeal/components/UsEoyAppealHeader.tsx
@@ -1,8 +1,30 @@
 import React from 'react';
+import { css } from '@emotion/core';
+import { Hide } from '@guardian/src-layout';
+import { news, brand } from '@guardian/src-foundations/palette';
 import ContributionsTemplateHeader from '../../contributionsTemplate/ContributionsTemplateHeader';
 
+const firstLineStyles = css`
+    color: ${news[400]};
+`;
+
+const secondLineStyles = css`
+    color: ${brand[400]};
+`;
+
 const UsEoyAppealVisual: React.FC = () => (
-    <ContributionsTemplateHeader copy={<>Help put America back together this Giving Tuesday</>} />
+    <ContributionsTemplateHeader
+        copy={
+            <>
+                <Hide above="tablet">As America begins a new chapter…</Hide>
+                <Hide below="tablet">
+                    <span css={firstLineStyles}>America’s future:</span>
+                    <br />
+                    <span css={secondLineStyles}>what’s next?</span>
+                </Hide>
+            </>
+        }
+    />
 );
 
 export default UsEoyAppealVisual;
