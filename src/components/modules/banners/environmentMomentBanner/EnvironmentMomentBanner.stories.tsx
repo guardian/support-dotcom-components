9b417d483056eeb06ed98d261034c40bfee659c1
Tom Pretty <tompretty2@gmail.com>
2020-11-26 09:25:30 +0000

    Refactor env moment stories

--- src/components/modules/banners/environmentMomentBanner/EnvironmentMomentBanner.stories.tsx
+++ src/components/modules/banners/environmentMomentBanner/EnvironmentMomentBanner.stories.tsx
@@ -1,42 +1,25 @@
-import React, { ReactElement } from 'react';
+import React from 'react';
+import { Story, Meta } from '@storybook/react';
 import { EnvironmentMomentBanner } from './EnvironmentMomentBanner';
-import { withKnobs, boolean } from '@storybook/addon-knobs';
-import { StorybookWrapper } from '../../../../utils/StorybookWrapper';
-import { BannerTracking } from '../../../../types/BannerTypes';
+import { BannerProps } from '../../../../types/BannerTypes';
+import { props } from '../utils/storybook';
 
 export default {
     component: EnvironmentMomentBanner,
-    title: 'Components/EnvironmentMomentBanner',
-    decorators: [withKnobs],
+    title: 'Banners/EnvironmentMoment',
+    args: props,
+} as Meta;
+
+const Template: Story<BannerProps> = (props: BannerProps) => <EnvironmentMomentBanner {...props} />;
+
+export const NonSupporters = Template.bind({});
+
+export const Supporters = Template.bind({});
+Supporters.args = {
+    isSupporter: true,
 };
 
-const tracking: BannerTracking = {
-    ophanPageId: 'kbluzw2csbf83eabedel',
-    platformId: 'GUARDIAN_WEB',
-    clientName: 'dcr',
-    referrerUrl: 'http://localhost:3030/Article',
-    abTestName: 'EnvironmentMomentBanner',
-    abTestVariant: 'control',
-    campaignCode: 'EnvironmentMomentBanner_fancy',
-    componentType: 'ACQUISITIONS_ENGAGEMENT_BANNER',
-    products: ['CONTRIBUTION', 'MEMBERSHIP_SUPPORTER'],
+export const Aus = Template.bind({});
+Aus.args = {
+    countryCode: 'AU',
 };
-
-export const defaultStory = (): ReactElement => {
-    const isSupporter = boolean('isSupporter', false);
-    const isAus = boolean('isAus', false);
-
-    return (
-        <StorybookWrapper>
-            <EnvironmentMomentBanner
-                isSupporter={isSupporter}
-                countryCode={isAus ? 'AU' : 'GB'}
-                tracking={tracking}
-                submitComponentEvent={(event): void => console.log(event)}
-                bannerChannel="contributions"
-            />
-        </StorybookWrapper>
-    );
-};
-
-defaultStory.story = { name: 'Environment Moment - Fancy Banner' };
