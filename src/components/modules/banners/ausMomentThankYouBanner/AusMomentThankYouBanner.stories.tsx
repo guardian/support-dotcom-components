1c39fa3f982d616c47eff61c97ca1fb2ef84fbde
Tom Pretty <tompretty2@gmail.com>
2020-11-26 09:20:06 +0000

    Refactor aus moment stories

--- src/components/modules/banners/ausMomentThankYouBanner/AusMomentThankYouBanner.stories.tsx
+++ src/components/modules/banners/ausMomentThankYouBanner/AusMomentThankYouBanner.stories.tsx
@@ -1,58 +1,15 @@
-import React, { ReactElement } from 'react';
+import React from 'react';
+import { Story, Meta } from '@storybook/react';
 import { AusMomentThankYouBanner } from './AusMomentThankYouBanner';
-import { withKnobs, boolean } from '@storybook/addon-knobs';
-import { StorybookWrapper } from '../../../../utils/StorybookWrapper';
-import { TickerCountType, TickerEndType } from '../../../../lib/variants';
-import { BannerTracking } from '../../../../types/BannerTypes';
+import { BannerProps } from '../../../../types/BannerTypes';
+import { props } from '../utils/storybook';
 
 export default {
     component: AusMomentThankYouBanner,
-    title: 'Components/AusMomentThankYouBanner',
-    decorators: [withKnobs],
-};
+    title: 'Banners/AusMomentThankYou',
+    args: props,
+} as Meta;
 
-const tracking: BannerTracking = {
-    ophanPageId: 'kbluzw2csbf83eabedel',
-    platformId: 'GUARDIAN_WEB',
-    clientName: 'dcr',
-    referrerUrl: 'http://localhost:3030/Article',
-    abTestName: 'AusMomentContributionsBanner',
-    abTestVariant: 'control',
-    campaignCode: 'AusMomentContributionsBanner_control',
-    componentType: 'ACQUISITIONS_ENGAGEMENT_BANNER',
-    products: ['CONTRIBUTION', 'MEMBERSHIP_SUPPORTER'],
-};
+const Template: Story<BannerProps> = (props: BannerProps) => <AusMomentThankYouBanner {...props} />;
 
-const tickerSettings = {
-    countType: TickerCountType.people,
-    endType: TickerEndType.unlimited,
-    currencySymbol: '$',
-    // Usually we need the ticker copy, but this banner has a very custom ticker
-    copy: {
-        countLabel: '',
-        goalReachedPrimary: '',
-        goalReachedSecondary: '',
-    },
-    tickerData: {
-        total: 120_000,
-        goal: 150_000,
-    },
-};
-
-export const defaultStory = (): ReactElement => {
-    const isSupporter = boolean('isSupporter', false);
-
-    return (
-        <StorybookWrapper>
-            <AusMomentThankYouBanner
-                bannerChannel="contributions"
-                isSupporter={isSupporter}
-                tickerSettings={tickerSettings}
-                tracking={tracking}
-                countryCode="AU"
-            />
-        </StorybookWrapper>
-    );
-};
-
-defaultStory.story = { name: 'Aus Moment - Thank You banner' };
+export const Default = Template.bind({});
