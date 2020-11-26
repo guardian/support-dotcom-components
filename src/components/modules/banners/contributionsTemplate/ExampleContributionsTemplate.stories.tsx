43f3f5f0576606451d8268c0d034bd89b3984de5
Tom Pretty <tompretty2@gmail.com>
2020-11-26 08:54:59 +0000

    Refactor example banners

--- src/components/modules/banners/contributionsTemplate/ExampleContributionsTemplate.stories.tsx
+++ src/components/modules/banners/contributionsTemplate/ExampleContributionsTemplate.stories.tsx
@@ -1,71 +1,15 @@
-import React, { ReactElement } from 'react';
+import React from 'react';
+import { Story, Meta } from '@storybook/react';
 import { Example } from './ExampleContributionsTemplate';
-import { withKnobs, number } from '@storybook/addon-knobs';
-import { StorybookWrapper } from '../../../../utils/StorybookWrapper';
-import { TickerCountType, TickerEndType } from '../../../../lib/variants';
-import { BannerProps, BannerContent, BannerTracking } from '../../../../types/BannerTypes';
+import { BannerProps } from '../../../../types/BannerTypes';
+import { props } from '../utils/storybook';
 
 export default {
     component: Example,
-    title: 'Components/ContributionsTemplate',
-    decorators: [withKnobs],
-};
+    title: 'Banners/ContributionsTemplate',
+    args: props,
+} as Meta;
 
-const tracking: BannerTracking = {
-    ophanPageId: 'kbluzw2csbf83eabedel',
-    componentType: 'ACQUISITIONS_ENGAGEMENT_BANNER',
-    platformId: 'GUARDIAN_WEB',
-    clientName: 'dcr',
-    referrerUrl: 'http://localhost:3030/Article',
-    abTestName: 'ContributionsTemplate',
-    abTestVariant: 'control',
-    campaignCode: 'ContributionsTemplate_control',
-};
+const Template: Story<BannerProps> = (props: BannerProps) => <Example {...props} />;
 
-const content: BannerContent = {
-    messageText:
-        '<strong> We chose a different approach. Will you support it?</strong> Unlike many news organisations, we made a choice to keep our journalism open for all. At a time when factual information is a necessity, we believe that each of us, around the world, deserves access to accurate reporting with integrity at its heart. Every contribution, however big or small, is so valuable – it is essential in protecting our editorial independence.',
-    highlightedText: ' Support the Guardian today from as little as %%CURRENCY_SYMBOL%%1.',
-    cta: {
-        baseUrl: 'https://support.theguardian.com/contribute',
-        text: 'Support The Guardian',
-    },
-};
-
-const tickerSettings = {
-    countType: TickerCountType.money,
-    endType: TickerEndType.hardstop,
-    currencySymbol: '$',
-    copy: {
-        countLabel: 'contributed',
-        goalReachedPrimary: "We've met our goal - thank you",
-        goalReachedSecondary: 'Contributions are still being accepted',
-    },
-    tickerData: {
-        total: 120_000,
-        goal: 150_000,
-    },
-};
-
-const props: BannerProps = {
-    bannerChannel: 'contributions',
-    tracking,
-    isSupporter: false,
-    countryCode: 'GB',
-    content,
-    tickerSettings,
-};
-
-export const Default = (): ReactElement => {
-    const total = number('total', 125_000);
-    const goal = number('goal', 150_000);
-
-    tickerSettings.tickerData.total = total;
-    tickerSettings.tickerData.goal = goal;
-
-    return (
-        <StorybookWrapper>
-            <Example {...props} />
-        </StorybookWrapper>
-    );
-};
+export const Default = Template.bind({});
