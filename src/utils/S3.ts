33b3853377ec18ef93ffff3657d354b94804bebc
Tom Forbes <tom.forbes@theguardian.com>
2020-12-02 10:09:49 +0000

    fix

--- src/utils/S3.ts
+++ src/utils/S3.ts
@@ -1,9 +1,9 @@
 import { GetObjectOutput } from 'aws-sdk/clients/s3';
 import AWS from 'aws-sdk';
 
-const credentials = new AWS.SharedIniFileCredentials({ profile: 'membership' });
-AWS.config.credentials = credentials;
-
+if (process.env.NODE_ENV === 'development') {
+    AWS.config.credentials = new AWS.SharedIniFileCredentials({profile: 'membership'});
+}
 const S3 = new AWS.S3();
 
 export const fetchS3Data = (bucket: string, key: string): Promise<string> => {
