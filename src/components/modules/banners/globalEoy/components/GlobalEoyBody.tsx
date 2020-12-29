6518740c713c840fece70da07297fed061b07523
Tom Forbes <tom.forbes@theguardian.com>
2020-12-29 11:18:49 +0000

    Add payment method icons to banner template

--- src/components/modules/banners/globalEoy/components/GlobalEoyBody.tsx
+++ src/components/modules/banners/globalEoy/components/GlobalEoyBody.tsx
@@ -31,10 +31,10 @@ const GlobalEoyBody: React.FC<GlobalEoyBodyProps> = ({
                     <Hide below="tablet">
                         {shouldShowArticleCount ? (
                             <>
-                                In an extraordinary 2020, our independent journalism was powered by
-                                more than a million supporters. Thanks to you, we provided vital
-                                news and analysis for everyone, led by science and truth.
-                                You&apos;ve read{' '}
+                                In the extraordinary year that was 2020, our independent journalism
+                                was powered by more than a million supporters. Thanks to you, we
+                                provided vital news and analysis for everyone, led by science and
+                                truth. You&apos;ve read{' '}
                                 <ArticleCountOptOut
                                     numArticles={numArticles}
                                     nextWord=" articles"
@@ -46,11 +46,11 @@ const GlobalEoyBody: React.FC<GlobalEoyBodyProps> = ({
                             </>
                         ) : (
                             <>
-                                In an extraordinary 2020, our independent journalism was powered by
-                                more than a million supporters. Thanks to you, we provided vital
-                                news and analysis for everyone, led by science and truth. With 2021
-                                offering renewed hope, we commit to another year of high-impact
-                                reporting. Support us from as little as{' '}
+                                In the extraordinary year that was 2020, our independent journalism
+                                was powered by more than a million supporters. Thanks to you, we
+                                provided vital news and analysis for everyone, led by science and
+                                truth. With 2021 offering renewed hope, we commit to another year of
+                                high-impact reporting. Support us from as little as{' '}
                                 {getLocalCurrencySymbol(countryCode)}1.
                             </>
                         )}
