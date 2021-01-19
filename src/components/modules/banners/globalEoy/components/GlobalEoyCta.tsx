110e42a0764b75faaf73264b39a351c9a755a7b6
Tom Forbes <tom.forbes@theguardian.com>
2021-01-19 09:05:29 +0000

    remove 2nd cta

--- src/components/modules/banners/globalEoy/components/GlobalEoyCta.tsx
+++ src/components/modules/banners/globalEoy/components/GlobalEoyCta.tsx
@@ -6,8 +6,6 @@ import { BannerTracking } from '../../../../../types/BannerTypes';
 import { addRegionIdAndTrackingParamsToSupportUrl } from '../../../../../lib/tracking';
 
 const BASE_LANDING_PAGE_URL = 'https://support.theguardian.com/contribute';
-const IMPACT_REPORT_LINK =
-    'https://www.theguardian.com/info/ng-interactive/2020/dec/21/the-guardian-in-2020?INTCMP=global_eoy_banner';
 
 interface GlobalEoyCtaProps {
     onContributeClick: () => void;
@@ -48,32 +46,7 @@ const GlobalEoyCta: React.FC<GlobalEoyCtaProps> = ({
                     </Hide>
                 </div>
             }
-            secondaryCta={
-                countryCode.toUpperCase() === 'US' ? null : (
-                    <div>
-                        <Hide above="desktop">
-                            <LinkButton
-                                href={IMPACT_REPORT_LINK}
-                                onClick={onReadMoreClick}
-                                size="small"
-                                priority="tertiary"
-                            >
-                                2020 highlights
-                            </LinkButton>
-                        </Hide>
-                        <Hide below="desktop">
-                            <LinkButton
-                                href={IMPACT_REPORT_LINK}
-                                onClick={onReadMoreClick}
-                                size="default"
-                                priority="tertiary"
-                            >
-                                2020 highlights
-                            </LinkButton>
-                        </Hide>
-                    </div>
-                )
-            }
+            secondaryCta={null}
         />
     );
 };
