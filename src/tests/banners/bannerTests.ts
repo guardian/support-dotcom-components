51259e27870a62309d28873ee9a8e36ed6ff6cae
Tom Forbes <tom.forbes@theguardian.com>
2021-01-25 14:50:40 +0000

    Remove global EOY banner test

--- src/tests/banners/bannerTests.ts
+++ src/tests/banners/bannerTests.ts
@@ -4,17 +4,14 @@ import {
     channel1BannersAllTestsGenerator,
     channel2BannersAllTestsGenerator,
 } from './ChannelBannerTests';
-import {
-    GlobalEoyInaugurationACBanner,
-    GlobalEoyInaugurationNoACBanner,
-} from './GlobalEoyBannerTest';
+import { GlobalEoyACBanner, GlobalEoyNoACBanner } from './GlobalEoyBannerTest';
 import { cacheAsync } from '../../lib/cache';
 
 const defaultBannerTestGenerator: BannerTestGenerator = () =>
     Promise.resolve([DefaultContributionsBanner]);
 
 const globalEoyTestGenerator: BannerTestGenerator = () =>
-    Promise.resolve([GlobalEoyInaugurationACBanner, GlobalEoyInaugurationNoACBanner]);
+    Promise.resolve([GlobalEoyACBanner, GlobalEoyNoACBanner]);
 
 const flattenArray = <T>(array: T[][]): T[] => ([] as T[]).concat(...array);
 
