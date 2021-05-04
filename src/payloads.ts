8c713b2d8d321b9dd364189406206c62c1ccdbc9
Tom Pretty <tompretty2@gmail.com>
2021-05-04 14:02:21 +0100

    Update fallback epic (#435)
    * Use hardcoded fallback epic

--- src/payloads.ts
+++ src/payloads.ts
@@ -11,8 +11,6 @@ import {
 import { Debug, findTestAndVariant, findForcedTestAndVariant, Variant, Test } from './lib/variants';
 import { getArticleViewCountForWeeks } from './lib/history';
 import { buildBannerCampaignCode, buildCampaignCode } from './lib/tracking';
-
-import { getAllHardcodedTests } from './tests';
 import { Params } from './lib/params';
 import { baseUrl } from './lib/env';
 import { addTickerDataToSettings, getTickerSettings } from './lib/fetchTickerData';
@@ -39,7 +37,9 @@ import {
     puzzlesBanner,
     header,
 } from './modules';
+import { fallbackEpicTest } from './tests/epics/fallback';
 import { getReminderFields } from './lib/reminderFields';
+import { logger } from './utils/logging';
 
 interface EpicDataResponse {
     data?: {
@@ -107,22 +107,27 @@ const [, fetchConfiguredLiveblogEpicTestsCached] = cacheAsync(
 );
 
 const getArticleEpicTests = async (mvtId: number, isForcingTest: boolean): Promise<Test[]> => {
-    const [regular, holdback, hardcoded] = await Promise.all([
-        fetchConfiguredArticleEpicTestsCached(),
-        fetchConfiguredArticleEpicHoldbackTestsCached(),
-        getAllHardcodedTests(),
-    ]);
+    try {
+        const [regular, holdback] = await Promise.all([
+            fetchConfiguredArticleEpicTestsCached(),
+            fetchConfiguredArticleEpicHoldbackTestsCached(),
+        ]);
 
-    if (isForcingTest) {
-        return [...regular.tests, ...holdback.tests, ...hardcoded];
+        if (isForcingTest) {
+            return [...regular.tests, ...holdback.tests, fallbackEpicTest];
+        }
+
+        const shouldHoldBack = mvtId % 100 === 0; // holdback 1% of the audience
+        if (shouldHoldBack) {
+            return [...holdback.tests];
+        }
+
+        return [...regular.tests, fallbackEpicTest];
+    } catch (err) {
+        logger.warn(`Error getting article epic tests: ${err}`);
+
+        return [fallbackEpicTest];
     }
-
-    const shouldHoldBack = mvtId % 100 === 0; // holdback 1% of the audience
-    if (shouldHoldBack) {
-        return [...holdback.tests];
-    }
-
-    return [...regular.tests, ...hardcoded];
 };
 
 const getLiveblogEpicTests = async (): Promise<Test[]> => {
--- src/tests/index.ts
+++
@@ -1,7 +0,0 @@
-import { Test } from '../lib/variants';
-import { askFourEarningHardcodedTest } from '../tests/askFourEarning';
-
-export const getAllHardcodedTests = async (): Promise<Test[]> => {
-    const askFourEarning: Test = await askFourEarningHardcodedTest();
-    return [askFourEarning];
-};
