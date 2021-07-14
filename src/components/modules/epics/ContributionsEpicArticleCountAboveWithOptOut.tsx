ef895c3967f1c516661c3651e259917b068f1176
John Duffell <john.duffell@guardian.co.uk>
2021-07-14 17:34:28 +0100

    animate article count on epic

--- src/components/modules/epics/ContributionsEpicArticleCountAboveWithOptOut.tsx
+++ src/components/modules/epics/ContributionsEpicArticleCountAboveWithOptOut.tsx
@@ -6,6 +6,7 @@ import { palette, space } from '@guardian/src-foundations';
 import { Button } from '@guardian/src-button';
 import { ButtonLink } from '@guardian/src-link';
 import { css } from '@emotion/core';
+import { useHasBeenSeen, HasBeenSeen } from '../../../hooks/useHasBeenSeen';
 import { OphanComponentEvent } from '../../../types/OphanTypes';
 import {
     OPHAN_COMPONENT_ARTICLE_COUNT_OPT_OUT_OPEN,
@@ -16,6 +17,7 @@ import {
     OPHAN_COMPONENT_ARTICLE_COUNT_OPT_IN,
 } from './utils/ophan';
 import { from, until } from '@guardian/src-foundations/mq';
+import {png1, png2, png3, png4, png5} from './utils/images';
 
 // --- Styles --- //
 
@@ -40,6 +42,85 @@ const articleCountAboveContainerStyles = css`
 
 const optOutContainer = css`
     color: ${palette.opinion[400]};
+    position: relative;
+`;
+
+const staticNumber = css`
+    &:after {
+        content: 0;
+    }
+`;
+
+const animateNumber = (numArticles: number) => {
+    return css`
+        @property --num {
+            syntax: '<integer>';
+            initial-value: 0;
+            inherits: false;
+        }
+        transition: --num 5s cubic-bezier(0, 0.5, 0, 1) 1s;
+        counter-reset: num var(--num);
+
+        --num: ${numArticles};
+        &:after {
+            content: counter(num);
+        }
+    `;
+};
+
+const articles = css``;
+const article = (index: number) => css`
+    position: absolute;
+
+    left: 60%;
+    top: 50%;
+    opacity: 0;
+    filter: blur(0px);
+    transform: translate(-50%, -50%) scaleX(0.2) scaleY(0.05) rotateX(0deg) rotateY(0deg) rotateZ(0deg);
+    border: 20px solid rgba(0,0,0,1);
+
+    animation: raise${index} 6s ease 1;
+    animation-fill-mode: forwards;
+    animation-delay: ${4 + index * 0}s;
+    z-index: ${20 - index};
+
+    @keyframes raise${index} {
+        ${(index * 0.5) + 30}% {
+            left: 90%;
+            top: 50%;
+            opacity: 0.6;
+            filter: blur(0px);
+            transform: translate(-50%, -50%) scale(0.2) rotateX(0deg) rotateY(0deg) rotateZ(0deg);
+            animation-timing-function: cubic-bezier(0, 1, 0, 1);
+            border-color: rgba(0,0,0,0.2);
+        }
+        ${(index * 6) + 30}% {
+            left: ${(index * 100) + 10}%;
+            top: 1500%;
+            opacity: 1;
+            filter: blur(0px);
+            transform: translate(-50%, -50%) scale(1) rotateX(0deg) rotateY(0deg) rotateZ(0deg);
+            animation-timing-function: cubic-bezier(0.53, 0.04, 0.65, 0.1);
+            border-color: rgba(0,0,0,0.2);
+        }
+        ${(index * 10) + 30}% {
+            left: ${(index * 70) + 10}%;
+            top: 1300%;
+            opacity: 1;
+            filter: blur(0px);
+            transform: translate(-50%, -50%) scale(1) rotateX(0deg) rotateY(0deg) rotateZ(0deg);
+            animation-timing-function: cubic-bezier(0.53, 0.04, 0.65, 0.1);
+            border-color: rgba(0,0,0,0.2);
+        }
+        to {
+            left: ${index * 200}%;
+            top: -50vh;
+            opacity: 0;
+            filter: blur(2px);
+            transform: translate(-50%, -50%) scale(2) rotateX(180deg) rotateY(90deg) rotateZ(90deg);
+            border-color: rgba(0,0,0,0.2);
+        }
+    }
 `;
 
 const articleCountOnHeaderContainerStyles = css`
@@ -243,14 +324,35 @@ const ArticleCountWithToggle: React.FC<ArticleCountWithToggleProps> = ({
     numArticles,
     onToggleClick,
 }: ArticleCountWithToggleProps) => {
+    const debounce = true;
+    const [hasBeenSeen, setNode] = useHasBeenSeen(
+        {
+            rootMargin: '-18px',
+            threshold: 0,
+        },
+        debounce,
+    ) as HasBeenSeen;
+
     if (isArticleCountOn && numArticles >= 5) {
         return (
-            <div css={articleCountOnHeaderContainerStyles}>
+            <div ref={setNode} css={articleCountOnHeaderContainerStyles}>
                 <div css={articleCountAboveContainerStyles}>
                     {numArticles >= 5 && (
                         <>
                             You&apos;ve read{' '}
-                            <span css={optOutContainer}>{numArticles} articles</span> in the last
+                            <span css={optOutContainer}>
+                                <span css={hasBeenSeen ? animateNumber(numArticles) : staticNumber}>
+                                    &nbsp;
+                                </span>{' '}
+                                articles
+                                <span css={articles}>
+                                    <img css={article(1)} src={png1} />
+                                    <img css={article(2)} src={png2} />
+                                    <img css={article(3)} src={png3} />
+                                    <img css={article(4)} src={png4} />
+                                    <img css={article(5)} src={png5} />
+                                </span>
+                            </span>{' '}
                             year
                         </>
                     )}
