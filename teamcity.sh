d6d54a3a4dee3e6c944d5cac0dd756587164ee0a
Tom Pretty <tompretty2@gmail.com>
2021-08-03 14:33:06 +0100

    upload everything

--- teamcity.sh
+++ teamcity.sh
@@ -16,17 +16,8 @@ yarn setup
 yarn lint
 yarn test
 
-# Will place .js files in dist
-NODE_ENV=production yarn run build
-
-# These also need to be in the RiffRaff package
-cp packages/server/package.json packages/server/dist
-cp riff-raff.yaml packages/server/dist
-cp cfn.yaml packages/server/dist
-
-pushd packages/server/dist
-# Ensures the RiffRaff package has the node_modules needed to run
-yarn install --production
-popd
+# rm -rf node_modules
+# yarn install --production
+NODE_ENV=production yarn build
 
 yarn run riffraff
