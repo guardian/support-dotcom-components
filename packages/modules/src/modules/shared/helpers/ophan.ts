d458c447f4f0e147a6fcd861241965535dcd3436
Tom Forbes <tom.forbes@theguardian.com>
2021-08-17 08:48:00 +0100

    Use 'ACQUISITIONS_OTHER' componentType for AC opt-out popup events

--- packages/modules/src/modules/shared/helpers/ophan.ts
+++ packages/modules/src/modules/shared/helpers/ophan.ts
@@ -9,38 +9,42 @@ export const ophanComponentEventOptOutView = (
     componentType: OphanComponentType,
 ): OphanComponentEvent => ({
     component: {
-        componentType,
+        componentType: 'ACQUISITIONS_OTHER',
         id: OPHAN_COMPONENT_ID_OPT_OUT_VIEW,
     },
     action: 'VIEW',
+    value: `article-count-opt-out-popup-${componentType}`,
 });
 
 export const ophanComponentEventOptOutOpen = (
     componentType: OphanComponentType,
 ): OphanComponentEvent => ({
     component: {
-        componentType,
+        componentType: 'ACQUISITIONS_OTHER',
         id: OPHAN_COMPONENT_ID_OPT_OUT_OPEN,
     },
     action: 'CLICK',
+    value: `article-count-opt-out-popup-${componentType}`,
 });
 
 export const ophanComponentEventOptOutClose = (
     componentType: OphanComponentType,
 ): OphanComponentEvent => ({
     component: {
-        componentType,
+        componentType: 'ACQUISITIONS_OTHER',
         id: OPHAN_COMPONENT_ID_OPT_OUT_CLOSE,
     },
     action: 'CLICK',
+    value: `article-count-opt-out-popup-${componentType}`,
 });
 
 export const ophanComponentEventOptOutConfirm = (
     componentType: OphanComponentType,
 ): OphanComponentEvent => ({
     component: {
-        componentType,
+        componentType: 'ACQUISITIONS_OTHER',
         id: OPHAN_COMPONENT_ID_OPT_OUT_CONFIRM,
     },
     action: 'CLICK',
+    value: `article-count-opt-out-popup-${componentType}`,
 });
