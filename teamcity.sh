6e1f59fb7e33c6ad507f3d31e359a753e3603213
Tom Pretty <tompretty2@gmail.com>
2021-09-01 14:26:38 +0100

    fix ci linting

--- teamcity.sh
+++ teamcity.sh
@@ -2,10 +2,6 @@
 
 set -e
 
-pushd cdk
-./script/ci
-popd
-
 export NODE_OPTIONS="--max-old-space-size=16384"
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
@@ -22,5 +18,9 @@ yarn test
 
 NODE_ENV=production yarn server build
 NODE_ENV=production yarn modules build
+
+pushd cdk
+./script/ci
+popd
 
 yarn run riffraff
