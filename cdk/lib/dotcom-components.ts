95c036139c0dd77e9d4b4f6336f2c0b200504d41
Tom Pretty <tompretty2@gmail.com>
2021-09-03 14:06:27 +0100

    CDK migration - phase 2 (#512)
    * introduce basic cdk
    
    * generate from template
    
    * setup ci scripts
    
    * cleanup
    
    * setup ci scripts
    
    * wip
    
    * fix ci linting
    
    * cleanup
    
    * introduce GuEc2App pattern
    
    * fix snapshot test
    
    * add policies and scaling
    
    * fix vpcid
    
    * update riffraff to allow two asgs
    
    * turn modules back on
    
    * add super mode logging

--- cdk/lib/dotcom-components.ts
+++ cdk/lib/dotcom-components.ts
@@ -1,8 +1,17 @@
 import path from 'path';
+import type { Policy } from '@aws-cdk/aws-iam';
 import { CfnInclude } from '@aws-cdk/cloudformation-include';
 import type { App } from '@aws-cdk/core';
+import { Tags } from '@aws-cdk/core';
+import { AccessScope, GuEc2App } from '@guardian/cdk';
+import { Stage } from '@guardian/cdk/lib/constants/stage';
 import type { GuStackProps, GuStageParameter } from '@guardian/cdk/lib/constructs/core';
-import { GuStack } from '@guardian/cdk/lib/constructs/core';
+import {
+    GuDistributionBucketParameter,
+    GuStack,
+    GuStringParameter,
+} from '@guardian/cdk/lib/constructs/core';
+import { GuDynamoDBReadPolicy, GuGetS3ObjectsPolicy } from '@guardian/cdk/lib/constructs/iam';
 
 const yamlTemplateFilePath = path.join(__dirname, '../cfn.yaml');
 
@@ -10,10 +19,96 @@ export class DotcomComponents extends GuStack {
     constructor(scope: App, id: string, props: GuStackProps) {
         super(scope, id, props);
 
+        const appName = 'dotcom-components';
+        const baseUrl = new GuStringParameter(this, 'BaseUrl', {
+            description: 'Base URL of the service.',
+        });
+        const elkStream = new GuStringParameter(this, 'ELKStream', {
+            description: 'Name of the Kinesis stream used to send logs to the central ELK stack.',
+        });
+
+        const userData = `#!/bin/bash
+
+groupadd support
+useradd -r -m -s /usr/bin/nologin -g support dotcom-components
+cd /home/dotcom-components
+
+aws --region eu-west-1 s3 cp s3://${
+            GuDistributionBucketParameter.getInstance(this).valueAsString
+        }/support/${this.stage}/${appName}/${appName}.tar ./
+mkdir ${appName}
+tar -xvf ${appName}.tar --directory ${appName}
+
+chown -R dotcom-components:support ${appName}
+
+cd ${appName}
+
+export TERM=xterm-256color
+export NODE_ENV=production
+export stage=${this.stage}
+export base_url=${baseUrl.valueAsString}
+
+mkdir /var/log/dotcom-components
+chown -R dotcom-components:support /var/log/dotcom-components
+
+/usr/local/node/pm2 start --uid dotcom-components --gid support server.js
+
+/opt/aws-kinesis-agent/configure-aws-kinesis-agent ${this.region} ${
+            elkStream.valueAsString
+        } /var/log/dotcom-components/dotcom-components.log`;
+
+        const policies: Policy[] = [
+            new GuGetS3ObjectsPolicy(this, 'S3ReadPolicySupportAdminConsole', {
+                bucketName: 'support-admin-console',
+                paths: [`${this.stage}/banner-deploy/*`, `${this.stage}/channel-switches.json`],
+            }),
+            new GuGetS3ObjectsPolicy(this, 'S3ReadPolicyGuContributionsPublic', {
+                bucketName: 'gu-contributions-public',
+                paths: [`epic/${this.stage}/*`, `banner/${this.stage}/*`],
+            }),
+            new GuDynamoDBReadPolicy(this, 'DynamoReadPolicy', {
+                tableName: `super-mode-${this.stage}`,
+            }),
+            // TODO: remove when secondary indexes are included in GuDynamoDBRead
+            new GuDynamoDBReadPolicy(this, 'DynamoReadPolicySecondaryIndex', {
+                tableName: `super-mode-${this.stage}/index/*`,
+            }),
+        ];
+
+        const ec2App = new GuEc2App(this, {
+            applicationPort: 3030,
+            app: appName,
+            access: { scope: AccessScope.PUBLIC },
+            certificateProps: {
+                [Stage.CODE]: {
+                    // TODO: should be dotcom-components.support.code.dev-guardianapis.com
+                    domainName: 'dotcom-components-code.support.guardianapis.com',
+                },
+                [Stage.PROD]: {
+                    domainName: 'dotcom-components.support.guardianapis.com',
+                },
+            },
+            // TODO: review monitoring config
+            monitoringConfiguration: { noMonitoring: true },
+            userData,
+            roleConfiguration: {
+                additionalPolicies: policies,
+            },
+            // TODO: review scaling policy on cpu usage
+            scaling: { PROD: { minimumInstances: 3, maximumInstances: 18 } },
+        });
+
+        // TODO: remove this when we just have one ASG (deleted the old one)
+        const ec2AppAsg = ec2App.autoScalingGroup;
+        Tags.of(ec2AppAsg).add('gu:riffraff:new-asg', 'true');
+
+        // TODO: Once this code has been removed we can delete old acm certificates
         new CfnInclude(this, 'Template', {
             templateFile: yamlTemplateFilePath,
             parameters: {
                 Stage: this.getParam<GuStageParameter>('Stage'),
+                BaseUrl: baseUrl,
+                ELKStream: elkStream,
             },
         });
     }
