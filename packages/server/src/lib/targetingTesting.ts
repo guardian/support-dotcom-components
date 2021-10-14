20c2f54e576751fd280ef54158683e20c620490b
Tom Forbes <tom.forbes@theguardian.com>
2021-10-14 08:11:42 +0100

    comment

--- packages/server/src/lib/targetingTesting.ts
+++ packages/server/src/lib/targetingTesting.ts
@@ -10,6 +10,12 @@ interface TargetingTestVariant<T> extends Variant {
     canShow: (targeting: T) => boolean; // Can a message be shown?
 }
 
+/**
+ * A targeting test seeks to understand the impact of changes to targeting rules.
+ * A user is assigned to a variant in a targeting test in the usual way.
+ * But the variant then determines if the user should see a message at all, based on the user's targeting data.
+ * Each variant in a TargetingTest has a `canShow` function. This decides if the user can be put into a message test.
+ */
 export interface TargetingTest<T> extends Test<TargetingTestVariant<T>> {
     name: string;
     variants: TargetingTestVariant<T>[];
