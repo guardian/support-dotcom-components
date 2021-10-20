43e5f16ace30c1274ccf7a1d38c2260d75f21f0c
Tom Forbes <tom.forbes@theguardian.com>
2021-10-20 08:22:20 +0100

    comment

--- packages/shared/src/types/shared.ts
+++ packages/shared/src/types/shared.ts
@@ -146,7 +146,7 @@ export interface ControlProportionSettings {
 export type Stage = 'PROD' | 'CODE' | 'DEV';
 
 /**
- * Secondary tests are for experimenting with targeting.
+ * Targeting tests are for experimenting with targeting rules.
  * It is not a message test and should not affect what the user sees once they're in a test.
  * But we do need to carry the test/variant names through in the tracking.
  */
