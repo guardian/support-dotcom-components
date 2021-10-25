4444ef98e2569092232655b55c43913df0d6dd82
Tom Forbes <tom.forbes@theguardian.com>
2021-10-25 11:03:20 +0100

    remove logging

--- packages/server/src/tests/epics/singleContributorPropensityTest/singleContributorPropensityData.ts
+++ packages/server/src/tests/epics/singleContributorPropensityTest/singleContributorPropensityData.ts
@@ -6,21 +6,12 @@ const singleContributorPropensityIds: Set<string> = new Set<string>();
 
 const fetchSingleContributorPropensityIds = (): void => {
     logInfo('Loading singleContributorPropensityIds...');
-    logInfo(`MEMORY1: ${process.memoryUsage().heapUsed / 1024 / 1024}`);
     streamS3DataByLine(
         'support-admin-console',
         `${isProd ? 'PROD' : 'CODE'}/single-contributor-propensity-test/ids.txt`,
-        line => {
-            if (singleContributorPropensityIds.size % 10000 === 0) {
-                logInfo(
-                    `...got ${singleContributorPropensityIds.size} singleContributorPropensityIds`,
-                );
-            }
-            singleContributorPropensityIds.add(line);
-        },
+        line => singleContributorPropensityIds.add(line),
         () => {
             logInfo(`Loaded ${singleContributorPropensityIds.size} singleContributorPropensityIds`);
-            logInfo(`MEMORY2: ${process.memoryUsage().heapUsed / 1024 / 1024}`);
         },
     );
 };
