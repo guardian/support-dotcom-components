afc9ad3d943cef54e1b873b307ef86f17c9e712d
Tom Pretty <tompretty2@gmail.com>
2021-11-09 16:12:12 +0000

    wip -  create PaymentIntent

--- packages/modules/src/modules/epics/ContributionsEpicPaymentRequestButton.tsx
+++ packages/modules/src/modules/epics/ContributionsEpicPaymentRequestButton.tsx
@@ -2,12 +2,14 @@ import React, { useState, useEffect } from 'react';
 import { Elements, PaymentRequestButtonElement, useStripe } from '@stripe/react-stripe-js';
 import { loadStripe, PaymentRequest, CanMakePaymentResult } from '@stripe/stripe-js';
 
+const STRIPE_KEY = 'pk_live_auSwLB4KBzbN3JOUVHvKMe6f';
+
 const Container: React.FC = () => {
-    const [stripe] = useState(loadStripe('pk_live_auSwLB4KBzbN3JOUVHvKMe6f'));
+    const [stripe] = useState(loadStripe(STRIPE_KEY));
 
     return (
         <Elements stripe={stripe}>
-            <ContributionsEpicPaymentRequestButton />;
+            <ContributionsEpicPaymentRequestButton />
         </Elements>
     );
 };
@@ -41,6 +43,30 @@ const ContributionsEpicPaymentRequestButton: React.FC = () => {
             });
         }
     }, [stripe]);
+
+    useEffect(() => {
+        if (paymentRequest) {
+            console.log('[PRB]: creating payment intent');
+
+            const URL = 'https://support.thegulocal.com/stripe/create-setup-intent/prb';
+
+            fetch(URL, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    stripePublicKey: STRIPE_KEY,
+                }),
+            }).catch((err: unknown) => {
+                if (err instanceof Error) {
+                    console.log(`[PRB]: error creating payment intent - ${err.message}`);
+                } else {
+                    console.log('[PRB]: error creating payment intent');
+                }
+            });
+        }
+    }, [paymentRequest]);
 
     return (
         <div>
