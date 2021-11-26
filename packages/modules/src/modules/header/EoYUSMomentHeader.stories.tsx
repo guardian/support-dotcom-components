dadb38664269af830c9cd507024e1bf4be4496a4
Maria Olanipekun <m.olanipekun@hotmail.com>
2021-11-26 15:55:49 +0000

    update showing subscribe button in header logic
    Co-authored-by: Samantha Gottlieb <samanthagottlieb@users.noreply.github.com>

--- packages/modules/src/modules/header/EoYUSMomentHeader.stories.tsx
+++ packages/modules/src/modules/header/EoYUSMomentHeader.stories.tsx
@@ -8,8 +8,9 @@ import { brand } from '@guardian/src-foundations';
 const givingTuesdayStart = new Date('2021-11-29T17:00:00'); //remove "Subscribe" Monday 12:00 PM EST
 const givingTuesdayEnd = new Date('2021-11-01T09:00:00'); //re-add "Subscribe" on Wednesday morning GMT
 const currentDateTime = new Date();
-const shouldShowSubscribeButton =
-    givingTuesdayStart <= currentDateTime && currentDateTime <= givingTuesdayEnd;
+const shouldShowSubscribeButton = !(
+    currentDateTime >= givingTuesdayStart && currentDateTime <= givingTuesdayEnd
+);
 
 export const props: HeaderProps = {
     content: {
--- packages/server/src/tests/header/headerSelection.ts
+++ packages/server/src/tests/header/headerSelection.ts
@@ -31,8 +31,9 @@ const nonSupportersTestUS = (): HeaderTest => {
     const givingTuesdayStart = new Date('2021-11-29T17:00:00'); //remove "Subscribe" Monday 12:00 PM EST
     const givingTuesdayEnd = new Date('2021-11-01T09:00:00'); //re-add "Subscribe" on Wednesday morning GMT
     const currentDateTime = new Date();
-    const shouldShowSubscribeButton =
-        givingTuesdayStart <= currentDateTime && currentDateTime <= givingTuesdayEnd;
+    const shouldShowSubscribeButton = !(
+        currentDateTime >= givingTuesdayStart && currentDateTime <= givingTuesdayEnd
+    );
 
     return {
         name: 'RemoteRrHeaderLinksTest__USEOY',
