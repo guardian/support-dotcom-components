f3d7d5bc8f4a12cc5c31defb91d2d9f4df971d2c
Tom Forbes <tom.forbes@theguardian.com>
2022-01-04 08:32:05 +0000

    rollup WIP

---
+++ packages/dotcom/rollup.config.js
@@ -0,0 +1,44 @@
+import babel from '@rollup/plugin-babel';
+import { DEFAULT_EXTENSIONS } from '@babel/core';
+import filesize from 'rollup-plugin-filesize';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import replace from '@rollup/plugin-replace';
+import { terser } from 'rollup-plugin-terser';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import pkg from './package.json';
+import visualizer from 'rollup-plugin-visualizer';
+
+const extensions = [...DEFAULT_EXTENSIONS, '.ts', '.tsx'];
+
+export default [
+    {
+        input: './src/index.ts',
+        output: [
+            {
+                file: pkg.main,
+                format: 'cjs',
+            },
+            {
+                file: pkg.module,
+                format: 'esm',
+                sourcemap: false,
+            },
+        ],
+        plugins: [
+            peerDepsExternal(),
+            resolve({ extensions: extensions }),
+            commonjs(),
+            replace({ 'process.env.NODE_ENV': '"production"' }),
+            babel({
+                babelHelpers: 'bundled',
+                extensions: extensions,
+            }),
+            terser(),
+            filesize(),
+            // Note, visualizer is useful for *relative* sizes, but reports
+            // pre-minification.
+            visualizer({ gzipSize: true }),
+        ],
+    }
+]
--- packages/dotcom/src/index.ts
+++ packages/dotcom/src/index.ts
@@ -7,6 +7,6 @@
 // export * from '../../shared/src/lib/history';
 // export { HeaderTargeting } from '../../shared/src/types/header';
 // Must import the actual code, not from @sdc/shared/types
-export { EpicPayload } from '../../shared/src/types/epic';
+// export { EpicPayload } from '../../shared/src/types/epic';
 // export { EpicPayload } from '@sdc/shared/types';
 export { getViewLog, logView } from '../../shared/src/lib/viewLog';
