7b01d33884735f56cd7366a0ee25017226221746
Rik Roots <rik.roots@gmail.com>
2022-01-05 14:10:01 +0000

    updating Jest tests

--- packages/server/src/tests/header/headerSelection.test.ts
+++ packages/server/src/tests/header/headerSelection.test.ts
@@ -2,106 +2,111 @@ import { header } from '@sdc/shared/config';
 import { HeaderTargeting, HeaderTest, HeaderTestSelection, HeaderVariant } from '@sdc/shared/types';
 import { selectBestTest } from './headerSelection';
 
+const modulePathBuilder = header.endpointPathBuilder;
+
+const remote_nonUK: HeaderTest = {
+    name: 'RemoteRrHeaderLinksTest__NonUK',
+    userCohort: 'AllNonSupporters',
+    isOn: true,
+    locations: [
+        'AUDCountries',
+        'Canada',
+        'EURCountries',
+        'NZDCountries',
+        'UnitedStates',
+        'International',
+    ],
+    variants: [
+        {
+            name: 'remote',
+            modulePathBuilder,
+            content: {
+                heading: 'Support the Guardian',
+                subheading: 'Available for everyone, funded by readers',
+                primaryCta: {
+                    url: 'https://support.theguardian.com/contribute',
+                    text: 'Contribute',
+                },
+                secondaryCta: {
+                    url: 'https://support.theguardian.com/subscribe',
+                    text: 'Subscribe',
+                },
+            },
+        },
+    ],
+};
+const remote_UK: HeaderTest = {
+    name: 'RemoteRrHeaderLinksTest__UK',
+    userCohort: 'AllNonSupporters',
+    isOn: true,
+    locations: ['GBPCountries'],
+    variants: [
+        {
+            name: 'remote',
+            modulePathBuilder,
+            content: {
+                heading: 'Support the Guardian',
+                subheading: 'Available for everyone, funded by readers',
+                primaryCta: {
+                    url: 'https://support.theguardian.com/subscribe',
+                    text: 'Subscribe',
+                },
+                secondaryCta: {
+                    url: 'https://support.theguardian.com/contribute',
+                    text: 'Contribute',
+                },
+            },
+        },
+    ],
+};
+const header_supporter: HeaderTest = {
+    name: 'header-supporter',
+    userCohort: 'AllExistingSupporters',
+    isOn: true,
+    locations: [
+        'AUDCountries',
+        'Canada',
+        'EURCountries',
+        'GBPCountries',
+        'NZDCountries',
+        'UnitedStates',
+        'International',
+    ],
+    variants: [
+        {
+            name: 'control',
+            modulePathBuilder,
+            content: {
+                heading: 'Thank you',
+                subheading: 'Your support powers our independent journalism',
+            },
+        },
+    ],
+};
+
+// Handle null returns - tests will still fail if presented with this but should give better indication of why test failed
+interface NullReturn {
+    name: string;
+}
+
+const testHasReturnedNull: NullReturn = {
+    name: 'test returned is null',
+};
+
+const variantHasReturnedNull: NullReturn = {
+    name: 'variant returned is null',
+};
+
+// Local testing - to make sure the selectBestTest algorithm isn't relying on the order in which header test objects are presented to it, to select the test we expect it to select for each of the Jest tests below, vary the order of header test objects in the mockTests array before running `yarn test`
+const mockTests: HeaderTest[] = [remote_nonUK, remote_UK, header_supporter];
+// const mockTests: HeaderTest[] = [remote_nonUK, header_supporter, remote_UK];
+// const mockTests: HeaderTest[] = [remote_UK, remote_nonUK, header_supporter];
+// const mockTests: HeaderTest[] = [remote_UK, header_supporter, remote_nonUK];
+// const mockTests: HeaderTest[] = [header_supporter, remote_nonUK, remote_UK];
+// const mockTests: HeaderTest[] = [header_supporter, remote_UK, remote_nonUK];
+
 describe('selectBestTest', () => {
-    it('should return an object including a HeaderTest and a HeaderVariant', () => {
-        // Mock HeaderTest[] array, as selectBestTest() expects an array which has already had the hardcoded tests (which we are testing against here) added to it
-        const modulePathBuilder = header.endpointPathBuilder;
-
-        const mockTests: HeaderTest[] = [
-            {
-                name: 'RemoteRrHeaderLinksTest__NonUK',
-                userCohort: 'AllNonSupporters',
-                isOn: true,
-                locations: [
-                    'AUDCountries',
-                    'Canada',
-                    'EURCountries',
-                    'NZDCountries',
-                    'UnitedStates',
-                    'International',
-                ],
-                variants: [
-                    {
-                        name: 'remote',
-                        modulePathBuilder,
-                        content: {
-                            heading: 'Support the Guardian',
-                            subheading: 'Available for everyone, funded by readers',
-                            primaryCta: {
-                                url: 'https://support.theguardian.com/contribute',
-                                text: 'Contribute',
-                            },
-                            secondaryCta: {
-                                url: 'https://support.theguardian.com/subscribe',
-                                text: 'Subscribe',
-                            },
-                        },
-                    },
-                ],
-            },
-            {
-                name: 'RemoteRrHeaderLinksTest__UK',
-                userCohort: 'AllNonSupporters',
-                isOn: true,
-                locations: ['GBPCountries'],
-                variants: [
-                    {
-                        name: 'remote',
-                        modulePathBuilder,
-                        content: {
-                            heading: 'Support the Guardian',
-                            subheading: 'Available for everyone, funded by readers',
-                            primaryCta: {
-                                url: 'https://support.theguardian.com/subscribe',
-                                text: 'Subscribe',
-                            },
-                            secondaryCta: {
-                                url: 'https://support.theguardian.com/contribute',
-                                text: 'Contribute',
-                            },
-                        },
-                    },
-                ],
-            },
-            {
-                name: 'header-supporter',
-                userCohort: 'AllExistingSupporters',
-                isOn: true,
-                locations: [
-                    'AUDCountries',
-                    'Canada',
-                    'EURCountries',
-                    'GBPCountries',
-                    'NZDCountries',
-                    'UnitedStates',
-                    'International',
-                ],
-                variants: [
-                    {
-                        name: 'control',
-                        modulePathBuilder,
-                        content: {
-                            heading: 'Thank you',
-                            subheading: 'Your support powers our independent journalism',
-                        },
-                    },
-                ],
-            },
-        ];
-
-        // Handle null returns - tests will still fail if presented with this but should give better indication of why test failed
-        interface NullReturn {
-            name: string;
-        }
-
-        const testHasReturnedNull: NullReturn = {
-            name: 'test returned is null',
-        };
-
-        const variantHasReturnedNull: NullReturn = {
-            name: 'variant returned is null',
-        };
-
+    it('It should return a non-UK non-supporter header test', () => {
         // Mock targeting data: not a supporter, not in UK
         const mockTargetingObject_1: HeaderTargeting = {
             showSupportMessaging: true,
@@ -130,7 +135,8 @@ describe('selectBestTest', () => {
         expect(result_1_test.name).toBe('RemoteRrHeaderLinksTest__NonUK');
         expect(result_1_variant).toHaveProperty('name');
         expect(result_1_variant.name).toBe('remote');
-
+    });
+    it('It should return a non-UK supporter header test', () => {
         // Mock targeting data: is a supporter, not in UK
         const mockTargetingObject_2: HeaderTargeting = {
             showSupportMessaging: false,
@@ -159,7 +165,8 @@ describe('selectBestTest', () => {
         expect(result_2_test.name).toBe('header-supporter');
         expect(result_2_variant).toHaveProperty('name');
         expect(result_2_variant.name).toBe('control');
-
+    });
+    it('It should return a UK-based non-supporter header test', () => {
         // Mock targeting data: not a supporter, is in UK
         const mockTargetingObject_3: HeaderTargeting = {
             showSupportMessaging: true,
@@ -188,7 +195,8 @@ describe('selectBestTest', () => {
         expect(result_3_test.name).toBe('RemoteRrHeaderLinksTest__UK');
         expect(result_3_variant).toHaveProperty('name');
         expect(result_3_variant.name).toBe('remote');
-
+    });
+    it('It should return a UK-based supporter header test', () => {
         // Mock targeting data: is a supporter, is in UK
         const mockTargetingObject_4: HeaderTargeting = {
             showSupportMessaging: false,
