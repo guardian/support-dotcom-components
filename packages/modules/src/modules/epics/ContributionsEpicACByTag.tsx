3a0debd3e9c8f5e4374739a99f01ddd8b47440f5
Tom Forbes <tom.forbes@theguardian.com>
2022-01-14 11:59:08 +0000

    Remove AC-by-tag epic

--- packages/modules/src/modules/epics/ContributionsEpicACByTag.tsx
+++
@@ -1,8 +0,0 @@
-import { getContributionsEpic, validate } from './ContributionsEpic';
-import { EpicProps } from '@sdc/shared/dist/types';
-import { withParsedProps } from '../shared/ModuleWrapper';
-
-export const ContributionsEpic: React.FC<EpicProps> = withParsedProps(
-    getContributionsEpic(true),
-    validate,
-);
