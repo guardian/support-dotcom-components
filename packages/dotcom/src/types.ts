0282cf375644426d0a0c9fe62f7a9186fc1e3fed
Tom Forbes <tom.forbes@theguardian.com>
2022-01-20 14:07:26 +0000

    Export specific targeting types

--- packages/dotcom/src/types.ts
+++ packages/dotcom/src/types.ts
@@ -1,2 +1,7 @@
-export * from '../../shared/src/types/targeting';
+export {
+    BannerPayload,
+    EpicPayload,
+    HeaderPayload,
+    WeeklyArticleHistory,
+} from '../../shared/src/types/targeting';
 export { ModuleDataResponse, ModuleData } from './requests';
