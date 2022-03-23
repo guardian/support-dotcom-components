581ae75a412f06a5091c46751dd29441e378d629
Tom Forbes <tom.forbes@theguardian.com>
2022-03-23 11:42:24 +0000

    Remove variant2 from banner deploy test

--- packages/server/src/tests/banners/bannerTargetingTests.ts
+++ packages/server/src/tests/banners/bannerTargetingTests.ts
@@ -1,10 +1,6 @@
 import { TargetingTest } from '../../lib/targetingTesting';
 import { BannerTargeting } from '@sdc/shared/types';
 
-const isNetworkFront = (targeting: BannerTargeting): boolean =>
-    // https://github.com/guardian/frontend/blob/5b970cd7308175cfc1bcae2d4fb8c06ee13c5fa0/common/app/model/DotcomContentType.scala#L33
-    targeting.contentType?.toLowerCase() === 'network front';
-
 export const bannerTargetingTests: TargetingTest<BannerTargeting>[] = [
     {
         name: '2022-03-18_BannerTargeting_PageView',
@@ -20,14 +16,6 @@ export const bannerTargetingTests: TargetingTest<BannerTargeting>[] = [
                 name: 'variant1',
                 // show on/after 1st article view of the day
                 canShow: (targeting: BannerTargeting) => (targeting.articleCountToday || 0) >= 1,
-            },
-            {
-                name: 'variant2',
-                // show after 1st article view of the day
-                canShow: (targeting: BannerTargeting): boolean => {
-                    const count = targeting.articleCountToday || 0;
-                    return count >= 2 || (count >= 1 && isNetworkFront(targeting));
-                },
             },
         ],
     },
