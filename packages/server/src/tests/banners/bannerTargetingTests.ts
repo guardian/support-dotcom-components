0316e945efc9747cb31f0f29abfaca122c598ba3
Tom Forbes <tom.forbes@theguardian.com>
2022-04-08 09:18:13 +0100

    Remove banner pageview targeting test (#677)
    * Remove banner pageview targeting test
    
    * delete tests

--- packages/server/src/tests/banners/bannerTargetingTests.ts
+++ packages/server/src/tests/banners/bannerTargetingTests.ts
@@ -1,22 +1,4 @@
 import { TargetingTest } from '../../lib/targetingTesting';
 import { BannerTargeting } from '@sdc/shared/types';
 
-export const bannerTargetingTests: TargetingTest<BannerTargeting>[] = [
-    {
-        name: '2022-03-18_BannerTargeting_PageView',
-        // Exclude browsers that have not consented to article counting
-        canInclude: (targeting: BannerTargeting) => targeting.articleCountToday !== undefined,
-        variants: [
-            {
-                name: 'control',
-                // show banner on first page view after redeploy
-                canShow: () => true,
-            },
-            {
-                name: 'variant1',
-                // show on/after 1st article view of the day
-                canShow: (targeting: BannerTargeting) => (targeting.articleCountToday || 0) >= 1,
-            },
-        ],
-    },
-];
+export const bannerTargetingTests: TargetingTest<BannerTargeting>[] = [];
