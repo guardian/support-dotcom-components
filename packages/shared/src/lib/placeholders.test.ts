30332b654bdc2bc4927f28e5568a99d7066c67d2
Tom Forbes <tom.forbes@theguardian.com>
2022-04-21 14:33:34 +0100

    Optimise template substitution

--- packages/shared/src/lib/placeholders.test.ts
+++ packages/shared/src/lib/placeholders.test.ts
@@ -1,4 +1,5 @@
-import { containsNonArticleCountPlaceholder } from './placeholders';
+import {containsNonArticleCountPlaceholder, replaceNonArticleCountPlaceholders} from './placeholders';
+import {Prices} from "../types";
 
 describe('containsNonArticleCountPlaceholder', () => {
     it('returns true if string contains placeholder (that is not %%ARTICLE_COUNT%%)', () => {
@@ -21,5 +22,145 @@ describe('containsNonArticleCountPlaceholder', () => {
     it('returns false if string does not contain placeholder', () => {
         const got = containsNonArticleCountPlaceholder('apple without placeholder');
         expect(got).toBe(false);
+    });
+});
+
+describe('replaceNonArticleCountPlaceholders', () => {
+    const prices: Prices = {
+        GBPCountries: {
+            GuardianWeekly: {
+                Monthly: {
+                    price: '0.00',
+                },
+                Annual: {
+                    price: '0.00',
+                },
+            },
+            Digisub: {
+                Monthly: {
+                    price: '0.00',
+                },
+                Annual: {
+                    price: '0.00',
+                },
+            },
+        },
+        UnitedStates: {
+            GuardianWeekly: {
+                Monthly: {
+                    price: '0.00',
+                },
+                Annual: {
+                    price: '0.00',
+                },
+            },
+            Digisub: {
+                Monthly: {
+                    price: '0.00',
+                },
+                Annual: {
+                    price: '0.00',
+                },
+            },
+        },
+        EURCountries: {
+            GuardianWeekly: {
+                Monthly: {
+                    price: '0.00',
+                },
+                Annual: {
+                    price: '0.00',
+                },
+            },
+            Digisub: {
+                Monthly: {
+                    price: '0.00',
+                },
+                Annual: {
+                    price: '0.00',
+                },
+            },
+        },
+        AUDCountries: {
+            GuardianWeekly: {
+                Monthly: {
+                    price: '0.00',
+                },
+                Annual: {
+                    price: '0.00',
+                },
+            },
+            Digisub: {
+                Monthly: {
+                    price: '0.00',
+                },
+                Annual: {
+                    price: '0.00',
+                },
+            },
+        },
+        International: {
+            GuardianWeekly: {
+                Monthly: {
+                    price: '0.00',
+                },
+                Annual: {
+                    price: '0.00',
+                },
+            },
+            Digisub: {
+                Monthly: {
+                    price: '0.00',
+                },
+                Annual: {
+                    price: '0.00',
+                },
+            },
+        },
+        NZDCountries: {
+            GuardianWeekly: {
+                Monthly: {
+                    price: '0.00',
+                },
+                Annual: {
+                    price: '0.00',
+                },
+            },
+            Digisub: {
+                Monthly: {
+                    price: '0.00',
+                },
+                Annual: {
+                    price: '0.00',
+                },
+            },
+        },
+        Canada: {
+            GuardianWeekly: {
+                Monthly: {
+                    price: '0.00',
+                },
+                Annual: {
+                    price: '0.00',
+                },
+            },
+            Digisub: {
+                Monthly: {
+                    price: '0.00',
+                },
+                Annual: {
+                    price: '0.00',
+                },
+            },
+        },
+    };
+
+    it('replaces %%COUNTRY_NAME%%', () => {
+        const result = replaceNonArticleCountPlaceholders(
+            'You live in %%COUNTRY_NAME%%.',
+            'IT',
+            prices,
+        );
+        expect(result).toBe('You live in Italy.');
     });
 });
--- packages/shared/src/lib/placeholders.ts
+++ packages/shared/src/lib/placeholders.ts
@@ -1,9 +1,9 @@
 import { getCountryName, getLocalCurrencySymbol, countryCodeToCountryGroupId } from './geolocation';
-import { Prices } from '../types/prices';
+import { GuardianProduct, Prices, RatePlan } from '../types/prices';
 
 // we have to treat %%ARTICLE_COUNT%% placeholders specially as they are replaced
 // with react components, not a simple text substitution
-export const replaceNonArticleCountPlaceholders = (
+export const replaceNonArticleCountPlaceholders2 = (
     content: string | undefined,
     countryCode?: string,
     prices?: Prices,
@@ -44,6 +44,59 @@ export const replaceNonArticleCountPlaceholders = (
         );
     }
     return content;
+};
+
+interface PlaceholderRule {
+    placeholder: string;
+    substitute: () => string | undefined;
+}
+const buildRule = (placeholder: string, substitute: () => string | undefined): PlaceholderRule => ({
+    placeholder,
+    substitute,
+});
+
+export const replaceNonArticleCountPlaceholders = (
+    content: string | undefined,
+    countryCode?: string,
+    prices?: Prices,
+): string => {
+    if (!content) {
+        return '';
+    }
+
+    const localCurrencySymbol = getLocalCurrencySymbol(countryCode);
+    const countryGroupId = countryCodeToCountryGroupId(countryCode);
+    const localPrices = prices && prices[countryGroupId] ? prices[countryGroupId] : null;
+
+    const priceSubstitution = (product: GuardianProduct, ratePlan: RatePlan) => {
+        if (localPrices) {
+            return `${localCurrencySymbol}${localPrices[product][ratePlan].price}`;
+        }
+        return undefined;
+    };
+    const rules: PlaceholderRule[] = [
+        buildRule('CURRENCY_SYMBOL', () => localCurrencySymbol),
+        buildRule('COUNTRY_NAME', () => getCountryName(countryCode)),
+        buildRule('PRICE_DIGISUB_MONTHLY', () => priceSubstitution('Digisub', 'Monthly')),
+        buildRule('PRICE_DIGISUB_ANNUAL', () => priceSubstitution('Digisub', 'Annual')),
+        buildRule('PRICE_GUARDIANWEEKLY_MONTHLY', () =>
+            priceSubstitution('GuardianWeekly', 'Monthly'),
+        ),
+        buildRule('PRICE_GUARDIANWEEKLY_ANNUAL', () =>
+            priceSubstitution('GuardianWeekly', 'Annual'),
+        ),
+    ];
+
+    content.replace(PLACEHOLDER_RE, placeholder => {
+        const rule = rules.find(rule => `%%${rule.placeholder}%%` === placeholder);
+        if (rule) {
+            const result = rule.substitute();
+            if (result) {
+                return result;
+            }
+        }
+        return placeholder; // leave it unchanged - the calling code must handle this
+    });
 };
 
 // Nb. don't attempt to use lookbehind (?<!) here, as IE 11 will break alas
