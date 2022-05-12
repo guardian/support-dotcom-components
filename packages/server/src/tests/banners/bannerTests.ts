c3b11813e5fb35bf1787f618077329382b456698
Tom Forbes <tom.forbes@theguardian.com>
2022-05-12 14:28:56 +0100

    use the test

--- packages/server/src/tests/banners/bannerTests.ts
+++ packages/server/src/tests/banners/bannerTests.ts
@@ -5,6 +5,7 @@ import {
     channel2BannersAllTestsGenerator,
 } from './ChannelBannerTests';
 import { DefaultContributionsBanner } from './DefaultContributionsBannerTest';
+import { propensityModelBannerTest } from './propensityModelTest/propensityModelTest';
 
 const defaultBannerTestGenerator: BannerTestGenerator = () =>
     Promise.resolve([DefaultContributionsBanner]);
@@ -13,6 +14,7 @@ const flattenArray = <T>(array: T[][]): T[] => ([] as T[]).concat(...array);
 
 const testGenerators: BannerTestGenerator[] = [
     channel1BannersAllTestsGenerator,
+    propensityModelBannerTest,
     channel2BannersAllTestsGenerator,
     defaultBannerTestGenerator,
 ];
