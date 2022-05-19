e27dd712ff005890b41a582d069a1eb29376c289
Tom Forbes <tom.forbes@theguardian.com>
2022-05-19 08:07:13 +0100

    Cloudwatch metrics, super mode

---
+++ packages/server/src/utils/cloudwatch.ts
@@ -0,0 +1,30 @@
+import * as AWS from 'aws-sdk';
+import { isProd } from '../lib/env';
+import { logError } from './logging';
+
+const cloudwatch = new AWS.CloudWatch({ region: 'eu-west-1' });
+
+const stage = isProd ? 'PROD' : 'CODE';
+const namespace = `support-dotcom-components-${stage}`;
+
+type Metric = 'super-mode-error' | 'channel-tests-error';
+
+// Sends a metric to cloudwatch.
+// Avoid doing this per-request, to avoid high costs. This should instead be called from within a cacheAsync
+export const putMetric = (
+    metricName: Metric,
+    dimensions: AWS.CloudWatch.Dimension[] = [],
+): void => {
+    cloudwatch
+        .putMetricData({
+            Namespace: namespace,
+            MetricData: [
+                {
+                    MetricName: metricName,
+                    Dimensions: dimensions,
+                },
+            ],
+        })
+        .promise()
+        .catch(error => logError(`Error putting cloudwatch metric: ${error}`));
+};
