13e8b05319554108e77943564577de045a8c49c2
Maël Zoungrana <mael.zoungrana@guardian.co.uk>
2022-05-20 15:50:54 +0100

    Create new DocumentClient for every fetch (global one was throwing credentials error)

--- packages/server/src/tests/testsStore.ts
+++ packages/server/src/tests/testsStore.ts
@@ -2,16 +2,12 @@ import * as AWS from 'aws-sdk';
 import { isProd } from '../lib/env';
 import { logError } from '../utils/logging';
 
-const docClient = new AWS.DynamoDB.DocumentClient({ region: 'eu-west-1' });
 const stage = isProd ? 'PROD' : 'CODE';
 
 export type ChannelTypes = 'Epic' | 'EpicAMP' | 'EpicAppleNews' | 'EpicLiveblog' | 'EpicHoldback' | 'Banner1' | 'Banner2' | 'Header';
 
-export const getTests = <T>(channel: ChannelTypes): Promise<T[]> => queryChannel(channel, stage, docClient)
-    .then(tests => {
-        console.log(tests.Items);
-        return (tests.Items ?? []) as T[];
-    })
+export const getTests = <T>(channel: ChannelTypes): Promise<T[]> => queryChannel(channel, stage)
+    .then(tests => (tests.Items ?? []) as T[])
     .catch(error => {
         logError(`Error reading tests from Dynamo: ${error.message}`);
         return [];
@@ -20,8 +16,8 @@ export const getTests = <T>(channel: ChannelTypes): Promise<T[]> => queryChannel
 function queryChannel(
     channel: ChannelTypes,
     stage: string,
-    docClient: AWS.DynamoDB.DocumentClient,
 ) {
+    const docClient = new AWS.DynamoDB.DocumentClient({ region: 'eu-west-1' });
     return docClient
         .query({
             TableName: `support-admin-console-channel-tests-${stage.toUpperCase()}`,
