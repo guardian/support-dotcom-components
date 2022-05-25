0597eecb4727741b2fac1dcd1772873b3474b495
Tom Forbes <tom.forbes@theguardian.com>
2022-05-25 15:33:30 +0100

    better

--- packages/server/src/tests/banners/propensityModelTest/propensityModelTestData.ts
+++ packages/server/src/tests/banners/propensityModelTest/propensityModelTestData.ts
@@ -3,21 +3,21 @@ import { streamS3DataByLine } from '../../../utils/S3';
 import { isProd } from '../../../lib/env';
 
 const guardianWeeklyHighPropensityIds: Set<string> = new Set<string>();
-const fetchHighPropensityIds = (): void => {
-    logInfo('Loading guardianWeeklyHighPropensityIds...');
-    streamS3DataByLine({
-        bucket: 'support-admin-console',
-        key: `${isProd ? 'PROD' : 'CODE'}/guardian-weekly-propensity-test/ids.txt`,
-        onLine: line => guardianWeeklyHighPropensityIds.add(line),
-        onComplete: () => {
-            logInfo(
-                `Loaded ${guardianWeeklyHighPropensityIds.size} guardianWeeklyHighPropensityIds`,
-            );
-        },
-    });
+export const fetchHighPropensityIds = (): void => {
+    if (guardianWeeklyHighPropensityIds.size === 0) {
+        logInfo('Loading guardianWeeklyHighPropensityIds...');
+        streamS3DataByLine({
+            bucket: 'support-admin-console',
+            key: `${isProd ? 'PROD' : 'CODE'}/guardian-weekly-propensity-test/ids.txt`,
+            onLine: line => guardianWeeklyHighPropensityIds.add(line),
+            onComplete: () => {
+                logInfo(
+                    `Loaded ${guardianWeeklyHighPropensityIds.size} guardianWeeklyHighPropensityIds`,
+                );
+            },
+        });
+    }
 };
 
 export const isInPropensityTest = (browserId: string): boolean =>
     guardianWeeklyHighPropensityIds.has(browserId);
-
-fetchHighPropensityIds();
