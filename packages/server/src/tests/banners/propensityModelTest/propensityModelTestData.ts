ecf35142329dc9ccad646b93b1ce8f4c18149901
Tom Forbes <tom.forbes@theguardian.com>
2022-05-25 15:55:08 +0100

    Load propensity test data once (#708)
    * Load popensity test data once
    
    * better

--- packages/server/src/tests/banners/propensityModelTest/propensityModelTestData.ts
+++ packages/server/src/tests/banners/propensityModelTest/propensityModelTestData.ts
@@ -4,17 +4,19 @@ import { isProd } from '../../../lib/env';
 
 const guardianWeeklyHighPropensityIds: Set<string> = new Set<string>();
 export const fetchHighPropensityIds = (): void => {
-    logInfo('Loading guardianWeeklyHighPropensityIds...');
-    streamS3DataByLine({
-        bucket: 'support-admin-console',
-        key: `${isProd ? 'PROD' : 'CODE'}/guardian-weekly-propensity-test/ids.txt`,
-        onLine: line => guardianWeeklyHighPropensityIds.add(line),
-        onComplete: () => {
-            logInfo(
-                `Loaded ${guardianWeeklyHighPropensityIds.size} guardianWeeklyHighPropensityIds`,
-            );
-        },
-    });
+    if (guardianWeeklyHighPropensityIds.size === 0) {
+        logInfo('Loading guardianWeeklyHighPropensityIds...');
+        streamS3DataByLine({
+            bucket: 'support-admin-console',
+            key: `${isProd ? 'PROD' : 'CODE'}/guardian-weekly-propensity-test/ids.txt`,
+            onLine: line => guardianWeeklyHighPropensityIds.add(line),
+            onComplete: () => {
+                logInfo(
+                    `Loaded ${guardianWeeklyHighPropensityIds.size} guardianWeeklyHighPropensityIds`,
+                );
+            },
+        });
+    }
 };
 
 export const isInPropensityTest = (browserId: string): boolean =>
