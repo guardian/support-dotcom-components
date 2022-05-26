6efb513c8c2c9f35fa52e76b4395092baca92100
Liam Duffy <3338808+liamness@users.noreply.github.com>
2022-05-26 10:18:24 +0100

    Tidy up layout

--- packages/modules/src/modules/banners/signInPrompt/SignInPromptBanner.tsx
+++ packages/modules/src/modules/banners/signInPrompt/SignInPromptBanner.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { ThemeProvider, css } from '@emotion/react';
-import { brand, brandAlt, brandText, space, neutral } from '@guardian/src-foundations';
+import { brand, brandAlt, space, neutral } from '@guardian/src-foundations';
 import { headline } from '@guardian/src-foundations/typography';
 import { Button, LinkButton, buttonBrand } from '@guardian/src-button';
 import { SvgRoundelBrandInverse } from '@guardian/src-brand';
 import { SecondaryCtaType } from '@sdc/shared/types';
+import { Container, Column, Columns } from '@guardian/src-layout';
 
 import { BannerRenderProps } from '../common/types';
 import { bannerWrapper, validatedBannerWrapper } from '../common/BannerWrapper';
@@ -13,17 +14,21 @@ const background = css`
     background-color: ${brand[400]};
 `;
 
+const mainColumn = css`
+    position: relative;
+`;
+
 const headingStyles = css`
     ${headline.medium({ fontWeight: 'bold' })}
     font-size: 32px;
     color: ${neutral[100]};
-    margin: 0;
+    margin: ${space[1]}px 0 0;
 `;
 
 const subHeadingStyles = css`
     ${headline.xxsmall({ fontWeight: 'bold' })};
     color: ${brandAlt[400]};
-    margin: 0;
+    margin: ${space[2]}px 0;
 `;
 
 const bulletStyles = css`
@@ -31,6 +36,10 @@ const bulletStyles = css`
     color: ${neutral[100]};
     display: flex;
     flex-direction: column;
+
+    span:not(:first-of-type) {
+        margin-top: 10px;
+    }
 
     span::before {
         content: '';
@@ -43,11 +52,18 @@ const bulletStyles = css`
     }
 `;
 
+const actions = css`
+    margin: ${space[5]}px 0;
+`;
+
 const closeButton = css`
     margin-left: ${space[5]}px;
 `;
 
 const logo = css`
+    position: absolute;
+    top: ${space[2]}px;
+    right: 0px;
     width: 42px;
     height: 42px;
 `;
@@ -57,31 +73,44 @@ const SignInPromptBanner: React.FC<BannerRenderProps> = props => {
     const [subheading, ...bullets] = paragraphs;
 
     return (
-        <div css={background}>
-            <h1 css={headingStyles}>{heading}</h1>
-            <h2 css={subHeadingStyles}>{subheading}</h2>
-            <div css={bulletStyles}>{bullets}</div>
-            <ThemeProvider theme={buttonBrand}>
-                {primaryCta && (
-                    <LinkButton priority="primary" href={primaryCta.ctaUrl} size="small">
-                        {primaryCta.ctaText}
-                    </LinkButton>
-                )}
-                {secondaryCta && secondaryCta.type === SecondaryCtaType.Custom && (
-                    <Button
-                        priority="subdued"
-                        size="small"
-                        onClick={props.onCloseClick}
-                        cssOverrides={closeButton}
-                    >
-                        {secondaryCta.cta.ctaText}
-                    </Button>
-                )}
-            </ThemeProvider>
-            <div css={logo}>
-                <SvgRoundelBrandInverse />
-            </div>
-        </div>
+        <Container cssOverrides={background}>
+            <Columns>
+                <Column width={[0, 0, 0, 2, 3]}> </Column>
+                <Column width={[4, 12, 12, 12, 13]} cssOverrides={mainColumn}>
+                    <h1 css={headingStyles}>{heading}</h1>
+                    <h2 css={subHeadingStyles}>{subheading}</h2>
+                    <div css={bulletStyles}>{bullets}</div>
+
+                    <div css={actions}>
+                        <ThemeProvider theme={buttonBrand}>
+                            {primaryCta && (
+                                <LinkButton
+                                    priority="primary"
+                                    href={primaryCta.ctaUrl}
+                                    size="small"
+                                >
+                                    {primaryCta.ctaText}
+                                </LinkButton>
+                            )}
+                            {secondaryCta && secondaryCta.type === SecondaryCtaType.Custom && (
+                                <Button
+                                    priority="subdued"
+                                    size="small"
+                                    onClick={props.onCloseClick}
+                                    cssOverrides={closeButton}
+                                >
+                                    {secondaryCta.cta.ctaText}
+                                </Button>
+                            )}
+                        </ThemeProvider>
+                    </div>
+
+                    <div css={logo}>
+                        <SvgRoundelBrandInverse />
+                    </div>
+                </Column>
+            </Columns>
+        </Container>
     );
 };
 
