0ac9b0c33ba95018d77ce891f392576db8a018f4
Tom Forbes <tom.forbes@theguardian.com>
2022-06-08 10:47:20 +0100

    End GW ML test v2

--- packages/server/src/tests/banners/bannerTests.ts
+++ packages/server/src/tests/banners/bannerTests.ts
@@ -5,7 +5,6 @@ import {
     channel2BannersAllTestsGenerator,
 } from './channelBannerTests';
 import { DefaultContributionsBanner } from './DefaultContributionsBannerTest';
-import { propensityModelBannerTest } from './propensityModelTest/propensityModelTest';
 
 const defaultBannerTestGenerator: BannerTestGenerator = () =>
     Promise.resolve([DefaultContributionsBanner]);
@@ -14,7 +13,6 @@ const flattenArray = <T>(array: T[][]): T[] => ([] as T[]).concat(...array);
 
 const testGenerators: BannerTestGenerator[] = [
     channel1BannersAllTestsGenerator,
-    propensityModelBannerTest,
     channel2BannersAllTestsGenerator,
     defaultBannerTestGenerator,
 ];
