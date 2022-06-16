6177e5a7624f2c558e5efa03eb37559ca9453909
Tom Forbes <tom.forbes@theguardian.com>
2022-06-16 13:58:32 +0100

    make test draft

--- packages/server/src/tests/epics/usTopReaderCopy/index.ts
+++ packages/server/src/tests/epics/usTopReaderCopy/index.ts
@@ -49,7 +49,7 @@ export const usTopReaderCopyTest: EpicTest = {
     hasCountryName: false,
     highPriority: true,
     isLiveBlog: false,
-    status: 'Live',
+    status: 'Draft',
     locations: ['UnitedStates'],
     name: 'usTopReaderCopyTest',
     sections: [],
