bae32a162f328e6e0098dbda19f853d36550a0aa
Tom Forbes <tom.forbes@theguardian.com>
2022-06-16 13:50:15 +0100

    Fix top reader config

--- packages/server/src/tests/epics/usTopReaderCopy/index.ts
+++ packages/server/src/tests/epics/usTopReaderCopy/index.ts
@@ -49,7 +49,7 @@ export const usTopReaderCopyTest: EpicTest = {
     hasCountryName: false,
     highPriority: true,
     isLiveBlog: false,
-    isOn: true,
+    status: 'Live',
     locations: ['UnitedStates'],
     name: 'usTopReaderCopyTest',
     sections: [],
