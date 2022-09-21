a478bfaaf10b011e10e8b11a90356ea8daabab28
Imogen Hardy <imogen.hardy@guardian.co.uk>
2022-09-21 11:59:54 +0100

    use react-spring for path animation for safari compatibility

--- packages/modules/src/modules/banners/climateCrisisMoment/visual/ThermometerMercury.tsx
+++ packages/modules/src/modules/banners/climateCrisisMoment/visual/ThermometerMercury.tsx
@@ -1,65 +1,77 @@
-import { css, keyframes } from '@emotion/react';
+import { css } from '@emotion/react';
 import { news } from '@guardian/src-foundations';
-import { from } from '@guardian/src-foundations/mq';
+import { between, from } from '@guardian/src-foundations/mq';
 import React from 'react';
+import { animated, useReducedMotion, useSpring } from 'react-spring';
 
-export function ThermometerMercury(): JSX.Element {
-    return <path css={mercuryStyles} fill={news[300]} />;
+type MercuryHookProps = {
+    from: string;
+    to: string;
+};
+
+function useRisingMercury({ from, to }: MercuryHookProps) {
+    const reduceMotion = useReducedMotion();
+    return useSpring({
+        from: { d: from },
+        d: to,
+        delay: 1000,
+        config: {
+            duration: 5000,
+        },
+        immediate: reduceMotion,
+    });
 }
 
-const riseDesktop = keyframes`
-    0% {
-        d: path('M83,145 h22 v110 H83 v-110 Z');
-    }
-    100% {
-        d: path('M83, 10 h22 v200 H83 v-110 Z');
-    }
-`;
+function MercuryMobile() {
+    const { d } = useRisingMercury({
+        from: 'M41 0 V50 h8 v65 h-8 z',
+        to: 'M41 0 V5 h8 v65 h-8 z',
+    });
+    return <animated.path d={d} css={mobileStyles} fill={news[300]} />;
+}
 
-const riseTablet = keyframes`
-    0% {
-        d: path('M68 145 h15 v65 H68 V59 Z');
-    }
-    100% {
-        d: path('M68 10 h15 v165 H68 V59 Z');
-    }
-`;
+function MercuryTablet() {
+    const { d } = useRisingMercury({
+        from: 'M68 145 h15 v65 H68 V59 Z',
+        to: 'M68 10 h15 v165 H68 V59 Z',
+    });
+    return <animated.path d={d} css={tabletStyles} fill={news[300]} />;
+}
 
-const riseMobile = keyframes`
-    0% {
-        d: path('M41 0 V50 h8 v65 h-8 z');
-    }
-    100% {
-        d: path('M41 0 V5 h8 v65 h-8 z');
-    }
-`;
+function MercuryDesktop() {
+    const { d } = useRisingMercury({
+        from: 'M83,145 h22 v110 H83 v-110 Z',
+        to: 'M83, 10 h22 v200 H83 v-110 Z',
+    });
+    return <animated.path d={d} css={desktopStyles} fill={news[300]} />;
+}
 
-const mercuryStyles = css`
-    d: path('M41 0 V50 h8 v65 h-8 z');
-    animation: ${riseMobile} 5s ease-in;
-    animation-delay: 1s;
-    animation-fill-mode: forwards;
+export function ThermometerMercury(): JSX.Element {
+    return (
+        <>
+            <MercuryMobile />
+            <MercuryTablet />
+            <MercuryDesktop />
+        </>
+    );
+}
 
+const mobileStyles = css`
     ${from.tablet} {
-        d: path('M68 145 h15 v65 H68 V59 Z');
-        animation-name: ${riseTablet};
+        display: none;
     }
+`;
 
+const tabletStyles = css`
+    display: none;
+    ${between.tablet.and.desktop} {
+        display: block;
+    }
+`;
+
+const desktopStyles = css`
+    display: none;
     ${from.desktop} {
-        d: path('M83 145 h22 v110 H83 v-110 Z');
-        animation-name: ${riseDesktop};
-    }
-
-    @media (prefers-reduced-motion) {
-        animation: none !important;
-        d: path('M41 0 V30 h8 v65 h-8 z');
-
-        ${from.tablet} {
-            d: path('M68 59 h15 v107 H68 V59 Z');
-        }
-
-        ${from.desktop} {
-            d: path('M83 64 h22 v110 H83 v-110 Z');
-        }
+        display: block;
     }
 `;
