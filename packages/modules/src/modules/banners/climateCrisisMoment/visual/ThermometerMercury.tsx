6c6899644891212d388ade2b5662c8f1aa815e6e
Imogen Hardy <imogen.hardy@guardian.co.uk>
2022-09-21 16:15:57 +0100

    improve reducemotion handling

--- packages/modules/src/modules/banners/climateCrisisMoment/visual/ThermometerMercury.tsx
+++ packages/modules/src/modules/banners/climateCrisisMoment/visual/ThermometerMercury.tsx
@@ -7,13 +7,17 @@ import { animated, useReducedMotion, useSpring } from 'react-spring';
 type MercuryHookProps = {
     from: string;
     to: string;
+    fallback: string;
 };
 
-function useRisingMercury({ from, to }: MercuryHookProps) {
+function useRisingMercury({ from, to, fallback }: MercuryHookProps) {
     const reduceMotion = useReducedMotion();
+    const initial = reduceMotion ? fallback : from;
+    const final = reduceMotion ? fallback : to;
+
     return useSpring({
-        from: { d: from },
-        d: to,
+        from: { d: initial },
+        d: final,
         delay: 1000,
         config: {
             duration: 5000,
@@ -26,6 +30,7 @@ function MercuryMobile() {
     const { d } = useRisingMercury({
         from: 'M41 0 V50 h8 v65 h-8 z',
         to: 'M41 0 V5 h8 v65 h-8 z',
+        fallback: 'M41 0 V30 h8 v65 h-8 z',
     });
     return <animated.path d={d} css={mobileStyles} fill={news[300]} />;
 }
@@ -34,6 +39,7 @@ function MercuryTablet() {
     const { d } = useRisingMercury({
         from: 'M68 145 h15 v65 H68 V59 Z',
         to: 'M68 10 h15 v165 H68 V59 Z',
+        fallback: 'M68 59 h15 v107 H68 V59 Z',
     });
     return <animated.path d={d} css={tabletStyles} fill={news[300]} />;
 }
@@ -42,6 +48,7 @@ function MercuryDesktop() {
     const { d } = useRisingMercury({
         from: 'M83,145 h22 v110 H83 v-110 Z',
         to: 'M83, 10 h22 v200 H83 v-110 Z',
+        fallback: 'M83 64 h22 v110 H83 v-110 Z',
     });
     return <animated.path d={d} css={desktopStyles} fill={news[300]} />;
 }
