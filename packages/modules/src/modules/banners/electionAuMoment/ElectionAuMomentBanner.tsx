1f9abc8b71e6adda595a0e6ca4f050d7fa7d7c96
Tom Forbes <tom.forbes@theguardian.com>
2022-10-14 08:27:50 +0100

    Delete AU election banners (#773)
    * Delete AU election banners
    
    * tidy

--- packages/modules/src/modules/banners/electionAuMoment/ElectionAuMomentBanner.tsx
+++
@@ -1,9 +0,0 @@
-import { bannerWrapper, validatedBannerWrapper } from '../common/BannerWrapper';
-import getElectionAuMomentBanner from './components/getElectionAuMomentBanner';
-
-const ElectionAuMomentBanner = getElectionAuMomentBanner();
-
-const unvalidated = bannerWrapper(ElectionAuMomentBanner, 'election-au-moment-banner');
-const validated = validatedBannerWrapper(ElectionAuMomentBanner, 'election-au-moment-banner');
-
-export { validated as ElectionAuMomentBanner, unvalidated as ElectionAuMomentBannerUnvalidated };
