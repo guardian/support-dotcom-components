4111748e4ac09c6bf88c20a3f735491c256c7075
Maël Zoungrana <mael.zoungrana@guardian.co.uk>
2022-11-04 16:53:05 +0000

    Changed reminder fields for US reader for Giving Tuesday 2022 campaign

--- packages/shared/src/lib/reminderFields.ts
+++ packages/shared/src/lib/reminderFields.ts
@@ -29,6 +29,18 @@ export const buildReminderFields = (today: Date = new Date()): ReminderFields =>
     };
 };
 
-export const getReminderFields = (variant: EpicVariant): ReminderFields | undefined => {
-    return variant.showReminderFields ?? buildReminderFields();
+export const GIVING_TUESDAY_REMINDER_FIELDS: ReminderFields = {
+    reminderCta: 'Remind me on Giving Tuesday',
+    reminderPeriod: '2022-11-01',
+    reminderLabel: 'on Giving Tuesday',
+    reminderOption: 'giving-tuesday-2022',
+};
+
+export const getReminderFields = (
+    variant: EpicVariant,
+    countryCode?: string,
+): ReminderFields | undefined => {
+    return variant.showReminderFields ?? countryCode === 'US'
+        ? GIVING_TUESDAY_REMINDER_FIELDS
+        : buildReminderFields();
 };
