ba055433bae07b29de9bc0f3790db25b52b54a75
Maria Olanipekun <m.olanipekun@hotmail.com>
2022-11-16 11:21:34 +0000

    update image urls

--- packages/modules/src/modules/banners/usEoyMoment/UsEoyMomentBanner.stories.tsx
+++ packages/modules/src/modules/banners/usEoyMoment/UsEoyMomentBanner.stories.tsx
@@ -51,9 +51,9 @@ const UsEoyMomentBanner = bannerWrapper(
         },
         imageSettings: {
             mainUrl:
-                'https://media.guim.co.uk/56d31914ea2f5bb335c4a2b69c6711903e999c86/0_0_820_932/820.png',
+                'https://i.guim.co.uk/img/media/56d31914ea2f5bb335c4a2b69c6711903e999c86/0_0_820_932/820.png?quality=85&s=e47ec639fdaf420ac3c0be91cda686ed',
             mobileUrl:
-                'https://media.guim.co.uk/2494b0dd21a753c373fcb85c26d4c461e13c6b5b/149_0_1195_588/500.png',
+                'https://i.guim.co.uk/img/media/2494b0dd21a753c373fcb85c26d4c461e13c6b5b/149_0_1195_588/500.png?quality=85&s=3a55e291549838bfacbcbb495ca5be6b',
             altText: 'The United States Capitol Building',
         },
         bannerId: 'us-eoy-banner',
