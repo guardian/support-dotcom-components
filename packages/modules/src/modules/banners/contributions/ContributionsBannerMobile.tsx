3e46709533d3d504d6ab80f0270bdeee703cab69
Rik Roots <rik.roots@gmail.com>
2022-12-08 14:23:12 +0000

    remove commented out code

--- packages/modules/src/modules/banners/contributions/ContributionsBannerMobile.tsx
+++ packages/modules/src/modules/banners/contributions/ContributionsBannerMobile.tsx
@@ -38,13 +38,6 @@ const styles = {
         margin-top: 2px;
         padding: 0 ${space[3]}px;
     `,
-    // ctasContainer: css`
-    //     padding: 0 ${space[3]}px;
-
-    //     & > * + * {
-    //         margin-top: ${space[3]}px;
-    //     }
-    // `,
     ctasContainer: css`
         display: flex;
         flex-direction: row;
@@ -55,12 +48,6 @@ const styles = {
             margin-top: ${space[3]}px;
         }
     `,
-    // ctaContainer: css`
-    //     > :first-child {
-    //         margin-right: 5px;
-    //     }
-    //     margin-top: 20px;
-    // `,
     ctaContainer: css`
         > :first-child {
             margin-right: 5px;
