e4bcbc1f4e4850e967a3f40bbc3e2ab925cf33ea
Paul Dempsey <76729591+paul-daniel-dempsey@users.noreply.github.com>
2022-12-21 16:59:27 +0000

    removed AUS election moment banner

--- packages/modules/src/modules/banners/common/types.tsx
+++ packages/modules/src/modules/banners/common/types.tsx
@@ -5,7 +5,6 @@ export type BannerId =
     | 'climate-crisis-moment-banner'
     | 'contributions-banner'
     | 'research-survey-banner'
-    | 'aus-moment-banner'
     | 'investigations-moment-banner'
     | 'environment-moment-banner'
     | 'subscription-banner'
