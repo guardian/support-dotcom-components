cc0bbf0aa0569c714a86aebb03cffb12c2d21fee
Paul Dempsey <76729591+paul-daniel-dempsey@users.noreply.github.com>
2023-02-09 18:54:20 +0000

    image breakpoints added

--- packages/modules/src/modules/banners/ukraineMoment/UkraineMomentBanner.stories.tsx
+++ packages/modules/src/modules/banners/ukraineMoment/UkraineMomentBanner.stories.tsx
@@ -57,15 +57,15 @@ const UkraineMomentBanner = bannerWrapper(
         },
         imageSettings: {
             mainUrl:
-                'https://i.guim.co.uk/img/media/62315536ef650b7c9bbf5739d192f10a8c19d66b/0_0_558_293/558.png?width=558&height=293&quality=75&s=171640e67a1f1f5d46e907d4d660d01d',
+                'https://i.guim.co.uk/img/media/77f5a525824c19d7c1ccd447db49f1a95557f12e/0_0_375_152/375.png?width=375&height=152&quality=75&s=fc6794bfbd1bf46853dbbe982ffa6877',
             mobileUrl:
-                'https://i.guim.co.uk/img/media/62315536ef650b7c9bbf5739d192f10a8c19d66b/0_0_558_293/558.png?width=558&height=293&quality=75&s=171640e67a1f1f5d46e907d4d660d01d',
+                'https://i.guim.co.uk/img/media/77f5a525824c19d7c1ccd447db49f1a95557f12e/0_0_375_152/375.png?width=375&height=152&quality=75&s=fc6794bfbd1bf46853dbbe982ffa6877',
             tabletUrl:
-                'https://i.guim.co.uk/img/media/62315536ef650b7c9bbf5739d192f10a8c19d66b/0_0_558_293/558.png?width=558&height=293&quality=75&s=171640e67a1f1f5d46e907d4d660d01d',
+                'https://i.guim.co.uk/img/media/8be245a70938a981ff69898eab033f94dab08b86/0_0_256_230/256.png?width=256&height=230&quality=75&s=4b53fcaee3b5f3bed4b6cb1d78b26a84',
             desktopUrl:
-                'https://i.guim.co.uk/img/media/62315536ef650b7c9bbf5739d192f10a8c19d66b/0_0_558_293/558.png?width=558&height=293&quality=75&s=171640e67a1f1f5d46e907d4d660d01d',
+                'https://i.guim.co.uk/img/media/5325001b389aaf3bc3ad1aec25ec7b90761f02c2/0_0_320_292/320.png?width=320&height=292&quality=75&s=5b193b151d7c6b444141df7a817763be',
             leftColUrl:
-                'https://i.guim.co.uk/img/media/62315536ef650b7c9bbf5739d192f10a8c19d66b/0_0_558_293/558.png?width=558&height=293&quality=75&s=171640e67a1f1f5d46e907d4d660d01d',
+                'https://i.guim.co.uk/img/media/e3ede24eaa18d18766a06d65564fba352064bb5f/0_0_441_292/441.png?width=441&height=292&quality=75&s=6cd61bd9d1eede1f8f9684a58b9cf6c9',
             altText: 'Ukraine one year on',
         },
         bannerId: 'ukraine-moment-banner',
