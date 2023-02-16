d44f3a29b03bd78c7bdbbff544df996c3838a50e
Paul Dempsey <76729591+paul-daniel-dempsey@users.noreply.github.com>
2023-02-16 18:30:08 +0000

    changes to image hires, primary hover color

--- packages/modules/src/modules/banners/ukraineMoment/UkraineMomentBanner.stories.tsx
+++ packages/modules/src/modules/banners/ukraineMoment/UkraineMomentBanner.stories.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { brand, culture, neutral } from '@guardian/src-foundations';
+import { brand, brandAlt, culture, neutral } from '@guardian/src-foundations';
 import { BannerProps, SecondaryCtaType } from '@sdc/shared/src/types';
 import { Meta, Story } from '@storybook/react';
 import { bannerWrapper } from '../common/BannerWrapper';
@@ -23,9 +23,8 @@ const UkraineMomentBanner = bannerWrapper(
                 textColour: 'white',
             },
             hover: {
-                backgroundColour: '#004E7C',
-                textColour: 'white',
-                border: '1px solid #004E7C',
+                backgroundColour: brandAlt[200],
+                textColour: brand[400],
             },
         },
         secondaryCtaSettings: {
@@ -56,13 +55,13 @@ const UkraineMomentBanner = bannerWrapper(
         },
         imageSettings: {
             mainUrl:
-                'https://i.guim.co.uk/img/media/77f5a525824c19d7c1ccd447db49f1a95557f12e/0_0_375_152/375.png?width=375&height=152&quality=75&s=fc6794bfbd1bf46853dbbe982ffa6877',
+                'https://i.guim.co.uk/img/media/5f55f84fbb107ad1539659d8caec5eeab7398dbc/0_0_1500_608/500.png?width=500&quality=75&s=2173abfa5e53fb64660c6596a887e9ac',
             mobileUrl:
-                'https://i.guim.co.uk/img/media/77f5a525824c19d7c1ccd447db49f1a95557f12e/0_0_375_152/375.png?width=375&height=152&quality=75&s=fc6794bfbd1bf46853dbbe982ffa6877',
+                'https://i.guim.co.uk/img/media/5f55f84fbb107ad1539659d8caec5eeab7398dbc/0_0_1500_608/500.png?width=500&quality=75&s=2173abfa5e53fb64660c6596a887e9ac',
             tabletUrl:
-                'https://i.guim.co.uk/img/media/8be245a70938a981ff69898eab033f94dab08b86/0_0_256_230/256.png?width=256&height=230&quality=75&s=4b53fcaee3b5f3bed4b6cb1d78b26a84',
+                'https://i.guim.co.uk/img/media/4f2ec5867b1a111b78927547f2fdceb0066695f9/0_0_1024_920/500.png?width=500&quality=75&s=ef15b7ff812d997251a68b59a1b4279c',
             desktopUrl:
-                'https://i.guim.co.uk/img/media/5325001b389aaf3bc3ad1aec25ec7b90761f02c2/0_0_320_292/320.png?width=320&height=292&quality=75&s=5b193b151d7c6b444141df7a817763be',
+                'https://i.guim.co.uk/img/media/8d9128c93a61155e0b84f0eb229edd57951ee3ff/0_0_1280_1168/500.png?width=500&quality=75&s=0d8ff342ffc3005186903969b22a4674',
             leftColUrl:
                 'https://i.guim.co.uk/img/media/359d8cde335895144b85a52ce0fe6b93fa8e1514/0_0_882_584/882.png?width=882&height=584&quality=75&s=da90ac1806631a1cb7f3903af88b5819',
             wideUrl:
