14e387cb5e85681838b847ec29a01474bc756e2b
Paul Dempsey <76729591+paul-daniel-dempsey@users.noreply.github.com>
2023-03-02 13:08:51 +0000

    add polygon component top right
    Co-authored-by: Maria Olanipekun <marialani@users.noreply.github.com>

--- packages/modules/src/modules/banners/investigationsMoment/components/InvestigationsMomentBannerPolygonTopRight.tsx
+++ packages/modules/src/modules/banners/investigationsMoment/components/InvestigationsMomentBannerPolygonTopRight.tsx
@@ -1,5 +1,50 @@
 import React from 'react';
 
+const polygonTopRight = {
+    mobile: {
+        viewBox: '0 0 150 80',
+        points: '0 0, 150 0, 150 80',
+        desktopShadow: 'desktopShadowRight',
+    },
+    mobileMedium: {
+        viewBox: '0 0 200 80',
+        points: '0 0, 200 0, 200 80',
+        desktopShadow: 'desktopShadowRight',
+    },
+    mobileLandscape: {
+        viewBox: '0 0 300 80',
+        points: '0 0, 300 0, 300 80',
+        desktopShadow: 'desktopShadowRight',
+    },
+    phablet: {
+        viewBox: '0 0 475 80',
+        points: '0 0, 475 0, 475 80',
+        desktopShadow: 'desktopShadowRight',
+    },
+    tablet: {
+        viewBox: '0 0 100 200',
+        points: '0 0, 100 0, 100 200',
+        desktopShadow: 'desktopShadowRight',
+    },
+    desktop: {
+        viewBox: '0 0 300 400',
+        points: '0 0, 300 0, 300 400',
+        desktopShadow: 'desktopShadowRight',
+    },
+    leftCol: {
+        viewBox: '0 0 350 400',
+        points: '0 0, 350 0, 350 400',
+        desktopShadow: 'desktopShadowRight',
+    },
+    wide: {
+        viewBox: '0 0 530 400',
+        points: '0 0, 530 0, 530 400',
+        desktopShadow: 'desktopShadowRight',
+    },
+};
+
+console.log(polygonTopRight);
+
 const InvestigationsMomentBannerPolygonTopRight = (): JSX.Element => {
     return <div>InvestigationsMomentBannerPolygonTopRight</div>;
 };
