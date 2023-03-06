66b8723c928662e93a54bc88714bd8aa6c5d000a
Tom Forbes <tom.forbes@theguardian.com>
2023-03-06 13:50:36 +0000

    upgrade cdk (#851)
    * upgrade cdk
    
    * build script
    
    * node version
    
    * templateStagePaths
    
    * artifact.json
    
    * alarm statistic

--- cdk/lib/dotcom-components.test.ts
+++ cdk/lib/dotcom-components.test.ts
@@ -1,12 +1,16 @@
-import '@aws-cdk/assert/jest';
-import { SynthUtils } from '@aws-cdk/assert';
-import { App } from '@aws-cdk/core';
+import { App } from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
 import { DotcomComponents } from './dotcom-components';
 
-describe('The Cfn stack', () => {
-    it('matches the snapshot', () => {
-        const app = new App();
-        const stack = new DotcomComponents(app, 'cfn', { stack: 'support' });
-        expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot();
-    });
+describe('The DotcomComponents stack', () => {
+	it('matches the snapshot', () => {
+		const app = new App();
+		const stack = new DotcomComponents(app, 'DotcomComponents', {
+			stack: 'support',
+			stage: 'TEST',
+			domainName: 'dotcom-components-test.support.guardianapis.com',
+		});
+		const template = Template.fromStack(stack);
+		expect(template.toJSON()).toMatchSnapshot();
+	});
 });
