6637d79fdb62e90e11c056dd9cc69b571fbf16c3
Paul Dempsey <76729591+paul-daniel-dempsey@users.noreply.github.com>
2023-03-10 11:32:50 +0000

    removed un-used css styles

--- packages/modules/src/modules/banners/investigationsMoment/InvestigationsMomentBanner.tsx
+++ packages/modules/src/modules/banners/investigationsMoment/InvestigationsMomentBanner.tsx
@@ -28,68 +28,6 @@ const styles = {
             background: ${neutral[100]};
         }
     `,
-    desktopShadowRight: css`
-        position: absolute;
-        pointer-events: none;
-        display: flex;
-        justify-content: flex-end;
-        top: 0;
-        right: 0;
-        width: 150px;
-        height: 80px;
-
-        ${from.mobileMedium} {
-            width: 200px;
-        }
-
-        ${from.mobileLandscape} {
-            width: 300px;
-        }
-
-        ${from.phablet} {
-            width: 475px;
-        }
-
-        ${from.tablet} {
-            bottom: 0;
-            width: auto;
-            height: auto;
-        }
-
-        svg {
-            display: block;
-            height: 100%;
-
-            ${from.tablet} {
-                height: 90%;
-            }
-
-            ${from.desktop} {
-                height: 95%;
-            }
-
-            ${from.leftCol} {
-                height: 90%;
-            }
-        }
-    `,
-    desktopShadowBottom: css`
-        position: absolute;
-        pointer-events: none;
-        bottom: 0;
-        left: 0;
-        right: 20px;
-
-        svg {
-            display: block;
-        }
-
-        ${from.wide} {
-            height: 125px;
-            width: 1250px;
-            right: auto;
-        }
-    `,
     headerContainer: css`
         margin: 0 -${space[3]}px;
 
