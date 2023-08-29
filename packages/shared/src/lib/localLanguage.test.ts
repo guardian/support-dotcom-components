6fc0e3bc0a0d9b6f44ae8f4cac49e97a2e4abc90
Paul Dempsey <76729591+paul-daniel-dempsey@users.noreply.github.com>
2023-08-29 17:14:13 +0100

    local language Epic highlighted text added

--- packages/shared/src/lib/localLanguage.test.ts
+++ packages/shared/src/lib/localLanguage.test.ts
@@ -12,8 +12,9 @@ describe('getCountryCodeToLocalLanguage', () => {
                 epicParagraphs: [
                     `Le Guardian est une rédaction indépendante au sein de laquelle nos journalistes se passionnent pour la justice sociale, l’environnement et la science. Cette semaine/ Ce mois-ci, nous lançons notre nouvelle édition en ligne dédiée à nos lecteurs en Europe, et donc à vous, en France. Nous vous invitons à faire partie de cette aventure en nous apportant votre soutien dès aujourd’hui.`,
                     `Nos investigations mettent en lumière les dessous du pouvoir et dénoncent l’injustice et la corruption. Dans un monde surmédiatisé, étouffé par la désinformation et le sensationnalisme, nous nous engageons à relayer l'exactitude et la véracité des faits. Aucun milliardaire, aucun actionnaire ne contrôle notre ligne éditoriale. Nos contenus sont gratuits et accessibles à tous, nous n’avons pas de paywall. Nous voulons garantir un accès libre à une information fiable et intègre, et ce, au plus grand nombre.`,
-                    `Afin que ce travail essentiel perdure dans les années à venir, nous avons besoin du soutien des lecteurs. Si vous le pouvez, une contribution de seulement 2 euros par mois peut dès aujourd’hui faire toute la différence. Merci.`,
+                    `Afin que ce travail essentiel perdure dans les années à venir, nous avons besoin du soutien des lecteurs. Si vous le pouvez, `,
                 ],
+                epicHighlightedText: `une contribution de seulement 2 euros par mois peut dès aujourd’hui faire toute la différence. Merci.`,
             },
         },
         {
@@ -24,6 +25,7 @@ describe('getCountryCodeToLocalLanguage', () => {
                 bannerHeader: 'Nehmen Sie an unserem neuen europäischen Abenteuer teil!',
                 epicHeader: 'Nehmen Sie an unserem neuen europäischen Abenteuer teil!',
                 epicParagraphs: ['1', '2', '3', '4'],
+                epicHighlightedText: `Highlight (German)`,
             },
         },
         {
@@ -42,6 +44,7 @@ describe('getCountryCodeToLocalLanguage', () => {
                 bannerHeader: 'Neem deel aan ons nieuwe Europese avontuur!',
                 epicHeader: 'Neem deel aan ons nieuwe Europese avontuur!',
                 epicParagraphs: ['1', '2', '3'],
+                epicHighlightedText: `Highlight (Dutch)`,
             },
         },
         {
@@ -74,11 +77,13 @@ describe('getCountryCodeToLocalLanguage', () => {
                 bannerHeader: 'BannerHeaderTest',
                 epicHeader: 'EpicHeaderTest',
                 epicParagraphs: ['1', '2', '3', '4', '5'],
+                epicHighlightedText: `Highlight (US)`,
             },
             output: {
                 bannerHeader: 'BannerHeaderTest',
                 epicHeader: 'EpicHeaderTest',
                 epicParagraphs: ['1', '2', '3', '4', '5'],
+                epicHighlightedText: `Highlight (US)`,
             },
         },
         {
@@ -89,11 +94,13 @@ describe('getCountryCodeToLocalLanguage', () => {
                 bannerHeader: 'BannerHeaderTest',
                 epicHeader: 'EpicHeaderTest',
                 epicParagraphs: ['1', '2', '3', '4', '5'],
+                epicHighlightedText: `Highlight`,
             },
             output: {
                 bannerHeader: 'BannerHeaderTest',
                 epicHeader: 'EpicHeaderTest',
                 epicParagraphs: ['1', '2', '3', '4', '5'],
+                epicHighlightedText: `Highlight`,
             },
         },
     ];
--- packages/shared/src/lib/localLanguage.ts
+++ packages/shared/src/lib/localLanguage.ts
@@ -8,6 +8,7 @@ export type LocalLanguage = {
     bannerHeader?: string;
     epicHeader?: string;
     epicParagraphs?: string[];
+    epicHighlightedText?: string;
 };
 type LocalLanguages = Record<string, LocalLanguage>;
 
@@ -18,13 +19,15 @@ const localLanguages: LocalLanguages = {
         epicParagraphs: [
             `Le Guardian est une rédaction indépendante au sein de laquelle nos journalistes se passionnent pour la justice sociale, l’environnement et la science. Cette semaine/ Ce mois-ci, nous lançons notre nouvelle édition en ligne dédiée à nos lecteurs en Europe, et donc à vous, en France. Nous vous invitons à faire partie de cette aventure en nous apportant votre soutien dès aujourd’hui.`,
             `Nos investigations mettent en lumière les dessous du pouvoir et dénoncent l’injustice et la corruption. Dans un monde surmédiatisé, étouffé par la désinformation et le sensationnalisme, nous nous engageons à relayer l'exactitude et la véracité des faits. Aucun milliardaire, aucun actionnaire ne contrôle notre ligne éditoriale. Nos contenus sont gratuits et accessibles à tous, nous n’avons pas de paywall. Nous voulons garantir un accès libre à une information fiable et intègre, et ce, au plus grand nombre.`,
-            `Afin que ce travail essentiel perdure dans les années à venir, nous avons besoin du soutien des lecteurs. Si vous le pouvez, une contribution de seulement 2 euros par mois peut dès aujourd’hui faire toute la différence. Merci.`,
+            `Afin que ce travail essentiel perdure dans les années à venir, nous avons besoin du soutien des lecteurs. Si vous le pouvez, `,
         ],
+        epicHighlightedText: `une contribution de seulement 2 euros par mois peut dès aujourd’hui faire toute la différence. Merci.`,
     },
     DE: {
         bannerHeader: 'Nehmen Sie an unserem neuen europäischen Abenteuer teil!',
         epicHeader: 'Nehmen Sie an unserem neuen europäischen Abenteuer teil!',
         epicParagraphs: ['1', '2', '3', '4'],
+        epicHighlightedText: `Highlight (German)`,
     },
     IT: {
         bannerHeader: 'Prendi parte alla nostra nuova avventura europea!',
@@ -33,6 +36,7 @@ const localLanguages: LocalLanguages = {
         bannerHeader: 'Neem deel aan ons nieuwe Europese avontuur!',
         epicHeader: 'Neem deel aan ons nieuwe Europese avontuur!',
         epicParagraphs: ['1', '2', '3'],
+        epicHighlightedText: `Highlight (Dutch)`,
     },
     SE: {
         bannerHeader: 'Ta del av vårt nya europeiska äventyr!',
@@ -57,6 +61,7 @@ export const countryCodeToVerfiedLocalLanguage = (
                 bannerHeader: dfltLocalLanguage?.bannerHeader,
                 epicHeader: dfltLocalLanguage?.epicHeader,
                 epicParagraphs: dfltLocalLanguage?.epicParagraphs,
+                epicHighlightedText: dfltLocalLanguage?.epicHighlightedText,
             }
         );
     }
