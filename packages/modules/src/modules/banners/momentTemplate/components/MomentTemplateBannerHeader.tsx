1ef13027f6367fcae8cce5db7f006ed71e589dd4
Paul Dempsey <76729591+paul-daniel-dempsey@users.noreply.github.com>
2023-09-04 14:56:21 +0100

    mainOrMobile content passed down to simplify subcomponent

--- packages/modules/src/modules/banners/momentTemplate/components/MomentTemplateBannerHeader.tsx
+++ packages/modules/src/modules/banners/momentTemplate/components/MomentTemplateBannerHeader.tsx
@@ -4,31 +4,20 @@ import { from } from '@guardian/src-foundations/mq';
 import { headline } from '@guardian/src-foundations/typography';
 import { neutral } from '@guardian/src-foundations/palette';
 import { HeaderSettings } from '../settings';
-import useMediaQuery from '../../../../hooks/useMediaQuery';
 
 interface MomentTemplateBannerHeaderProps {
-    heading: JSX.Element | JSX.Element[] | null;
-    mobileHeading: JSX.Element | JSX.Element[] | null;
+    mainOrMobileContent: JSX.Element | JSX.Element[] | null;
     headerSettings: HeaderSettings | undefined;
 }
 
 export function MomentTemplateBannerHeader({
-    heading,
-    mobileHeading,
+    mainOrMobileContent,
     headerSettings,
 }: MomentTemplateBannerHeaderProps): JSX.Element {
-    const isTabletOrAbove = useMediaQuery(from.tablet);
-
     return (
         <div css={styles.container}>
             <header css={styles.header(headerSettings)}>
-                <h2>
-                    {headerSettings?.image
-                        ? headerSettings.image
-                        : isTabletOrAbove
-                        ? heading
-                        : mobileHeading}
-                </h2>
+                <h2>{headerSettings?.image ? headerSettings.image : mainOrMobileContent}</h2>
             </header>
         </div>
     );
