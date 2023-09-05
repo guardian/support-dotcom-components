9f1c4796bed00489dc4747118c84258ec5749c70
Tom Wey <tom.wey@theguardian.com>
2023-09-05 15:47:28 +0100

    Fix story

--- packages/modules/src/modules/banners/designableBanner/stories/Default.tsx
+++ packages/modules/src/modules/banners/designableBanner/stories/Default.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import { BannerProps } from '@sdc/shared/src/types';
 import { Story } from '@storybook/react';
-import { bannerWrapper } from '../../common/BannerWrapper';
 import { DesignableBanner } from '../DesignableBanner';
-import { BannerRenderProps } from '../../common/types';
 
-export const DefaultBanner: React.FC<BannerProps> = bannerWrapper((props: BannerRenderProps) => {
-    return <DesignableBanner {...props} />;
-}, 'designable-banner');
+export const DefaultBanner: React.FC<BannerProps> = DesignableBanner;
 
 export const DefaultTemplate: Story<BannerProps> = (props: BannerProps) => (
     <DefaultBanner {...props} />
