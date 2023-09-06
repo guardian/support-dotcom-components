63e0d285ab77bcd2792201ade00c42718e46ad7f
Paul Dempsey <76729591+paul-daniel-dempsey@users.noreply.github.com>
2023-09-06 09:29:49 +0100

    epic copy initial update

--- packages/server/src/tests/epics/hardCodedTests/localLanguageCopy.ts
+++ packages/server/src/tests/epics/hardCodedTests/localLanguageCopy.ts
@@ -1,21 +1,28 @@
 import { EpicTest, EpicVariant } from '@sdc/shared/dist/types';
 
 const localLanguagesEpicCopy = {
-    // Copy to be updated if this approach is taken forward
     FR: {
-        heading: 'Bonjour',
-        paragraphs: ['Bonjour', 'Bonjour', 'Bonjour '],
-        highlightedText: 'Bonjour',
+        heading: '… il existe une bonne raison de ne pas soutenir le Guardian.',
+        paragraphs: [
+            `Actuellement, beaucoup de lecteurs ne sont pas ou plus en mesure de payer pour accéder aux informations. C’est pour cela que nos contenus sont et resteront gratuits et ouverts à tous. Si tel est votre cas, vous pouvez continuer à lire nos articles librement, comme vous le faites à présent, depuis la France.`,
+            `Si au contraire, vous avez les moyens de contribuer financièrement, voici trois bonnes raisons de soutenir le Guardian dès aujourd’hui.`,
+            `1. Nous sommes une rédaction indépendante. Personne ne contrôle notre ligne éditoriale ; fait rare en Europe où la liberté des médias est souvent compromise par le poids des actionnaires et la corruption. `,
+            `2. Notre journalisme d'investigation met en lumière les dessous du pouvoir et dénonce l’injustice en Europe et dans le monde entier.`,
+            `3. Même sous l'ère du Brexit, nous restons plus européens que jamais. Nous venons de lancer notre nouvelle édition en ligne en anglais, dédiée à nos lecteurs en Europe, et donc à vous, en France. Cette année, nous avons investi dans notre journalisme européen, recruté de nouveaux correspondants sur le continent et publié plus de 10 000 articles sur les affaires européennes. Nous comptons désormais environ 180 000 de nos contributeurs en Europe.`,
+            `Afin que ce travail essentiel perdure dans les années à venir, nous avons besoin du soutien des lecteurs. Si vous le pouvez, `,
+        ],
+        highlightedText:
+            'une contribution de seulement 2 euros par mois peut dès aujourd’hui faire toute la différence. Merci.',
     },
     DE: {
         heading: 'Hallo Germany',
-        paragraphs: ['Hallo', 'Hallo', 'Hallo '],
-        highlightedText: 'Hallo',
+        paragraphs: ['Hallo1', 'Hallo2', 'Hallo3'],
+        highlightedText: 'Hallo Germany',
     },
     NL: {
         heading: 'Hallo Netherlands',
-        paragraphs: ['Hallo', 'Hallo', 'Hallo '],
-        highlightedText: 'Hallo',
+        paragraphs: ['Hallo1', 'Hallo2'],
+        highlightedText: 'Hallo Netherlands',
     },
 };
 
