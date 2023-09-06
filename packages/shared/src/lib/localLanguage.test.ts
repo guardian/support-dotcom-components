51ddf3d4f98568727e2c64f28557070458fa9bea
Paul Dempsey <76729591+paul-daniel-dempsey@users.noreply.github.com>
2023-09-06 08:03:58 +0100

    localLanguage epic removal

--- packages/shared/src/lib/localLanguage.test.ts
+++ packages/shared/src/lib/localLanguage.test.ts
@@ -8,16 +8,6 @@ describe('getCountryCodeToLocalLanguage', () => {
             inputVariant: 'CONTROL',
             output: {
                 bannerHeader: 'Soutenez un journalisme européen et indépendant ',
-                epicHeader: '… il existe une bonne raison de ne pas soutenir le Guardian.',
-                epicParagraphs: [
-                    `Actuellement, beaucoup de lecteurs ne sont pas ou plus en mesure de payer pour accéder aux informations. C’est pour cela que nos contenus sont et resteront gratuits et ouverts à tous. Si tel est votre cas, vous pouvez continuer à lire nos articles librement, comme vous le faites à présent, depuis la France.`,
-                    `Si au contraire, vous avez les moyens de contribuer financièrement, voici trois bonnes raisons de soutenir le Guardian dès aujourd’hui.`,
-                    `1. Nous sommes une rédaction indépendante. Personne ne contrôle notre ligne éditoriale ; fait rare en Europe où la liberté des médias est souvent compromise par le poids des actionnaires et la corruption. `,
-                    `2. Notre journalisme d'investigation met en lumière les dessous du pouvoir et dénonce l’injustice en Europe et dans le monde entier.`,
-                    `3. Même sous l'ère du Brexit, nous restons plus européens que jamais. Nous venons de lancer notre nouvelle édition en ligne en anglais, dédiée à nos lecteurs en Europe, et donc à vous, en France. Cette année, nous avons investi dans notre journalisme européen, recruté de nouveaux correspondants sur le continent et publié plus de 10 000 articles sur les affaires européennes. Nous comptons désormais environ 180 000 de nos contributeurs en Europe.`,
-                    `Afin que ce travail essentiel perdure dans les années à venir, nous avons besoin du soutien des lecteurs. Si vous le pouvez, `,
-                ],
-                epicHighlightedText: `une contribution de seulement 2 euros par mois peut dès aujourd’hui faire toute la différence. Merci.`,
             },
         },
         {
@@ -26,14 +16,6 @@ describe('getCountryCodeToLocalLanguage', () => {
             inputVariant: 'CONTROL',
             output: {
                 bannerHeader: 'Unterstützen Sie unabhängigen europäischen Journalismus',
-                epicHeader: 'Header (German)',
-                epicParagraphs: [
-                    'Para1 (German)',
-                    'Para2 (German)',
-                    'Para3 (German)',
-                    'Para4 (German), ',
-                ],
-                epicHighlightedText: `Highlight (German)`,
             },
         },
         {
@@ -50,9 +32,6 @@ describe('getCountryCodeToLocalLanguage', () => {
             inputVariant: 'CONTROL',
             output: {
                 bannerHeader: 'Steun de onafhankelijke journalistiek',
-                epicHeader: 'Header (Dutch)',
-                epicParagraphs: ['Para1 (Dutch)', 'Para2 (Dutch)', 'Para3 (Dutch), '],
-                epicHighlightedText: `Highlight (Dutch)`,
             },
         },
         {
@@ -107,15 +86,9 @@ describe('getCountryCodeToLocalLanguage', () => {
             inputVariant: 'CONTROL',
             inputDefaultLocalLanguage: {
                 bannerHeader: 'BannerHeaderTest',
-                epicHeader: 'EpicHeaderTest',
-                epicParagraphs: ['1', '2', '3', '4', '5'],
-                epicHighlightedText: `Highlight (US)`,
             },
             output: {
                 bannerHeader: 'BannerHeaderTest',
-                epicHeader: 'EpicHeaderTest',
-                epicParagraphs: ['1', '2', '3', '4', '5'],
-                epicHighlightedText: `Highlight (US)`,
             },
         },
         {
@@ -124,15 +97,9 @@ describe('getCountryCodeToLocalLanguage', () => {
             inputVariant: 'CONTROL',
             inputDefaultLocalLanguage: {
                 bannerHeader: 'BannerHeaderTest',
-                epicHeader: 'EpicHeaderTest',
-                epicParagraphs: ['1', '2', '3', '4', '5'],
-                epicHighlightedText: `Highlight`,
             },
             output: {
                 bannerHeader: 'BannerHeaderTest',
-                epicHeader: 'EpicHeaderTest',
-                epicParagraphs: ['1', '2', '3', '4', '5'],
-                epicHighlightedText: `Highlight`,
             },
         },
         {
@@ -147,9 +114,6 @@ describe('getCountryCodeToLocalLanguage', () => {
             inputVariant: '',
             inputDefaultLocalLanguage: {
                 bannerHeader: 'BannerHeaderTest',
-                epicHeader: 'EpicHeaderTest',
-                epicParagraphs: ['1', '2', '3', '4', '5'],
-                epicHighlightedText: `Highlight`,
             },
             output: undefined,
         },
