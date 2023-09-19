e21904e531cff7b26bfc961f7504cbd027305c8d
Paul Dempsey <76729591+paul-daniel-dempsey@users.noreply.github.com>
2023-09-19 18:53:00 +0100

    ContributionsBannerWithSignIn re-instated for RRCP and storybook

---
+++ packages/modules/src/modules/banners/contributions/ContributionsBannerWithSignIn.stories.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react';
+import { ContributionsBannerWithSignInUnvalidated as ContributionsBannerWithSignIn } from './ContributionsBannerWithSignIn';
+import { props } from '../utils/storybook';
+import { BannerProps } from '@sdc/shared/types';
+
+export default {
+    component: ContributionsBannerWithSignIn,
+    title: 'Banners/Custom/Contributions',
+    parameters: {
+        chromatic: {
+            delay: 300,
+        },
+    },
+    args: props,
+} as Meta;
+
+const Template: Story<BannerProps> = (props: BannerProps) => (
+    <ContributionsBannerWithSignIn {...props} />
+);
+
+export const WithSignIn = Template.bind({});
