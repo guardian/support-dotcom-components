d9026f33b8c07a1495e41a746760cb4b83061f2d
Paul Dempsey <76729591+paul-daniel-dempsey@users.noreply.github.com>
2023-10-03 12:49:18 +0100

    Images 100% scaled now

--- packages/modules/src/modules/banners/environmentMoment/EnvironmentMomentBanner.stories.tsx
+++ packages/modules/src/modules/banners/environmentMoment/EnvironmentMomentBanner.stories.tsx
@@ -17,21 +17,21 @@ export default {
     args: props,
 } as Meta;
 
-const image = {
+const imageDoubleSized = {
     mainUrl:
-        'https://i.guim.co.uk/img/media/c0e64841caf710a717788744e9c4970c482b73d3/0_0_508_150/master/508.jpg?width=254&height=75&quality=50&s=b6b3cc5ef6160a185e22af5a4b987d85',
+        'https://i.guim.co.uk/img/media/c0e64841caf710a717788744e9c4970c482b73d3/0_0_508_150/master/508.jpg?width=254&height=75&quality=100&s=6fbbba88e9822c18f17af82f734ba79b',
     mobileSmallUrl:
-        'https://i.guim.co.uk/img/media/c0e64841caf710a717788744e9c4970c482b73d3/0_0_508_150/master/508.jpg?width=254&height=75&quality=50&s=b6b3cc5ef6160a185e22af5a4b987d85b',
+        'https://i.guim.co.uk/img/media/c0e64841caf710a717788744e9c4970c482b73d3/0_0_508_150/master/508.jpg?width=254&height=75&quality=100&s=6fbbba88e9822c18f17af82f734ba79b',
     mobileMediumUrl:
-        'https://i.guim.co.uk/img/media/036510bc15ecdba97355f464006e3db5fbde9129/0_0_620_180/master/620.jpg?width=310&height=90&quality=50&s=c40742a7b002d519d72525b238e09a4d',
+        'https://i.guim.co.uk/img/media/036510bc15ecdba97355f464006e3db5fbde9129/0_0_620_180/master/620.jpg?width=310&height=90&quality=100&s=01c604815a2f9980a1227c0d91ffa6b1',
     tabletUrl:
-        'https://i.guim.co.uk/img/media/0d0bda341c85d6d415384968d63898399ca49952/0_0_764_252/master/764.jpg?width=382&height=126&quality=50&s=760f782895be1f63652f0bef41362607',
+        'https://i.guim.co.uk/img/media/0d0bda341c85d6d415384968d63898399ca49952/0_0_764_252/master/764.jpg?width=382&height=126&quality=100&s=53cf657cae48d854003aa44c8578bda1',
     desktopUrl:
-        'https://i.guim.co.uk/img/media/7030f9d98e368d6e5c7a34c643c76d7d1f5ac63c/0_0_1056_366/master/1056.jpg?width=528&height=183&quality=50&s=413d79e6994c5d251a86f08349ea90fc',
+        'https://i.guim.co.uk/img/media/7030f9d98e368d6e5c7a34c643c76d7d1f5ac63c/0_0_1056_366/master/1056.jpg?width=528&height=183&quality=100&s=f0c02cddda84dfaf4ef261d91bd26159',
     leftColUrl:
-        'https://i.guim.co.uk/img/media/6f09850dd1bff07005c162501a95c506ee88038c/0_0_1248_358/master/1248.jpg?width=624&height=179&quality=50&s=27d353f78472a5c8824c8fcaf8378727',
+        'https://i.guim.co.uk/img/media/6f09850dd1bff07005c162501a95c506ee88038c/0_0_1248_358/master/1248.jpg?width=624&height=179&quality=100&s=bdff3fd934eb6271082f5d947c0f7890',
     wideUrl:
-        'https://i.guim.co.uk/img/media/3c1cb611785d3dccc2674636a6f692da1e2fcdb6/0_0_1392_366/master/1392.jpg?width=696&height=183&quality=50&s=e0a4d709994fa9908f9a4a42e5bc1708',
+        'https://i.guim.co.uk/img/media/3c1cb611785d3dccc2674636a6f692da1e2fcdb6/0_0_1392_366/master/1392.jpg?width=696&height=183&quality=100&s=5935c1ae5e8cbc5d9ed616bbadb3b09e',
     altText: 'Guardian Our Planet cant Speak for itself',
 };
 
@@ -43,7 +43,12 @@ const EnvironmentMomentBanner = bannerWrapper(
         headerSettings: {
             showHeader: { text: false, image: true },
             textColour: neutral[0],
-            image: <EnvironmentVisual settings={image} bannerId={'environment-moment-banner'} />,
+            image: (
+                <EnvironmentVisual
+                    settings={imageDoubleSized}
+                    bannerId={'environment-moment-banner'}
+                />
+            ),
         },
         primaryCtaSettings: {
             default: {
