eabeb04f8ad402b75201a3c177d99bf88dd394c8
Michael Jacobson <michael.jacobson@guardian.co.uk>
2023-10-31 15:42:08 +0000

    add functions to calculate US thanksgiving and giving tuesday program… (#996)
    * add functions to calculate US thanksgiving and giving tuesday programmatically
    
    * update function name to clarify that it returns the date of Thanksgiving in the US, as opposed to Canada
    
    * Sets reminder label and CTA dynamically for the current year. Adds explainer comments for Thanksgiving calculator function. Adds start date for Remind me on Giving Tuesday CTA.
    
    * fix comment
    
    * ensure functionality works properly when Giving Tuesday falls in December
    
    * set Giving Tuesday reminder start date to Oct 20th, in line with the 20th being the standard cut-off for reminders the month after next
    
    * fix old comment

--- packages/shared/src/lib/reminderFields.ts
+++ packages/shared/src/lib/reminderFields.ts
@@ -5,6 +5,31 @@ export interface ReminderFields {
     reminderOption?: string;
 }
 
+// Thanksgiving in the USA is always the fourth Thursday in November
+const thanksgivingUSA = (year: number): Date => {
+    // An integer representing the day of the week for November 1st (0 == Sunday, 1 == Monday, 2 == Tuesday, etc.)
+    const novemberFirstDay = new Date(year, 10, 1).getDay();
+
+    // We calculate the offset between the first of November and the first Thursday in November using a modulus operation
+    const firstThursdayOffset = (11 - novemberFirstDay) % 7;
+
+    // The 22nd is the earliest possible date in November for Thanksgiving (USA). We add the offset to 22 to find the
+    // date of Thanksgiving for the given year
+    const thanksgivingDate = 22 + firstThursdayOffset;
+
+    return new Date(year, 10, thanksgivingDate);
+};
+
+// Giving Tuesday is always the Tuesday following Thanksgiving, i.e. five days after Thanksgiving
+const givingTuesday = (year: number): Date => {
+    const date = thanksgivingUSA(year);
+    // We cannot simply query the date of the month for Thanksgiving and add 5 to it, we need to allow for Giving Tuesday
+    // to be in the first week of December
+    date.setDate(date.getDate() + 5);
+
+    return date;
+};
+
 const getReminderDate = (date: Date): Date => {
     const monthsAhead = date.getDate() < 20 ? 1 : 2;
     date.setMonth(date.getMonth() + monthsAhead);
@@ -12,18 +37,25 @@ const getReminderDate = (date: Date): Date => {
     return date;
 };
 
-export const GIVING_TUESDAY_REMINDER_FIELDS: ReminderFields = {
-    reminderCta: 'Remind me on Giving Tuesday',
-    reminderPeriod: '2023-11-01',
-    reminderLabel: 'on Giving Tuesday',
-    reminderOption: 'giving-tuesday-2023',
+const givingTuesdayReminderFields = (year: number): ReminderFields => {
+    // Giving Tuesday can be in either November or December
+    // Months in JS run from 0-11, we want 01-12 for the reminderPeriod field
+    const givingTuesdayMonth = (givingTuesday(year).getMonth() + 1).toString().padStart(0, '2');
+    return {
+        reminderCta: 'Remind me on Giving Tuesday',
+        reminderPeriod: `${year}-${givingTuesdayMonth}-01`,
+        reminderLabel: 'on Giving Tuesday',
+        reminderOption: `giving-tuesday-${year}`,
+    };
 };
 
-export const NEW_YEARS_EVE_REMINDER_FIELDS: ReminderFields = {
-    reminderCta: 'Remind me on New Years Eve',
-    reminderPeriod: '2023-12-01',
-    reminderLabel: 'on New Years Eve',
-    reminderOption: 'new-years-eve-2023',
+const newYearsEveReminderFields = (year: number): ReminderFields => {
+    return {
+        reminderCta: 'Remind me on New Years Eve',
+        reminderPeriod: `${year}-12-01`,
+        reminderLabel: 'on New Years Eve',
+        reminderOption: `new-years-eve-${year}`,
+    };
 };
 
 export const buildReminderFields = (today: Date = new Date()): ReminderFields => {
@@ -41,26 +73,29 @@ export const buildReminderFields = (today: Date = new Date()): ReminderFields =>
     };
 };
 
-const givingTuesdayCutOff = new Date('2023-11-27');
+const newYearsEve = new Date('2023-12-31');
+const givingTuesdayReminderStart = (date: Date): Date => {
+    return new Date(date.getFullYear(), 9, 20); // October 20th (provisional auto start date, to be confirmed)
+};
 
-const givingTuesdayIsActive = (date: Date): boolean => date <= givingTuesdayCutOff;
+// Giving Tuesday is 'active' from October 20th until the day before Giving Tuesday
+const givingTuesdayIsActive = (date: Date): boolean =>
+    date >= givingTuesdayReminderStart(date) && date < givingTuesday(date.getFullYear());
 
-const newYearsEveStart = new Date('2023-11-28');
-const newYearsEveCutOff = new Date('2023-12-30');
-
+// New Years Eve is 'active' from Giving Tuesday until the day before NYE
 const newYearsEveIsActive = (date: Date): boolean =>
-    date >= newYearsEveStart && date <= newYearsEveCutOff;
+    date >= givingTuesday(date.getFullYear()) && date < newYearsEve;
 
 export const getReminderFields = (
     countryCode?: string,
     date: Date = new Date(),
 ): ReminderFields => {
     if (countryCode === 'US' && givingTuesdayIsActive(date)) {
-        return GIVING_TUESDAY_REMINDER_FIELDS;
+        return givingTuesdayReminderFields(date.getFullYear());
     }
 
     if (countryCode === 'US' && newYearsEveIsActive(date)) {
-        return NEW_YEARS_EVE_REMINDER_FIELDS;
+        return newYearsEveReminderFields(date.getFullYear());
     }
 
     return buildReminderFields(date);
