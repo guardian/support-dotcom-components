b8488946f788c06cd78885b67d03951e380a9988
Tom Forbes <tom.forbes@theguardian.com>
2023-11-15 13:02:21 +0000

    types

--- packages/dotcom/src/index.ts
+++ packages/dotcom/src/index.ts
@@ -1,10 +1,8 @@
 export { getEpicViewLog, logEpicView } from '../../shared/src/lib/viewLog';
 export { getWeeklyArticleHistory, incrementWeeklyArticleCount } from '../../shared/src/lib/history';
 export { replaceNonArticleCountPlaceholders, containsNonArticleCountPlaceholder } from '../../shared/src/lib/placeholders';
-export { EpicProps } from '../../shared/src/types/props/epic';
 export * from '../../shared/src/lib/geolocation';
 export * from '../../shared/src/lib/viewLog';
-export * from '../../shared/src/types/prices';
 export * from '../../shared/src/lib/reminderFields';
 export * from '../../shared/src/types/reminders';
 export * from './requests';
--- packages/dotcom/src/types.ts
+++ packages/dotcom/src/types.ts
@@ -4,4 +4,6 @@ export {
     HeaderPayload,
     WeeklyArticleHistory,
 } from '../../shared/src/types/targeting';
+export * from '../../shared/src/types/prices';
+export { EpicProps } from '../../shared/src/types/props/epic';
 export { ModuleDataResponse, ModuleData } from './requests';
