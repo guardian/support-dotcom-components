1d13b48015b19f73bc77a9ee5b862c7b84a21e3f
Tom Forbes <tom.forbes@theguardian.com>
2023-11-16 16:40:43 +0000

    lint

--- packages/dotcom/src/index.ts
+++ packages/dotcom/src/index.ts
@@ -1,6 +1,9 @@
 export { getEpicViewLog, logEpicView } from '../../shared/src/lib/viewLog';
 export { getWeeklyArticleHistory, incrementWeeklyArticleCount } from '../../shared/src/lib/history';
-export { replaceNonArticleCountPlaceholders, containsNonArticleCountPlaceholder } from '../../shared/src/lib/placeholders';
+export {
+    replaceNonArticleCountPlaceholders,
+    containsNonArticleCountPlaceholder,
+} from '../../shared/src/lib/placeholders';
 export * from '../../shared/src/lib/geolocation';
 export * from '../../shared/src/lib/viewLog';
 export * from '../../shared/src/lib/reminderFields';
