b2d8daa37d2b09c410ab7ca34b58610cbf27bf84
Tom Forbes <tom.forbes@theguardian.com>
2023-11-17 13:39:41 +0000

    expand dotcom library (#1003)
    * expand dotcom lib
    
    * types
    
    * fix build
    
    * fix ophan types
    
    * types
    
    * remove comments
    
    * lint

--- packages/dotcom/src/index.ts
+++ packages/dotcom/src/index.ts
@@ -1,3 +1,11 @@
-export { getEpicViewLog } from '../../shared/src/lib/viewLog';
+export { getEpicViewLog, logEpicView } from '../../shared/src/lib/viewLog';
 export { getWeeklyArticleHistory, incrementWeeklyArticleCount } from '../../shared/src/lib/history';
+export {
+    replaceNonArticleCountPlaceholders,
+    containsNonArticleCountPlaceholder,
+} from '../../shared/src/lib/placeholders';
+export * from '../../shared/src/lib/geolocation';
+export * from '../../shared/src/lib/viewLog';
+export * from '../../shared/src/lib/reminderFields';
+export { SecondaryCtaType } from '../../shared/src/types/props/shared';
 export * from './requests';
--- packages/dotcom/src/types.ts
+++ packages/dotcom/src/types.ts
@@ -4,4 +4,7 @@ export {
     HeaderPayload,
     WeeklyArticleHistory,
 } from '../../shared/src/types/targeting';
+export * from '../../shared/src/types/prices';
+export { EpicProps } from '../../shared/src/types/props/epic';
+export * from '../../shared/src/types/reminders';
 export { ModuleDataResponse, ModuleData } from './requests';
