628fe3656b930e14b00ec109381ff16c0f33b975
Tom Forbes <tom.forbes@theguardian.com>
2023-12-29 14:48:13 +0000

    tidy

--- packages/modules/src/modules/banners/common/choiceCard/Button.tsx
+++ packages/modules/src/modules/banners/common/choiceCard/Button.tsx
@@ -1,100 +1,36 @@
 import React from 'react';
 import { SerializedStyles, css } from '@emotion/react';
-import { palette } from '@guardian/src-foundations';
-import { ThemeProvider } from '@emotion/react';
-import { Button as DSButton, LinkButton } from '@guardian/src-button';
+import { LinkButton } from '@guardian/src-button';
 import { SvgArrowRightStraight } from '@guardian/src-icons';
 import type { ReactComponent } from '../../../../types';
-
-// Custom theme for Button/LinkButton
-// See also `tertiaryButtonOverrides` below.
-const buttonStyles = {
-    textPrimary: palette.neutral[7],
-    backgroundPrimary: palette.brandAlt[400],
-    backgroundPrimaryHover: palette.brandAlt[300],
-    textSecondary: palette.neutral[7],
-    backgroundSecondary: palette.neutral[93],
-    backgroundSecondaryHover: palette.neutral[86],
-    borderSecondary: palette.neutral[86],
-};
-
-const contributionsTheme = {
-    button: buttonStyles,
-    link: buttonStyles,
-};
-
-type Url = string;
 
 type Props = {
     ctaText: string;
     ctaUrl: string;
-    onClickAction: Function | Url;
-    // children: React.ReactElement | string;
-    priority?: 'primary' | 'secondary';
-    showArrow?: boolean;
-    isTertiary?: boolean;
-    cssOverrides?: SerializedStyles | SerializedStyles[] | undefined;
     onCtaClick: () => void;
+    cssOverrides?: SerializedStyles | SerializedStyles[] | undefined;
 };
 
-// Overrides for tertiary button
-// Unfortunatly they all need !important :(
-const tertiaryButtonOverrides = css`
-    border: 1px solid ${palette.neutral[7]} !important;
-    background-color: transparent !important;
-
-    :hover {
-        background-color: ${palette.neutral[86]} !important;
-    }
+// Always override the LinkButton border
+const style = css`
+    border: none;
 `;
 
-export const Button: ReactComponent<Props> = (allProps: Props) => {
-    const {
-        ctaText,
-        ctaUrl,
-        onCtaClick,
-        // children,
-        showArrow = false,
-        // priority = 'primary',
-        isTertiary,
-        cssOverrides,
-        ...props
-    } = allProps;
-    // LinkButton doesn't support 'tertiary' priority (unlike Button)
-    // So we'll map that to 'primary' and apply a CSS override on both of
-    // them so they get the same styles for 'tertiary' priority
+export const Button: ReactComponent<Props> = (props: Props) => {
+    const { ctaText, ctaUrl, onCtaClick, cssOverrides } = props;
     return (
-        <ThemeProvider theme={contributionsTheme}>
-            {/*<LinkButton*/}
-            {/*    href={ctaUrl}*/}
-            {/*    onClick={onCtaClick}*/}
-            {/*    icon={<SvgArrowRightStraight />}*/}
-            {/*    iconSide="right"*/}
-            {/*    target="_blank"*/}
-            {/*    rel="noopener noreferrer"*/}
-            {/*    // priority={isTertiary ? 'primary' : priority}*/}
-            {/*    priority="tertiary"*/}
-            {/*    css={isTertiary ? tertiaryButtonOverrides : undefined}*/}
-            {/*    cssOverrides={cssOverrides}*/}
-            {/*    {...props}*/}
-            {/*>*/}
-            {/*    {ctaText}*/}
-            {/*</LinkButton>*/}
-
-            <LinkButton
-                href={ctaUrl}
-                onClick={onCtaClick}
-                priority="tertiary"
-                cssOverrides={cssOverrides}
-
-                icon={<SvgArrowRightStraight />}
-                iconSide="right"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                {ctaText}
-            </LinkButton>
-
-        </ThemeProvider>
+        <LinkButton
+            href={ctaUrl}
+            onClick={onCtaClick}
+            priority="tertiary"
+            cssOverrides={cssOverrides}
+            css={style}
+            icon={<SvgArrowRightStraight />}
+            iconSide="right"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            {ctaText}
+        </LinkButton>
     );
 };
--- packages/modules/src/modules/banners/momentTemplate/MomentTemplateBanner.tsx
+++ packages/modules/src/modules/banners/momentTemplate/MomentTemplateBanner.tsx
@@ -143,6 +143,7 @@ export function getMomentTemplateBanner(
                                 content={content}
                                 getCtaText={getCtaText}
                                 cssCtaOverides={buttonStyles(templateSettings.primaryCtaSettings)}
+                                onCtaClick={onCtaClick}
                             />
                         )}
                     </div>
