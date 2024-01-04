6cb5a3654270e00f6fb797e7264051e19fb9115e
Tom Forbes <tom.forbes@theguardian.com>
2024-01-04 08:56:17 +0000

    More exports to support migration of epic to DCR (#1028)
    * WIP - epic migration to dcr
    
    * changeset
    
    * lint

--- packages/dotcom/src/index.ts
+++ packages/dotcom/src/index.ts
@@ -7,5 +7,11 @@ export {
 export * from '../../shared/src/lib/geolocation';
 export * from '../../shared/src/lib/viewLog';
 export * from '../../shared/src/lib/reminderFields';
-export { SecondaryCtaType } from '../../shared/src/types/props/shared';
+export {
+    SecondaryCtaType,
+    TickerCountType,
+    TickerEndType,
+} from '../../shared/src/types/props/shared';
+export { contributionTabFrequencies } from '../../shared/src/types/abTests/epic';
+export { epicPropsSchema } from '../../shared/src/types/props/epic';
 export * from './requests';
