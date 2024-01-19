4b7ea71d0e55f742f2d0d4316e7defa811b23122
andrewHEguardian <114918544+andrewHEguardian@users.noreply.github.com>
2024-01-19 16:37:07 +0000

    remove lint rule as it applied to server side too

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,6 @@
 module.exports = {
     parser: '@typescript-eslint/parser',
     extends: [
-        'plugin:@guardian/eslint-plugin-source-foundations/recommended',
         'plugin:@typescript-eslint/recommended',
         'plugin:react/recommended',
         'plugin:import/typescript',
