c24eaed49d8ddbfcc329a4df5f55bfeffbd418dc
andrewHEguardian <114918544+andrewHEguardian@users.noreply.github.com>
2024-04-03 15:36:50 +0100

    get samples and calculate means of each variant

--- packages/server/src/bandit/banditData.ts
+++ packages/server/src/bandit/banditData.ts
@@ -23,8 +23,8 @@ const queryResultSchema = z.array(testSampleSchema);
 type TestSample = z.infer<typeof testSampleSchema>;
 
 export async function testBanditLocally(): Promise<string> {
-    const queryResult = await getBanditSamplesForTest('2024-03-05_EPIC_PRIMARY__US');
-    console.log('getBanditSamplesForTest', queryResult);
+    const queryResult = await buildBanditDataForTest('2024-03-05_EPIC_PRIMARY__US');
+    console.log('buildBanditDataForTest', queryResult);
     return 'success';
 }
 
@@ -44,7 +44,7 @@ function queryForTestSamples(testName: string, nSamples: number) {
 }
 
 async function getBanditSamplesForTest(testName: string): Promise<TestSample[]> {
-    const queryResult = await queryForTestSamples(testName, 1);
+    const queryResult = await queryForTestSamples(testName, 2);
 
     const parsedResults = queryResultSchema.safeParse(queryResult.Items);
 
@@ -55,17 +55,41 @@ async function getBanditSamplesForTest(testName: string): Promise<TestSample[]> 
     return parsedResults.data;
 }
 
-interface BanditData {
-    testName: string;
-    variants: {
-        variantName: string;
-        mean: number;
-    };
+interface BanditVariantData {
+    variantName: string;
+    mean: number;
 }
 
-// function buildBanditDataForTest(testName: string): Promise<BanditData> {
-//     // TODO - get samples and calculate variant means
-// }
+interface BanditData {
+    testName: string;
+    variants: BanditVariantData[];
+}
+
+async function buildBanditDataForTest(testName: string): Promise<BanditData> {
+    const samples = await getBanditSamplesForTest(testName);
+
+    const variantsData = samples.flatMap((sample) => sample.variants);
+    const variantNames = Array.from(new Set(variantsData.map((variant) => variant.variantName)));
+
+    const variantMeans = variantNames.map((variantName) => {
+        const filteredVariantsData = variantsData.filter(
+            (variant) => variant.variantName === variantName,
+        );
+
+        const sum = filteredVariantsData.reduce((acc, curr) => acc + curr.avGbpPerView, 0);
+        const mean = sum / filteredVariantsData.length;
+
+        return {
+            variantName,
+            mean,
+        };
+    });
+
+    return {
+        testName,
+        variants: variantMeans,
+    };
+}
 
 // function buildBanditData(epicTestsProvider: ValueProvider<EpicTest[]>): Promise<BanditData[]> {
 //     const testNames = epicTestsProvider.get().map((test) => test.name);
