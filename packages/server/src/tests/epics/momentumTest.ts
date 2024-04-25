580b06f0b2d34f0d3528939bd0cfa6ab7e96863f
andrewHEguardian <114918544+andrewHEguardian@users.noreply.github.com>
2024-04-25 11:59:06 +0100

    update to target mediumHigh and make the categories explicit

--- packages/server/src/tests/epics/momentumTest.ts
+++ packages/server/src/tests/epics/momentumTest.ts
@@ -2,6 +2,59 @@ import { getWeeksInWindow } from '../../lib/history';
 import { WeeklyArticleHistory, WeeklyArticleLog } from '@sdc/shared/types';
 import { Filter } from './epicSelection';
 
+/*
+We categorize into 6 buckets of article count/engagement.
+
+    1. 1-10
+    2. 11-20
+    3. 21-30
+    4. 31-40
+    5.41-50
+    6. 50+
+
+We use the below logic to measure the momentum of engagement.
+Low  -- jumped two categories
+Medium -- jumped three categories
+Medium-High -- jumped four categories
+High -- jumped five categories
+ */
+
+const jumps = {
+    low: 2,
+    medium: 3,
+    mediumHigh: 4,
+    high: 5
+} as const;
+
+function isIncreasedEngagement(weeksInWindow: WeeklyArticleHistory): boolean {
+    const categoryForThirdMonth = getCategoryOfArticleViewed(weeksInWindow.slice(0, 4));
+    const categoryForSecondMonth = getCategoryOfArticleViewed(weeksInWindow.slice(4, 8));
+    const categoryForFirstMonth = getCategoryOfArticleViewed(weeksInWindow.slice(8));
+    if (
+        categoryForThirdMonth >= categoryForSecondMonth &&
+        categoryForSecondMonth >= categoryForFirstMonth
+    ) {
+        return categoryForThirdMonth - categoryForFirstMonth >= jumps.mediumHigh;
+    }
+    return false;
+}
+
+const getCategoryOfArticleViewed = (history: WeeklyArticleHistory): number => {
+    const sum = history.reduce(
+        (accumulator: number, articleLog: WeeklyArticleLog) => articleLog.count + accumulator,
+        0,
+    );
+    return getCategory(sum);
+};
+
+const getCategory = (count: number): number => {
+    if (count > 50) {
+        return 6;
+    }
+
+    return Math.ceil(count / 10);
+};
+
 export const momentumMatches: Filter = {
     id: 'momentumMatches',
     test: (test, targeting): boolean => {
@@ -17,48 +70,4 @@ export const momentumMatches: Filter = {
         }
         return true;
     },
-};
-
-function isIncreasedEngagement(weeksInWindow: WeeklyArticleHistory): boolean {
-    const categoryForThirdMonth = getCategoryOfArticleViewed(weeksInWindow.slice(0, 4));
-    const categoryForSecondMonth = getCategoryOfArticleViewed(weeksInWindow.slice(4, 8));
-    const categoryForFirstMonth = getCategoryOfArticleViewed(weeksInWindow.slice(8));
-    if (
-        categoryForThirdMonth >= categoryForSecondMonth &&
-        categoryForSecondMonth >= categoryForFirstMonth
-    ) {
-        return categoryForThirdMonth - categoryForFirstMonth >= 5;
-    }
-    return false;
-}
-
-const getCategoryOfArticleViewed = (history: WeeklyArticleHistory): number => {
-    const sum = history.reduce(
-        (accumulator: number, articleLog: WeeklyArticleLog) => articleLog.count + accumulator,
-        0,
-    );
-    return getCategory(sum);
-};
-
-//We categorize into 6 buckets of article count/engagement.
-/*
-    1. 1-10
-    2. 11-20
-    3. 21-30
-    4. 31-40
-    5.41-50
-    6. 50+
-
-We use te below logic to measure the momentum of engagement.
-Low  -- jumped two categories
-Medium-- jumped three categories
-Medium-High-- jumped four categories
-High -- jumped five categories
-
- */
-const getCategory = (count: number): number => {
-    if (count > 50) {
-        return 6;
-    }
-    return Math.ceil(count / 10);
 };
