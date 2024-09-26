e6b2709c2b1005e834276c0e492f5a9d0cf4f0a0
Tom Forbes <tom.forbes@theguardian.com>
2024-09-26 14:45:42 +0100

    undo jest changes

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,6 @@
 module.exports = {
-    preset: 'ts-jest/presets/js-with-ts',
+    preset: 'ts-jest',
     testEnvironment: 'node',
     roots: ['<rootDir>/src'],
     modulePathIgnorePatterns: ['<rootDir>/src/server/factories/test.ts'],
-    transform: {
-        '^.+\\.tsx?$': [
-            'ts-jest',
-            {
-                tsconfig: 'tsconfig.test.json',
-            },
-        ],
-    },
 };
