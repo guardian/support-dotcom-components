025a14e9d21694e3be1e901bee7d12515966d334
Sue Burt <sue.burt@guardian.co.uk>
2024-11-04 16:32:57 +0000

    Adds default font heading size of medium for Factory

--- src/server/factories/bannerDesign.ts
+++ src/server/factories/bannerDesign.ts
@@ -28,6 +28,11 @@ export default Factory.define<BannerDesignFromTool>(() => ({
             'https://i.guim.co.uk/img/media/6c933a058d1ce37a5ad17f79895906150812dfee/0_0_1768_1420/500.png?width=500&quality=75&s=9277532ddf184a308e14218e3576543b',
         altText: 'Example alt text',
     },
+    fonts: {
+        heading: {
+            size: 'medium',
+        },
+    },
     colours: {
         basic: {
             background: stringToHexColour('F1F8FC'),
