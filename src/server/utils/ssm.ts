d13317da6117cfd1076e1156aaba0990273f21a6
Tom Forbes <tom.forbes@theguardian.com>
2025-01-21 08:20:44 +0000

    refactor ssm

--- src/server/utils/ssm.ts
+++ src/server/utils/ssm.ts
@@ -1,14 +1,23 @@
-import { SSMClient, GetParameterCommand } from '@aws-sdk/client-ssm'; // ES Modules import
+// import { SSMClient, GetParameterCommand } from '@aws-sdk/client-ssm'; // ES Modules import
+import * as AWS from 'aws-sdk';
 
 // https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/client/ssm/command/GetParametersByPathCommand/
 
 export async function getSsmValue(stage: string, id: string): Promise<string | undefined> {
     const name = `/membership/support-dotcom-components/${stage}/${id}`;
-    const client = new SSMClient({ region: 'eu-west-1' });
-    const input = {
-        Name: name,
-    };
-    const command = new GetParameterCommand(input);
-    const response = await client.send(command);
+    const client = new AWS.SSM({ region: 'eu-west-1' });
+    // const input = {
+    //     Name: name,
+    // };
+    // const command = new GetParameterCommand(input);
+    // const response = await client.send(command);
+    // return response.Parameter?.Value;
+
+    const response = await client
+        .getParameter({
+            Name: name,
+        })
+        .promise();
+
     return response.Parameter?.Value;
 }
