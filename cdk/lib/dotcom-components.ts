2f46ce2abdcf25b78fee16461a8ab09d4566a8ec
Tom Forbes <tom.forbes@theguardian.com>
2025-03-03 08:20:20 +0000

    minimum 6 instances

--- cdk/lib/dotcom-components.ts
+++ cdk/lib/dotcom-components.ts
@@ -216,7 +216,7 @@ chown -R dotcom-components:support /var/log/dotcom-components
         ];
 
 		const scaling: GuAsgCapacity = {
-			minimumInstances: this.stage === 'CODE' ? 1 : 4,
+			minimumInstances: this.stage === 'CODE' ? 1 : 6,
 			maximumInstances: this.stage === 'CODE' ? 2 : 18,
 		};
 
