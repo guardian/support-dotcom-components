a7cccdc8c462615cf119cc162d835633d526ac36
Tom Forbes <tom.forbes@theguardian.com>
2025-03-14 12:14:33 +0000

    minimum weight for roulette sampling

--- src/server/roulette/rouletteSelection.ts
+++ src/server/roulette/rouletteSelection.ts
@@ -18,13 +18,23 @@ export function selectVariantUsingRoulette<V extends Variant, T extends Test<V>>
         return selectRandomVariant(test);
     }
 
-    // sorted variant weights, which will add up to 1
+    const minWeight = 0.1; // Ensure no variant gets less than 10%
     const variantsWithWeights: { weight: number; variantName: string }[] = testBanditData.variants
-        .map(({ variantName, mean }) => ({ variantName, weight: mean / sumOfMeans }))
+        .map(({ variantName, mean }) => ({
+            variantName,
+            weight: Math.max(mean / sumOfMeans, minWeight),
+        }))
         .sort((a, b) => a.weight - b.weight);
 
-    for (let i = 0, acc = 0; i < variantsWithWeights.length; i++) {
-        const variant = variantsWithWeights[i];
+    // The sum of the weights may be greater than 1, so we now need to normalise them
+    const sumOfWeights = variantsWithWeights.reduce((sum, v) => sum + v.weight, 0);
+    const normalisedWeights = variantsWithWeights.map(({ variantName, weight }) => ({
+        variantName,
+        weight: weight / sumOfWeights,
+    }));
+
+    for (let i = 0, acc = 0; i < normalisedWeights.length; i++) {
+        const variant = normalisedWeights[i];
         if (rand < variant.weight + acc) {
             return test.variants.find((v) => v.name === variant.variantName);
         }
