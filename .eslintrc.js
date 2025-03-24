20d9948b46edd6e4c475d7a125007e77867a9f8e
Sue Burt <sue.burt@guardian.co.uk>
2025-03-24 17:13:35 +0000

    Remove React related eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,6 @@ module.exports = {
     parser: '@typescript-eslint/parser',
     extends: [
         'plugin:@typescript-eslint/recommended',
-        'plugin:react/recommended',
         'plugin:import/typescript',
         'plugin:prettier/recommended',
     ],
@@ -30,19 +29,5 @@ module.exports = {
                 ignoreRestSiblings: true,
             },
         ],
-        'react/display-name': 'off',
-
-        // Not compatible with TS + arrow functions - https://github.com/yannickcr/eslint-plugin-react/issues/2353#issuecomment-674792754
-        // TS does the type checking anyway
-        'react/prop-types': 'off',
-
-        // It's a known issue that eslint-plugin-react doesn't play nicely with the CSS prop:
-        // https://github.com/emotion-js/emotion/issues/2878#issuecomment-1236384580
-        'react/no-unknown-property': ['error', { ignore: ['css'] }],
-    },
-    settings: {
-        react: {
-            version: 'detect',
-        },
     },
 };
