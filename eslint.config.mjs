c439611e0e428a2b7aecd1d5a4ad4ca8d126ecd5
Sue Burt <sue.burt@guardian.co.uk>
2025-03-27 11:14:35 +0000

    add rules and ignores

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,36 @@
 import guardian from '@guardian/eslint-config';
 
 export default [
+	{
+		ignores: [
+			'node_modules',
+			'dist',
+
+			'customize.js',
+			'rollup.config.js',
+			'webpack.*js',
+		],
+	},
 	...guardian.configs.recommended,
 	...guardian.configs.jest,
+	{
+		rules: {
+			// Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+			// e.g. "@typescript-eslint/explicit-function-return-type": "off",
+			curly: 2,
+			'@typescript-eslint/no-inferrable-types': [
+				'error',
+				{
+					ignoreParameters: true,
+				},
+			],
+			'@typescript-eslint/no-unused-vars': [
+				'error',
+				{
+					args: 'after-used',
+					ignoreRestSiblings: true,
+				},
+			],
+		},
+	},
 ];
