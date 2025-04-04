4dcaee5d87db5454aeb6188c445369d28f57292b
Tom Forbes <tom.forbes@theguardian.com>
2025-04-04 14:41:49 +0100

    test it

--- src/server/productPrices.ts
+++ src/server/productPrices.ts
@@ -2,7 +2,7 @@ import type { Prices } from '../shared/types';
 import { logError } from './utils/logging';
 import { fetchSupportFrontendData } from './utils/supportFrontend';
 import type { ValueReloader } from './utils/valueReloader';
-import { buildReloader } from './utils/valueReloader';
+import { buildReloader} from './utils/valueReloader';
 
 const getProductPrices = (): Promise<Prices | undefined> =>
     fetchSupportFrontendData('prices')
