0011806568aebcc30403caa2dca3ed4be01f1354
Sue Burt <sookburt@users.noreply.github.com>
2025-04-09 11:30:14 +0100

    Reinstate typescript eslint naming convention rule (#1327)
    * Reenable typescript-eslint/naming-convention to default recommended
    
    * Add eslint-disable-next-line where conflicting scala naming convention
    
    * Fix following feedback
    
    ---------
    
    Co-authored-by: sookburt <“sue.burt@guardian.co.uk”>

--- src/shared/types/props/shared.ts
+++ src/shared/types/props/shared.ts
@@ -46,13 +46,16 @@ export const secondaryCtaSchema = z.discriminatedUnion('type', [
 ]);
 
 export enum TickerEndType {
+    // eslint-disable-next-line @typescript-eslint/naming-convention -- model predates linting rule
     unlimited = 'unlimited',
+    // eslint-disable-next-line @typescript-eslint/naming-convention -- model predates linting rule
     hardstop = 'hardstop', // currently unsupported
 }
 
 export const tickerEndTypeSchema = z.nativeEnum(TickerEndType);
 
 export enum TickerCountType {
+    // eslint-disable-next-line @typescript-eslint/naming-convention -- model predates linting rule
     money = 'money',
 }
 
--- src/shared/types/purchaseInfo.ts
+++ src/shared/types/purchaseInfo.ts
@@ -1,9 +1,11 @@
 import { z } from 'zod';
 
 const purchaseInfoProduct = ['Contribution', 'SupporterPlus', 'GuardianWeekly', 'Paper'] as const;
+// eslint-disable-next-line @typescript-eslint/naming-convention -- model predates linting rule
 export type purchaseInfoProduct = (typeof purchaseInfoProduct)[number];
 export const purchaseInfoProductSchema = z.enum(purchaseInfoProduct);
 
 const purchaseInfoUser = ['new', 'guest', 'current'] as const;
+// eslint-disable-next-line @typescript-eslint/naming-convention -- model predates linting rule
 export type purchaseInfoUser = (typeof purchaseInfoUser)[number];
 export const purchaseInfoUserSchema = z.enum(purchaseInfoUser);
