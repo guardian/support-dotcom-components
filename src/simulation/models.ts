f0464a7548557ab576e8acea8280c2d96ff5100a
Tom Forbes <tom.forbes@theguardian.com>
2025-04-16 14:23:48 +0100

    refactor

---
+++ src/simulation/models.ts
@@ -0,0 +1,25 @@
+import type { BanditData } from '../server/selection/banditData';
+import type { Test, Variant } from '../shared/types';
+
+export interface SelectionAlgorithm {
+    name: string;
+    run: <V extends Variant, T extends Test<V>>(
+        test: T,
+        testBanditData?: BanditData,
+    ) => V | undefined;
+}
+
+export interface VariantModel {
+    name: string;
+    mean: (timestep: number) => number;
+    standardDeviation: (timestep: number) => number;
+}
+
+export interface Simulation<V extends Variant, T extends Test<V>> {
+    test: T;
+    algorithms: SelectionAlgorithm[];
+    variantsScenario: VariantModel[];
+    timesteps: number;
+    impressionsPerTimestep: number;
+    runs: number;
+}
