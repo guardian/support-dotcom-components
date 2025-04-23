64264df0d1fca107c72ebfed11e085dc744b07af
Sue Burt <sookburt@users.noreply.github.com>
2025-04-23 11:52:23 +0100

    Bulk suppress existing eslint rule failures (#1330)
    * Bump version of eslint to use bulk suppressions
    
    * Add note about bulk suppressions and comment out all switched off rules
    
    * suppress all command run with fix flag to create suppression file
    
    * reorder rules, add link to eslint rules docs
    
    ---------
    
    Co-authored-by: sookburt <“sue.burt@guardian.co.uk”>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,20 +2,12 @@ import guardian from '@guardian/eslint-config';
 import globals from 'globals';
 
 export default [
-	{
-		ignores: [
-			'node_modules',
-			'dist',
-            'server-dist',
-
-			'rollup.config.js',
-			'webpack.*js',
-            'cdk',
-		],
-	},
-	...guardian.configs.recommended,
-	...guardian.configs.jest,
-	{
+    {
+        ignores: ['node_modules', 'dist', 'server-dist', 'rollup.config.js', 'webpack.*js', 'cdk'],
+    },
+    ...guardian.configs.recommended,
+    ...guardian.configs.jest,
+    {
         ignores: ['eslint.config.mjs'],
         languageOptions: {
             globals: {
@@ -30,28 +22,33 @@ export default [
                 tsconfigRootDir: './',
             },
         },
-		rules: {
-			// Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
-			// e.g. "@typescript-eslint/explicit-function-return-type": "off",
-			curly: 2,
+        rules: {
+            // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+            // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+            curly: 2,
 
-			// potentially to fix later see https://trello.com/c/lc8lG7Zj
-			'@typescript-eslint/no-unnecessary-condition': 'off',
-			'@typescript-eslint/no-unsafe-assignment': 'off',
-			'@typescript-eslint/prefer-nullish-coalescing': 'off',
-			'@typescript-eslint/no-unsafe-member-access': 'off',
-			'@typescript-eslint/require-await': 'off',
-			'@typescript-eslint/no-unsafe-enum-comparison': 'off',
-			'@typescript-eslint/no-unsafe-argument': 'off',
-			'@typescript-eslint/no-unsafe-return': 'off',
-			'@typescript-eslint/no-base-to-string': 'off',
-			'@typescript-eslint/prefer-promise-reject-errors': 'off',
-			'@typescript-eslint/no-unsafe-call': 'off',
-			'@typescript-eslint/no-floating-promise': 'off',
-			'@typescript-eslint/restrict-template-expressions': 'off',
-			'@typescript-eslint/no-floating-promises': 'off',
-            '@typescript-eslint/prefer-optional-chain': 'off',
-            '@typescript-eslint/await-thenable': 'off',
-		},
-	},
+            // Since we moved to using the @guardian/eslint-config we are getting many failures on existing code.
+            // These are all rules that we should be using but we are not ready to fix all the existing code yet.
+            // We are using bulk suppressions so we can apply the rules now and fix existing failures later
+            // https://eslint.org/blog/2025/04/introducing-bulk-suppressions/
+
+            // These are the rules that we may need to fix later see https://trello.com/c/lc8lG7Zj and https://typescript-eslint.io/rules/ 
+            // '@typescript-eslint/await-thenable': 'off',
+            // '@typescript-eslint/no-base-to-string': 'off',
+            // '@typescript-eslint/no-floating-promise': 'off',
+            // '@typescript-eslint/no-floating-promises': 'off',
+            // '@typescript-eslint/no-unnecessary-condition': 'off',
+            // '@typescript-eslint/no-unsafe-argument': 'off',
+            // '@typescript-eslint/no-unsafe-assignment': 'off',
+            // '@typescript-eslint/no-unsafe-call': 'off',
+            // '@typescript-eslint/no-unsafe-enum-comparison': 'off',
+            // '@typescript-eslint/no-unsafe-member-access': 'off',
+            // '@typescript-eslint/no-unsafe-return': 'off',
+            // '@typescript-eslint/prefer-nullish-coalescing': 'off',
+            // '@typescript-eslint/prefer-optional-chain': 'off',
+            // '@typescript-eslint/prefer-promise-reject-errors': 'off',
+            // '@typescript-eslint/require-await': 'off',
+            // '@typescript-eslint/restrict-template-expressions': 'off',
+        },
+    },
 ];
