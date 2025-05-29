2468ca13641d2d60bd44314bc45e14336d4c1049
Pascal Honoré <pascal@alseyn.net>
2025-05-29 07:41:15 +0100

    Introduce getTreatments for clarity

--- src/server/api/auxiaProxyRouter.ts
+++ src/server/api/auxiaProxyRouter.ts
@@ -170,6 +170,71 @@ const callLogTreatmentInteration = async (
     // We are not consuming an answer from the server, so we are not returning anything.
 };
 
+interface GetTreatmentRequestBody {
+    browserId: string | undefined; // optional field, will not be sent by the client is user has not consented to personal data use.
+    isSupporter: boolean;
+    dailyArticleCount: number; // [1]
+    articleIdentifier: string;
+    editionId: string;
+    contentType: string;
+    sectionId: string;
+    tagIds: string[];
+    gateDismissCount: number;
+    countryCode: string;
+    mvtId: number;
+    should_show_legacy_gate_tmp: boolean; // [2]
+}
+
+// [1] articleIdentifier examples:
+//  - 'www.theguardian.com/money/2017/mar/10/ministers-to-criminalise-use-of-ticket-tout-harvesting-software'
+//  - 'www.theguardian.com/tips'
+
+// [2] temporary attribute to help imminent rerouting of non Auxia audience share
+// to SDC, but without requiring a
+// full duplication of the client side logic into SDC.
+// See https://github.com/guardian/dotcom-rendering/pull/13944
+// for details.
+
+const getTreatments = async (
+    config: AuxiaRouterConfig,
+    body: GetTreatmentRequestBody,
+): Promise<AuxiaAPIGetTreatmentsResponseData | undefined> => {
+    // This function gets the body of a '/auxia/get-treatments' request and return the data to post to the client
+    // or undefined.
+
+    // As a first step of handling this request, we first check whether the call is from the Auxia
+    // audience or the non Auxia audience. If it is from the non Auxia audience, then we follow the value of
+    // should_show_legacy_gate_tmp to decide whether to return a default gate or not. If it is from the Auxia
+    // audience, then we proceed with `callGetTreatments` as normal.
+
+    if (!mvtIdIsAuxiaAudienceShare(body.mvtId)) {
+        if (body.should_show_legacy_gate_tmp) {
+            const auxiaData = guDefaultGateGetTreatmentsResponseData(
+                body.dailyArticleCount,
+                body.gateDismissCount,
+            );
+            return Promise.resolve(auxiaData);
+        } else {
+            return Promise.resolve(undefined);
+        }
+    }
+
+    return callGetTreatments(
+        config.apiKey,
+        config.projectId,
+        body.browserId,
+        body.isSupporter,
+        body.dailyArticleCount,
+        body.articleIdentifier,
+        body.editionId,
+        body.contentType,
+        body.sectionId,
+        body.tagIds,
+        body.gateDismissCount,
+        body.countryCode,
+    );
+};
+
 // --------------------------------
 // Router
 // --------------------------------
@@ -190,67 +255,12 @@ export const buildAuxiaProxyRouter = (config: AuxiaRouterConfig): Router => {
             'gateDismissCount',
             'countryCode',
             'mvtId',
+            'should_show_legacy_gate_tmp',
         ]),
         async (req: express.Request, res: express.Response, next: express.NextFunction) => {
             try {
-                const body = req.body as {
-                    browserId: string | undefined; // optional field, will not be sent by the client is user has not consented to personal data use.
-                    isSupporter: boolean;
-                    dailyArticleCount: number; // [1]
-                    articleIdentifier: string;
-                    editionId: string;
-                    contentType: string;
-                    sectionId: string;
-                    tagIds: string[];
-                    gateDismissCount: number;
-                    countryCode: string;
-                    mvtId: number;
-                    should_show_legacy_gate_tmp: boolean; // [2]
-                };
-
-                // [1] articleIdentifier examples:
-                //  - 'www.theguardian.com/money/2017/mar/10/ministers-to-criminalise-use-of-ticket-tout-harvesting-software'
-                //  - 'www.theguardian.com/tips'
-
-                // [2] temporary attribute to help imminent rerouting of non Auxia audience share
-                // to SDC, but without requiring a
-                // full duplication of the client side logic into SDC.
-                // See https://github.com/guardian/dotcom-rendering/pull/13944
-                // for details.
-
-                // As a first step of handling this request, we first check whether the call is from the Auxia
-                // audience or the non Auxia audience. If it is from the non Auxia audience, then we follow the value of
-                // should_show_legacy_gate_tmp to decide whether to return a default gate or not. If it is from the Auxia
-                // audience, then we proceed with `callGetTreatments` as normal.
-
-                let auxiaData;
-
-                if (!mvtIdIsAuxiaAudienceShare(body.mvtId)) {
-                    if (body.should_show_legacy_gate_tmp) {
-                        auxiaData = guDefaultGateGetTreatmentsResponseData(
-                            body.dailyArticleCount,
-                            body.gateDismissCount,
-                        );
-                    } else {
-                        auxiaData = undefined;
-                    }
-                } else {
-                    auxiaData = await callGetTreatments(
-                        config.apiKey,
-                        config.projectId,
-                        body.browserId,
-                        body.isSupporter,
-                        body.dailyArticleCount,
-                        body.articleIdentifier,
-                        body.editionId,
-                        body.contentType,
-                        body.sectionId,
-                        body.tagIds,
-                        body.gateDismissCount,
-                        body.countryCode,
-                    );
-                }
-
+                const getTreatmentRequestBody = req.body as GetTreatmentRequestBody;
+                const auxiaData = await getTreatments(config, getTreatmentRequestBody);
                 if (auxiaData !== undefined) {
                     const data = buildAuxiaProxyGetTreatmentsResponseData(auxiaData);
                     res.locals.auxiaTreatmentId = data?.userTreatment?.treatmentId;
