0c3df5b9c7af754e286eb5fcfaf7254d6e55bf06
Tom Forbes <tom.forbes@theguardian.com>
2025-06-26 15:46:27 +0100

    fromNodeProviderChain

--- src/server/utils/dynamodb.ts
+++ src/server/utils/dynamodb.ts
@@ -1,16 +1,13 @@
 import { DynamoDB } from '@aws-sdk/client-dynamodb';
-import { fromIni } from '@aws-sdk/credential-providers';
+import { fromIni, fromNodeProviderChain } from '@aws-sdk/credential-providers';
 import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
 import { isDev } from '../lib/env';
 
 export const getDynamoDbClient = (): DynamoDBDocumentClient => {
-    if (isDev) {
-        return DynamoDBDocumentClient.from(
-            new DynamoDB({
-                credentials: fromIni({ profile: 'membership' }),
-                region: 'eu-west-1',
-            }),
-        );
-    }
-    return DynamoDBDocumentClient.from(new DynamoDB({ region: 'eu-west-1' }));
+    return DynamoDBDocumentClient.from(
+        new DynamoDB({
+            credentials: isDev ? fromIni({ profile: 'membership' }) : fromNodeProviderChain(),
+            region: 'eu-west-1',
+        }),
+    );
 };
