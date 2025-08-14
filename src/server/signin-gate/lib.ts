8a65f9806e60cb0fbdf98b64530f14cf71827972
Pascal Honoré <pascal@alseyn.net>
2025-08-14 07:47:37 +0100

    [auxia proxy] code layout refactoring

--- src/server/signin-gate/lib.ts
+++ src/server/signin-gate/lib.ts
@@ -1,3 +1,7 @@
+// --------------------------------------------------------
+// Types
+// --------------------------------------------------------
+
 interface AuxiaAPIContextualAttributeString {
     key: string;
     stringValue: string;
@@ -61,6 +65,77 @@ export interface AuxiaAPIGetTreatmentsResponseData {
     responseId: string;
     userTreatments: AuxiaAPIUserTreatment[];
 }
+
+type ShowGateValues = 'true' | 'mandatory' | 'dismissible' | undefined;
+
+export interface GetTreatmentRequestBody {
+    browserId: string | undefined; // optional field, will not be sent by the client is user has not consented to personal data use.
+    isSupporter: boolean;
+    dailyArticleCount: number; // [1]
+    articleIdentifier: string;
+    editionId: string;
+    contentType: string;
+    sectionId: string;
+    tagIds: string[];
+    gateDismissCount: number;
+    countryCode: string;
+    mvtId: number;
+    should_show_legacy_gate_tmp: boolean; // [2]
+    hasConsented: boolean;
+    shouldServeDismissible: boolean; // [3]
+    showDefaultGate: ShowGateValues; // [4]
+    gateDisplayCount: number; // [5]
+}
+
+// [1] articleIdentifier examples:
+//  - 'www.theguardian.com/money/2017/mar/10/ministers-to-criminalise-use-of-ticket-tout-harvesting-software'
+//  - 'www.theguardian.com/tips'
+
+// [2] temporary attribute to help imminent rerouting of non Auxia audience share
+// to SDC, but without requiring a
+// full duplication of the client side logic into SDC.
+// See https://github.com/guardian/dotcom-rendering/pull/13944
+// for details.
+
+// [3]
+// date: 03rd July 2025
+// If shouldServeDismissible, we should show a dismissible (not mandatory) gate.
+
+// [4]
+
+// date: 25rd July 2025
+// author: Pascal
+
+// In order to facilitate internal testing, this attribute, when defined, forces
+// the display of a sign-in gate. The values 'true' and 'dismissible' displays the
+// dismissible variant of the gu default gate, and the value 'mandatory' displays
+// the mandatory variant of the gu default gate.
+
+// Note that this attributes override the value of should_show_legacy_gate_tmp.
+
+// [5] (comment group: 04f093f0)
+
+// date: 11 Aug 2025
+// author: Pascal
+
+// gateDisplayCount was introduced to enrich the behavior of the default gate.
+// That number represents the number of times the gate has been displayed, excluding the
+// current rendering. Therefore the first time the number is 0.
+
+// At the time these lines are written we want the experience for non consented users
+// in Ireland, to be that the gates, as they display are (first line) corresponding
+// to values of gateDisplayCount (second line)
+//  -------------------------------------------------------------------------
+// | dismissible | dismissible | dismissible | mandatory (remains mandatory) |
+// |     0       |      1      |      2      |      3           etc          |
+//  -------------------------------------------------------------------------
+
+// For non consenting users outside ireland, the behavior doesn't change, we serve
+// dismissible gates
+
+// --------------------------------------------------------
+// Functions
+// --------------------------------------------------------
 
 export const buildGetTreatmentsRequestPayload = (
     projectId: string,
