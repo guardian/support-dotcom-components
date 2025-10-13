e87bff08360f8794267d77390d8374397a3f352f
Anastasiia Balenko <anastasiia.balenko.mindera@guardian.co.uk>
2025-10-13 15:19:58 +0100

    ts types fix after removal of reminders

--- src/dotcom/types.ts
+++ src/dotcom/types.ts
@@ -7,6 +7,5 @@ export {
 } from '../shared/types/targeting';
 export * from '../shared/types/prices';
 export { EpicProps } from '../shared/types/props/epic';
-export * from '../shared/types/reminders';
 export { ModuleDataResponse, ModuleData } from './requests';
 export { Image } from '../shared/types/props/shared';
