eddea22824a7079d41dd4348b3cd20fa38c13a80
Tom Forbes <tom.forbes@theguardian.com>
2026-01-26 11:29:25 +0000

    log undefined if no request to mparticle

--- src/server/api/epicRouter.ts
+++ src/server/api/epicRouter.ts
@@ -286,7 +286,7 @@ export const buildEpicRouter = (
                     );
                 }
                 res.locals.hasAuthorization = !!authHeader;
-                res.locals.gotMParticleProfile = !!(await forLogging());
+                res.locals.gotMParticleProfile = await forLogging();
 
                 res.send(response);
             } catch (error) {
