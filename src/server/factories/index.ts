8ceb2e347a79c3e94f955789136301e4ff749228
Tom Forbes <tom.forbes@theguardian.com>
2026-02-17 15:27:22 +0000

    remove unused

--- src/server/factories/index.ts
+++ src/server/factories/index.ts
@@ -1,13 +1,9 @@
 import bannerDesign from './bannerDesign';
 import bannerVariant from './bannerVariant';
-import epicVariant from './epicVariant';
 import targeting from './targeting';
-import test from './test';
 
 export const factories = {
     targeting,
-    test,
-    epicVariant,
     bannerVariant,
     bannerDesign,
 };
