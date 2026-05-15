4a936ff620944a68655fcf9d738224fc510dfcc3
Juarez Mota <juarez.mota.mindera@guardian.co.uk>
2026-05-15 11:17:11 +0100

    chore: add webpack-node-externals to exclude node_modules from server bundle

--- webpack.common.js
+++ webpack.common.js
@@ -1,8 +1,10 @@
 const path = require('path');
+const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
     entry: './src/server/server.ts',
     target: 'node',
+    externals: [nodeExternals()],
     output: {
         filename: 'server.js',
         path: path.resolve(__dirname, 'server-dist'),
