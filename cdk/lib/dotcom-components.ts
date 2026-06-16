2c8edbd9485f4f1254d68e78d47c817058405a26
Juarez Mota <juarez.mota.mindera@guardian.co.uk>
2026-06-16 15:53:20 +0100

    chore: disable ALB access logging

--- cdk/lib/dotcom-components.ts
+++ cdk/lib/dotcom-components.ts
@@ -353,6 +353,7 @@ sudo amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-
 			},
 			scaling,
 			instanceMetricGranularity: '1Minute',
+			withAccessLogging: false,
 		});
 
 		ec2App.autoScalingGroup.scaleOnRequestCount(
