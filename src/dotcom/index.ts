f0c50a67140606f6fcf9391a8b342c4554dac204
Tom Forbes <tom.forbes@theguardian.com>
2026-08-13 08:50:00 +0100

    Remove unused epic types (#1805)

--- src/dotcom/index.ts
+++ src/dotcom/index.ts
@@ -8,7 +8,6 @@ export * from '../shared/lib/viewLog';
 export * from '../shared/lib/reminderFields';
 export { SecondaryCtaType } from '../shared/types/props/shared';
 export { hexColourToString } from '../shared/types/props/design';
-export { contributionTabFrequencies } from '../shared/types/abTests/epic';
 export { headerPropsSchema } from '../shared/types/props/header';
 export { epicPropsSchema } from '../shared/types/props/epic';
 export { bannerSchema } from '../shared/types/props/banner';
--- src/shared/types/abTests/epic.ts
+++ src/shared/types/abTests/epic.ts
@@ -1,5 +1,4 @@
 import { z } from 'zod';
-import type { CountryGroupId } from '../../lib';
 import { countryGroupIdSchema, targetedRegionsSchema } from '../../lib';
 import { variantSchema } from '../props';
 import type { EpicTargeting } from '../targeting';
@@ -19,78 +18,6 @@ export const maxViewsSchema = z.object({
 });
 
 export type MaxViews = z.infer<typeof maxViewsSchema>;
-
-export type ContributionFrequency = 'ONE_OFF' | 'MONTHLY' | 'ANNUAL';
-
-export const contributionTabFrequencies: ContributionFrequency[] = ['ONE_OFF', 'MONTHLY', 'ANNUAL'];
-
-interface ContributionTypeItem {
-    label: string;
-    suffix: string;
-}
-export type ContributionType = Record<ContributionFrequency, ContributionTypeItem>;
-
-/*
-An amounts test can be in one of two forms:
-
-Country test:
-  Bespoke tests targeted at one or more geographical countries
-  `targeting` object will include a `countries` attribute
-    - a String array containing 2-letter ISO country codes
-  When the `isLive` boolean is `false`:
-    - the test is ignored; users will see their appropriate region test
-  When the `isLive` boolean is `true`:
-    - users will be randomly segregated into an AB test and see the appropriate variant
-    - analytics will use the `liveTestName` label, if available, else the `testName` label
-  A country can appear in more than one country test:
-    - if 2+ live tests include the country, the test with the lowest `order` value will display
-
-Region test:
-  Evergreen tests, one per geographical region
-  `targeting` object will include a `region` attribute
-    - the region label, as defined by the Region type
-  When the `isLive` boolean is `false`:
-    - the CONTROL variant will display
-    - analytics will use the `testName` label
-  When the `isLive` boolean is `true`:
-    - users will be randomly segregated into an AB test and see the appropriate variant
-    - analytics will use the `liveTestName` label
-*/
-interface AmountValuesObject {
-    amounts: number[];
-    defaultAmount: number;
-    hideChooseYourAmount?: boolean;
-}
-
-export type AmountsCardData = Record<ContributionFrequency, AmountValuesObject>;
-
-export interface AmountsVariant {
-    variantName: string;
-    defaultContributionType: ContributionFrequency;
-    displayContributionType: ContributionFrequency[];
-    amountsCardData: AmountsCardData;
-}
-
-export interface SelectedAmountsVariant extends AmountsVariant {
-    testName: string;
-}
-
-export type AmountsTestTargeting =
-    | { targetingType: 'Region'; region: CountryGroupId }
-    | { targetingType: 'Country'; countries: string[] };
-
-export interface AmountsTest {
-    testName: string;
-    liveTestName?: string;
-    testLabel?: string;
-    isLive: boolean;
-    targeting: AmountsTestTargeting;
-    order: number;
-    seed: number;
-    variants: AmountsVariant[];
-}
-
-export type AmountsTests = AmountsTest[];
 
 // for validation from DynamoDB
 export type EpicTestFromTool = z.infer<typeof epicTestFromToolSchema>;
