91309578d11ab9184a2c56b81ab73e1741831308
Anastasiia Balenko <anastasiia.balenko.mindera@guardian.co.uk>
2026-09-01 10:47:41 +0100

    missing policy added

--- cdk/lib/dotcom-components.ts
+++ cdk/lib/dotcom-components.ts
@@ -271,6 +271,7 @@ sudo amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-
 					`${this.stage}/banner-deploy/*`,
 					`${this.stage}/channel-switches.json`,
 					`${this.stage}/configured-amounts-v3.json`,
+					`${this.stage}/default-choice-cards-config.json`,
 					`${this.stage}/exclusions.json`,
 					`${this.stage}/guardian-weekly-propensity-test/*`,
 					`PROD/auxia-credentials.json`,
